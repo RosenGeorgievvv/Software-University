c53839e849406864edcd65309c50df617f01e219
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-13 11:46:10 +0300

    JS-Fundamentals-2023

--- JS-Fundamentals-2023/JS Basic Syntax, Conditional Statements/1.)MultiplyNumberBy2.js
+++ JS-Fundamentals-2023/JS Basic Syntax, Conditional Statements/1.)MultiplyNumberBy2.js
@@ -7,5 +7,4 @@ function multiplyNumberBy2(input){
     console.log(input * 2);
 
 }
-multiplyNumberBy2(["2"])
-//
+multiplyNumberBy2(["2"]);
---
+++ JS-Fundamentals-2023/JS Basic Syntax, Conditional Statements/2.)StudentInformation.js
@@ -0,0 +1,20 @@
+//Task: You will be given 3 parameters – student name (string), age (number), and average grade (number). 
+//Your task is to print all the info about the student in the following format:
+
+// `Name: {student name}, Age: {student age}, Grade: {student grade}`
+
+// Note: The grade should be formatted to the second decimal point.
+
+// Examples
+
+// Input Output
+
+// 'John', 15, 5.54678 Name: John, Age: 15, Grade: 5.55
+
+// 'Steve', 16, 2.1426 Name: Steve, Age: 16, Grade: 2.14
+
+// 'Marry', 12, 6.00 Name: Marry, Age: 12, Grade: 6.00
+
+
+//Solution:
+
