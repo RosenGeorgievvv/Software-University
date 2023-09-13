da36fe94f06598b057f65aaee8a7acab3dbf258a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-13 14:23:33 +0300

    update tasks

--- JS-Fundamentals-2023/JS Basic Syntax, Conditional Statements/2.)StudentInformation.js
+++ JS-Fundamentals-2023/JS Basic Syntax, Conditional Statements/2.)StudentInformation.js
@@ -1,20 +1,17 @@
-//Task: You will be given 3 parameters – student name (string), age (number), and average grade (number). 
+//Task: You will be given 3 parameters – student name (string), age (number), and average grade (number).
 //Your task is to print all the info about the student in the following format:
 
 // `Name: {student name}, Age: {student age}, Grade: {student grade}`
 
 // Note: The grade should be formatted to the second decimal point.
 
-// Examples
-
-// Input Output
-
-// 'John', 15, 5.54678 Name: John, Age: 15, Grade: 5.55
-
-// 'Steve', 16, 2.1426 Name: Steve, Age: 16, Grade: 2.14
-
-// 'Marry', 12, 6.00 Name: Marry, Age: 12, Grade: 6.00
-
-
 //Solution:
 
+function studentsInfo(name, age, grade) {
+
+    let nameToStr = name;
+    let ageNum = Number(age);
+    let gradeNum = Number(grade);
+  console.log(`Name: ${nameToStr}, Age: ${ageNum}, Grade: ${gradeNum.toFixed(2)}`);
+}
+studentsInfo("John", "15", "5.54678");
---
+++ JS-Fundamentals-2023/JS Basic Syntax, Conditional Statements/3.)ExcellentGrade.js
@@ -0,0 +1,14 @@
+//Task: Write a function that receives a single number and checks if the grade is excellent or not. If it is, print "Excellent", otherwise print "Not excellent".
+
+//Solution:
+
+function isExcellent(input){
+
+    if(input >=5.50){
+        console.log("Excellent")
+    }else{
+        console.log("Not excellent")
+    }
+
+}
+isExcellent('5.49')
