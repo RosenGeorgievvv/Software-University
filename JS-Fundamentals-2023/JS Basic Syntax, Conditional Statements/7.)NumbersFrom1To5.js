a6664adbd2841060c67c221a4482ab92cbaf2da9
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-13 20:37:31 +0300

    Task 7 completed

---
+++ JS-Fundamentals-2023/JS Basic Syntax, Conditional Statements/7.)NumbersFrom1To5.js
@@ -0,0 +1,10 @@
+//Task: Write a function that prints all the numbers from 1 to 5 (inclusive) each on a separate line.
+
+//Solution:
+
+ function loop(){
+    for(let i = 1; i <= 5; i++){
+        console.log(i)
+    }
+ }
+ loop();
