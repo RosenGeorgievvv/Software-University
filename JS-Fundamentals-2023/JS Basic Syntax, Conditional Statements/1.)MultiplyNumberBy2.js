f164000c9b5baafdfa8622c88a8182808612764a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-13 12:46:07 +0300

    Update 1.)MultiplyNumberBy2.js

---
+++ JS-Fundamentals-2023/JS Basic Syntax, Conditional Statements/1.)MultiplyNumberBy2.js
@@ -0,0 +1,12 @@
+//Task:
+
+//Write a function that receives a number and prints as result that number multiplied by two.
+
+//Solution:
+
+function multiplyBy2(num){
+
+    console.log(num * 2);
+
+}
+multiplyBy2(["2"]);
