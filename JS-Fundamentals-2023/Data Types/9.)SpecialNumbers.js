39ed2a95af71f8187959dfe66f43d9efd27d3868
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-20 13:45:07 +0300

    Update 9.)SpecialNumbers.js

---
+++ JS-Fundamentals-2023/Data Types/9.)SpecialNumbers.js
@@ -0,0 +1,11 @@
+//Task: Write a program that receives a number n. For all numbers in the range [1…n] print the number and if it is special or not (True / False).
+
+// · A number is special when its sum of digits is 5, 7 or 11.
+
+//Solution:
+
+function specialNumbers(input) {
+
+    
+}
+specialNumbers(15);
