7597900f839ab60eabc69bd77ec87159bab115bc
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-20 13:51:23 +0300

    Update 10.)TriplesOfLatinLetters.js

--- JS-Fundamentals-2023/Data Types/10.)TriplesOfLatinLetters.js
+++ JS-Fundamentals-2023/Data Types/10.)TriplesOfLatinLetters.js
@@ -1,3 +1,9 @@
 //Task: Write a program that receives a string of number n and print all triples of the first n small Latin letters, ordered alphabetically
 
 //Solution:
+
+function triples(str){
+
+
+}
+triples('3');
