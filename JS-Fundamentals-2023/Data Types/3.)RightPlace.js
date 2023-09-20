0ac0e135a0811925aeef27b2f4eeb2874feb38da
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-20 13:01:25 +0300

    Update 3.)RightPlace.js

---
+++ JS-Fundamentals-2023/Data Types/3.)RightPlace.js
@@ -0,0 +1,5 @@
+//Task: You will receive 3 parameters (string, char, string). The first string will be a word with a missing char replaced with an underscore '_'. You have to replace the missing character (underscore) of the first string with the character passed as the second parameter and compare the result with the second string.
+
+// If they are equals, you should print "Matched", otherwise print "Not Matched".
+
+//Solution:
