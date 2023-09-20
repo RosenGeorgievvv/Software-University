a39607d9b86fcef0fedc8611ab6766bc8a0ccf07
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-20 13:54:21 +0300

    Update 10.)TriplesOfLatinLetters.js

--- JS-Fundamentals-2023/Data Types/10.)TriplesOfLatinLetters.js
+++ JS-Fundamentals-2023/Data Types/10.)TriplesOfLatinLetters.js
@@ -3,7 +3,15 @@
 //Solution:
 
 function triples(str){
-
-
+    for(let i = 0; i < str; i++){
+        let firstLetter = String.fromCharCode(i + 97);
+        for(let j = 0; j < str; j++){
+            let secondLetter = String.fromCharCode(j + 97);
+            for(let k = 0; k < str; k++){
+                let thirdLetter = String.fromCharCode(k + 97);
+                console.log(`${firstLetter}${secondLetter}${thirdLetter}`);
+            }
+        }
+    }
 }
 triples('3');
