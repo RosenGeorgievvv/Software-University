b1d1ea47a1c68976cc432b2e206b57f790f70e48
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-20 13:07:55 +0300

    Update 4.)IntegerAndFloat.js

--- JS-Fundamentals-2023/Data Types/4.)IntegerAndFloat.js
+++ JS-Fundamentals-2023/Data Types/4.)IntegerAndFloat.js
@@ -1,3 +1,13 @@
 //Task: You will receive 3 numbers. Your task is to find their sum and print result to the console in the following format: `{sum} - {type of the number (Integer or Float)}`
 
 //Solution:
+
+function intAndFloat(firstNum, secondNum, thirdNum){
+
+    let sum = firstNum + secondNum + thirdNum;
+    
+    let output = sum % 1 === 0 ? sum += " - Integer" : sum += " - Float";
+    console.log(output)
+
+}
+intAndFloat(9, 100, 1.1)
