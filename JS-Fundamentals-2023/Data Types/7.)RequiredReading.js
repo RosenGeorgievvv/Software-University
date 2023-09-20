5be2cea8c958c262ec8440ef6bcc3f5169a0e5e9
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-20 13:36:47 +0300

    Update 7.)RequiredReading.js

---
+++ JS-Fundamentals-2023/Data Types/7.)RequiredReading.js
@@ -0,0 +1,21 @@
+//Task: Write a function to help Ivan calculate how many hours a day he has to spend reading the necessary literature from the list given for the summer vacation.
+
+// As input, you will receive 3 parameters:
+
+// · Number of pages of the current book - integer [1… 1000]
+
+// · Pages read in 1 hour - integer [1… 1000]
+
+// · The number of days for which you must read the book - integer [1… 1000]
+
+// As output print on the console the number of hours, that Ivan has to read each day.
+
+//Solution:
+
+function requiredReading(pages, pageForHour, daysCount) {
+
+
+
+    
+}
+requiredReading(212, 20, 2);
