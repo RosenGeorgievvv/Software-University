1b1fd74aca1d6649fa7de43c2db3a42e9677fe68
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-20 12:53:39 +0300

    Update 1.)EchoType.js

--- JS-Fundamentals-2023/Data Types/1.)EchoType.js
+++ JS-Fundamentals-2023/Data Types/1.)EchoType.js
@@ -1,3 +1,12 @@
+//Task:Write a JS function that takes one parameter and prints on two lines the type of the parameter and then one of the following:
+
+// · If the parameter type is either string or number, print its value
+
+// · Otherwise, print the text 'Parameter is not suitable for printing'
+
+
+//Solution:
+
 function echo(input) {
 
     let param = typeof input;
@@ -8,7 +17,5 @@ function echo(input) {
     }else{
         console.log("Parameter is not suitable for printing")
     }
-
-
 }
 echo("Hello, JavaScript!");
