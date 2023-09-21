74a756eedcf93bb1df6db03eff1b57707e1b381e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-21 14:15:06 +0300

    Update 3.)TownInfo.js

---
+++ JS-Fundamentals-2023/Data Types Exercise/3.)TownInfo.js
@@ -0,0 +1,6 @@
+//Task: You will be given 3 parameters. The first parameter will be the name of the town (string), the second – the population (number), and the third the area (number). Print the result in the following format:
+
+// "Town {town name} has population of {population} and area {area} square km."
+
+
+//Solution:
