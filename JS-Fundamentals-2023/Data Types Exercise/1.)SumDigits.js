eed39770f27dd36c7d1361696e2fae257da72b45
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-21 13:42:41 +0300

    Update 1.)SumDigits.js

--- JS-Fundamentals-2023/Data Types Exercise/1.)SumDigits.js
+++ JS-Fundamentals-2023/Data Types Exercise/1.)SumDigits.js
@@ -1,3 +1,14 @@
 //Task: Write a function, which will be given a single number. Your task is to find the sum of its digits.
 
 //Solution:
+
+function sumDigits(input) {
+  input = input.toString();
+  let sum = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    sum += Number(input[i]);
+  }
+  console.log(sum);
+}
+sumDigits(245678);
