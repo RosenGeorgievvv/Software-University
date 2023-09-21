724f670541ac29fda0c5d114b772b7b8066e9de2
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-21 14:13:55 +0300

    Update 2.)CharsToString.js

--- JS-Fundamentals-2023/Data Types Exercise/2.)CharsToString.js
+++ JS-Fundamentals-2023/Data Types Exercise/2.)CharsToString.js
@@ -1,3 +1,13 @@
 //Task: Write a function, which receives 3 parameters. Each parameter is a single character. Combine all the characters into one string and print it on the console.
 
 //Solution:
+
+function charsToString(char1, char2, char3) {
+
+    let combined = char1 + char2 + char3;
+
+  console.log(combined)
+
+
+}
+charsToString("a","b","c");
