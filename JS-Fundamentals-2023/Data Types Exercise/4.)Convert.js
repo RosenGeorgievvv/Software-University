9418c3c2ef23777b30509a8f631340bac81eff78
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-21 14:19:18 +0300

    Update 4.)Convert.js

--- JS-Fundamentals-2023/Data Types Exercise/4.)Convert.js
+++ JS-Fundamentals-2023/Data Types Exercise/4.)Convert.js
@@ -1,3 +1,11 @@
 //Task: You will be given a number that will be the distance in meters. Write a program that converts meters to kilometers formatted to the second decimal point.2
 
 //Solution:
+
+function convert(num){
+
+    let converted = num / 1000;
+    console.log(converted.toFixed(2));
+    
+}
+convert(1852)
