0876904ceb26c3d80092e1b0012ef3f1ef196397
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-22 15:07:48 +0300

    Update 3.)ReverseNumbers.js

---
+++ JS-Fundamentals-2023/Arrays/3.)ReverseNumbers.js
@@ -0,0 +1,3 @@
+//Task: Write a program, which receives a number n and an array of elements. Your task is to create a new array with n numbers from the original array, reverse it and print its elements on a single line, space-separated
+
+//Solution:
