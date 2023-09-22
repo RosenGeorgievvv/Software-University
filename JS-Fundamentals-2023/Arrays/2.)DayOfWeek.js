8a55b1479643f5921693e9cf7989588d87069041
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-22 15:07:24 +0300

    Update 2.)DayOfWeek.js

---
+++ JS-Fundamentals-2023/Arrays/2.)DayOfWeek.js
@@ -0,0 +1,5 @@
+//Task: Write a program, which receives a number and prints the corresponding name of the day of the week (in English).
+
+// If the number is NOT a valid day, print: "Invalid day!".
+
+//Solution:
