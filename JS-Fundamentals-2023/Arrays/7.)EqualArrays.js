d4580fb929b43495a917bd17255b4e882fe61355
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-22 15:09:18 +0300

    Update 7.)EqualArrays.js

---
+++ JS-Fundamentals-2023/Arrays/7.)EqualArrays.js
@@ -0,0 +1,12 @@
+//Task: Write a program, which receives two string arrays containing number representations, and prints on the console whether they are identical.
+
+// Arrays are identical if their elements at same indexes are equal. If they are identical, find the sum of the first array and print the following message:
+
+// `Arrays are identical. Sum: {sum}`
+
+// If the arrays are NOT identical, find the first index where the arrays differ and print the following message:
+
+// `Arrays are not identical. Found difference at {index} index`
+
+
+//Solution:
