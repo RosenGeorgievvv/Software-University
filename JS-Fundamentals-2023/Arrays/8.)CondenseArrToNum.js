639b3333c26e2e023ad8f70461f7dd61078bf94e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-22 15:09:50 +0300

    Update 8.)CondenseArrToNum.js

---
+++ JS-Fundamentals-2023/Arrays/8.)CondenseArrToNum.js
@@ -0,0 +1,7 @@
+//Task: Write a program, which receives an array of numbers, and condenses them by summing adjacent couples of elements until a single number is obtained.
+
+// Examples
+
+// For example, if we have 3 elements [2, 10, 3], we sum the first two and the second two elements and obtain {2+10, 10+3} = {12, 13}, then we sum again all adjacent elements and obtain {12+13} = {25}.
+
+//Solution:
