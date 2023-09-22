a7e5f8c572d07b72c7549274245c91ec4ab54ef1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-22 15:15:17 +0300

    Update 1.)SumFirstLastElement.js

--- JS-Fundamentals-2023/Arrays/1.)SumFirstLastElement.js
+++ JS-Fundamentals-2023/Arrays/1.)SumFirstLastElement.js
@@ -1,3 +1,9 @@
 //Task: Write a function that receives an array of numbers and prints the sum of the first and last element in that array.
 
 //Solution:
+
+function sumElements(input){
+
+
+}
+sumElements([20, 30, 40]);
