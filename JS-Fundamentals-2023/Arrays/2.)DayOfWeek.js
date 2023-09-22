b3b48f7cc7a858410570ea215f897ca69833f50b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-22 15:21:10 +0300

    Update 2.)DayOfWeek.js

--- JS-Fundamentals-2023/Arrays/2.)DayOfWeek.js
+++ JS-Fundamentals-2023/Arrays/2.)DayOfWeek.js
@@ -3,3 +3,22 @@
 // If the number is NOT a valid day, print: "Invalid day!".
 
 //Solution:
+
+function dayOfWeek(input) {
+  let variants = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  if (input >= 1 && input <= 7) {
+    console.log(variants[input - 1]);
+  } else {
+    console.log("Invalid day!");
+  }
+}
+dayOfWeek(3);
