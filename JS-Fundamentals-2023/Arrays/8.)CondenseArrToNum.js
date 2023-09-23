2a2074dd522ac01eeebfee6089b40c73514de309
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-23 13:07:01 +0300

    Update 8.)CondenseArrToNum.js

--- JS-Fundamentals-2023/Arrays/8.)CondenseArrToNum.js
+++ JS-Fundamentals-2023/Arrays/8.)CondenseArrToNum.js
@@ -5,3 +5,15 @@
 // For example, if we have 3 elements [2, 10, 3], we sum the first two and the second two elements and obtain {2+10, 10+3} = {12, 13}, then we sum again all adjacent elements and obtain {12+13} = {25}.
 
 //Solution:
+
+function condense(arr) {
+  while (arr.length > 1) {
+    let condensed = Array(arr.length - 1);
+    for (let i = 0; i < arr.length - 1; i++) {
+      condensed[i] = Number(arr[i]) + Number(arr[i + 1]);
+    }
+    arr = condensed;
+  }
+  console.log(arr[0]);
+}
+condense([2, 10, 3]);
