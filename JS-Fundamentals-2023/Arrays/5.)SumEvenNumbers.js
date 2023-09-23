1ace8319c959d44b8485d16ff169a82f83f0d820
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-23 11:43:14 +0300

    Update 5.)SumEvenNumbers.js

--- JS-Fundamentals-2023/Arrays/5.)SumEvenNumbers.js
+++ JS-Fundamentals-2023/Arrays/5.)SumEvenNumbers.js
@@ -1,3 +1,18 @@
 //Task: Write a program, which receives an array of strings, parse them into numbers, and sum only the even numbers.
 
 //Solution:
+
+function sumEvenNumbers(arr) {
+ for(let i = 0; i < arr.length; i++){
+    arr[i] = Number(arr[i]);
+ }
+ let sum = 0;
+ for(let num of arr){
+    if(num % 2 == 0){
+        sum += num;
+    }
+
+ }
+ console.log(sum)
+}
+sumEvenNumbers(["1", "2", "3", "4", "5", "6"]);
