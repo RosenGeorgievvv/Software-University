fa9d26266938ced852359a39295157a2b694d1c3
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-23 13:04:54 +0300

    Update 7.)EqualArrays.js

--- JS-Fundamentals-2023/Arrays/7.)EqualArrays.js
+++ JS-Fundamentals-2023/Arrays/7.)EqualArrays.js
@@ -8,5 +8,22 @@
 
 // `Arrays are not identical. Found difference at {index} index`
 
-
 //Solution:
+
+function equalArr(arr1, arr2) {
+  let sum = 0;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`Arrays are not identical. Found difference at ${i} index`);
+      return;
+    }
+    sum += Number(arr1[i]);
+  }
+  console.log(`Arrays are identical. Sum: ${sum}`);
+}
+equalArr(
+  ["10", "20", "30"],
+
+  ["10", "20", "30"]
+);
