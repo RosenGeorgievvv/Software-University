869652a66495260a0c6e858f44f4628b01c82334
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-25 11:40:19 +0300

    update

---
+++ JS-Fundamentals-2023/Arrays Exercise/3.)MergeArrays.js
@@ -0,0 +1,16 @@
+//Task: Write a function, which receives two string arrays and merges them into a third array.
+
+// · If the index of the element is even, add into the third array the sum of both elements at that index
+
+// · If the index of the element is odd, add the concatenation of both elements at that index
+
+// Input
+
+// As input, you will receive two string arrays (with equal length).
+
+// Output
+
+// As output, you should print the resulting third array, each element separated by " - ".
+
+
+//Solution:
---
+++ JS-Fundamentals-2023/Arrays Exercise/4.)ArrayRotation.js
@@ -0,0 +1,10 @@
+//Task: Write a function that receives an array and the number of rotations you have to perform.
+
+// Note: Depending on the number of rotations, the first element goes to the end.
+
+// Output
+
+// Print the resulting array elements separated by a single space.
+
+
+//Solution:
