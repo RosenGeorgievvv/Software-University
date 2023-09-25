a231dd2eaf5a2894101d9ca789a85d49d5b23671
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-25 11:49:22 +0300

    Update 1.)AddSubstract.js

--- JS-Fundamentals-2023/Arrays Exercise/1.)AddSubstract.js
+++ JS-Fundamentals-2023/Arrays Exercise/1.)AddSubstract.js
@@ -8,5 +8,28 @@
 
 // On the first line print the newly modified array, on the second line print the sum of numbers from the original array, on the third line print the sum of numbers from the modified array.
 
-
 //Solution:
+
+function addSubstract(arr) {
+  let modifiedArray = [];
+  let originalSum = 0;
+  let modifiedSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    originalSum += arr[i];
+
+    let modifiedNum = 0;
+    if (arr[i] % 2 == 0) {
+      modifiedNum = arr[i] + i;
+    } else {
+      modifiedNum = arr[i] - i;
+    }
+
+    modifiedArray.push(modifiedNum);
+    modifiedSum += modifiedNum;
+  }
+  console.log(modifiedArray);
+  console.log(originalSum);
+  console.log(modifiedSum);
+}
+addSubstract([5, 15, 23, 56, 35]);
