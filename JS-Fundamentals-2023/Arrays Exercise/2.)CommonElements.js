76f4aa6e6ee9ae5bbdff5826d73c5f13e4024001
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-25 11:54:41 +0300

    Update 2.)CommonElements.js

--- JS-Fundamentals-2023/Arrays Exercise/2.)CommonElements.js
+++ JS-Fundamentals-2023/Arrays Exercise/2.)CommonElements.js
@@ -1,3 +1,27 @@
 //Task: Write a function, which prints common elements in two string arrays. Print all matches on separate lines. Compare the first array with the second array.
 
 //Solution:
+
+function commonElements(arr1, arr2) {
+
+    let newArr = []
+    for(let i = 0; i < arr1.length; i++){
+        for(let k = 0; k < arr2.length; k++){
+            if(arr1[i] === arr2[k]){
+                newArr.push(arr1[i]);
+                
+            }
+        }
+       
+    }
+    console.log(newArr.join('\n'))
+    
+
+
+
+}
+commonElements(
+  ["Hey", "hello", 2, 4, "Peter", "e"],
+
+  ["Petar", 10, "hey", 4, "hello", "2"]
+);
