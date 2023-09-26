eb786f50fc0439d062d17bb4f5cdb4d266784825
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-26 11:56:28 +0300

    Update 7.)MaxSeqOfEqualElements.js

--- JS-Fundamentals-2023/Arrays Exercise/7.)MaxSeqOfEqualElements.js
+++ JS-Fundamentals-2023/Arrays Exercise/7.)MaxSeqOfEqualElements.js
@@ -3,3 +3,24 @@
 // If several longest sequences exist, print the leftmost one.
 
 //Solution:
+
+function maxSeq(arr) {
+  let maxLength = 0;
+  let currLength = 1;
+  let longestSeq = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1]) {
+      currLength++;
+    } else {
+      currLength = 1;
+    }
+
+    if (currLength > maxLength) {
+      maxLength = currLength;
+      longestSeq = i - maxLength + 1;
+    }
+  }
+  console.log(arr.slice(longestSeq, longestSeq + maxLength).join(" "));
+}
+maxSeq([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
