e765da2cf8288c344dbe010fcba572cec2247a2e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-29 17:54:50 +0300

    Update 8.)MagicSum.js

--- JS-Fundamentals-2023/Arrays Exercise/8.)MagicSum.js
+++ JS-Fundamentals-2023/Arrays Exercise/8.)MagicSum.js
@@ -2,15 +2,13 @@
 
 //Solution:
 function magicSum(arr, magic) {
-
-for(let i = 0; i < arr.length - 1; i++){
-    for(let k = i + 1; k < arr.length; k++){
-        if(Number(arr[i]) + Number(arr[k]) === Number(magic)){
-            console.log(arr[i] + ` ` + arr[k])
-
-        }
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let k = i + 1; k < arr.length; k++) {
+      if (Number(arr[i]) + Number(arr[k]) === Number(magic)) {
+        console.log(arr[i] + ` ` + arr[k]);
+      }
     }
+  }
 }
 
-}
-magicSum([1, 7, 6, 2, 19, 23],8);
+magicSum([1, 7, 6, 2, 19, 23], 8);
