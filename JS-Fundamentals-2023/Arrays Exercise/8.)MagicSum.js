c4f3d4ef2c274bd02d8980eabd4e1bd574251d6c
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-09-29 10:22:46 +0300

    Update 8.)MagicSum.js

--- JS-Fundamentals-2023/Arrays Exercise/8.)MagicSum.js
+++ JS-Fundamentals-2023/Arrays Exercise/8.)MagicSum.js
@@ -1,3 +1,16 @@
 //Task: Write a function, which prints all unique pairs in an array of integers whose sum is equal to a given number.
 
 //Solution:
+function magicSum(arr, magic) {
+
+for(let i = 0; i < arr.length - 1; i++){
+    for(let k = i + 1; k < arr.length; k++){
+        if(Number(arr[i]) + Number(arr[k]) === Number(magic)){
+            console.log(arr[i] + ` ` + arr[k])
+
+        }
+    }
+}
+
+}
+magicSum([1, 7, 6, 2, 19, 23],8);
