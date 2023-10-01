f1b4c87fdabde80f0f94e2d1e9a43d893436ecf3
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-01 14:26:01 +0300

    Update 2.)MathPower.js

--- JS-Fundamentals-2023/Functions/2.)MathPower.js
+++ JS-Fundamentals-2023/Functions/2.)MathPower.js
@@ -1,3 +1,13 @@
 //Task: Write a function that calculates and print the value of a number raised to a given power:
 
 //Solution:
+
+function mathPower(number, power) {
+  let start = 1;
+
+  for (let i = 0; i < power; i++) {
+    start *= number;
+  }
+  console.log(start);
+}
+mathPower(2, 8);
