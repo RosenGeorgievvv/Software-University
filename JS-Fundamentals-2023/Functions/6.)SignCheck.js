99598dbc81dc3e971c7e1dbdc3b72b004d970448
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-01 14:32:40 +0300

    Update 6.)SignCheck.js

---
+++ JS-Fundamentals-2023/Functions/6.)SignCheck.js
@@ -0,0 +1,11 @@
+//Task: Write a function, that checks whether the result of the multiplication numOne * numTwo * numThree is positive or negative. Try to do this WITHOUT multiplying the 3 numbers.
+
+// Input
+
+// The input comes as parameters named numOne, numTwo, numThree.
+
+// Output
+
+// · If the result is positive, print on the console -> "Positive"
+
+// · Otherwise, print -> "Negative"
