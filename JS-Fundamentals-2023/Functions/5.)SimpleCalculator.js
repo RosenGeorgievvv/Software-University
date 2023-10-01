dc6385e11a7c6b044c70c9bac262c4fad4015b32
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-01 14:32:07 +0300

    Update 5.)SimpleCalculator.js

---
+++ JS-Fundamentals-2023/Functions/5.)SimpleCalculator.js
@@ -0,0 +1,9 @@
+//Task:Write a function that receives three parameters – two numbers and an operator (string) – and calculates the result depending on the operator. Operator can be 'multiply', 'divide', 'add' or 'subtract'. Try to solve this task using arrow functions.
+
+// Bonus
+
+// Solve this task without using any conditional statements (no if or switch statements or ternary operators).
+
+// Input
+
+// The input comes as parameters named numOne, numTwo, operator.
