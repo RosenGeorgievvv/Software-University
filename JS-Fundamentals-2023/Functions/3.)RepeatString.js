738114e85dc97c349d6238b9f9f50d1c224f187e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-01 14:29:20 +0300

    Update 3.)RepeatString.js

--- JS-Fundamentals-2023/Functions/3.)RepeatString.js
+++ JS-Fundamentals-2023/Functions/3.)RepeatString.js
@@ -1,3 +1,12 @@
 //Task:Write a function that receives a string and a repeat count n. The function should return a new string (the old one repeated n times).
 
 //Solution:
+
+function repeatString(str, count) {
+  let newStr = "";
+  for (let i = 0; i < count; i++) {
+    newStr += str;
+  }
+  return newStr;
+}
+console.log(repeatString("abc", 3));
