a403a9bbc2c48d26b31dbc52a55346456ffd8315
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-01 14:30:42 +0300

    Update 4.)Orders.js

---
+++ JS-Fundamentals-2023/Functions/4.)Orders.js
@@ -0,0 +1,14 @@
+//Task: Write a function that calculates the total price of an order and prints it on the console. The function should receive one of the following products: coffee, coke, water, snacks; and a quantity of the product. The prices for a single piece of each product are:
+
+// · coffee - 1.50
+
+// · water - 1.00
+
+// · coke - 1.40
+
+// · snacks - 2.00
+
+// Print the result formatted to the second decimal place
+
+
+//Solution: 
