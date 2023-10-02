3affd1d8ac4fe6477cd4715c59d8f7dd52534aea
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-02 12:10:42 +0300

    Update 5.)SimpleCalculator.js

--- JS-Fundamentals-2023/Functions/5.)SimpleCalculator.js
+++ JS-Fundamentals-2023/Functions/5.)SimpleCalculator.js
@@ -9,3 +9,27 @@
 // The input comes as parameters named numOne, numTwo, operator.
 
 //Solution:
+
+function calc(a, b, operator) {
+  switch (operator) {
+    case "multiply":
+      let multiply = (a, b) => a * b;
+      console.log(multiply(a, b));
+      break;
+    case "divide":
+      let divide = (a, b) => a / b;
+      console.log(divide(a, b));
+      break;
+    case "add":
+      let add =(a, b) => a + b;
+      console.log(add(a, b));
+      break;
+    case "subtract":
+      let subtract = (a, b) => a - b;
+      console.log(subtract(a, b));
+      break;
+    default:
+      return;
+  }
+}
+calc(5, 5, "multiply");
