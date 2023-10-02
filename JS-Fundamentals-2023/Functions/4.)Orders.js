3504a9fd119de509f8fb03ac215e0f5724fe3a47
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-02 11:59:45 +0300

    Update 4.)Orders.js

--- JS-Fundamentals-2023/Functions/4.)Orders.js
+++ JS-Fundamentals-2023/Functions/4.)Orders.js
@@ -12,3 +12,27 @@
 
 
 //Solution: 
+
+function orders(product, quantity) {
+    let price = 0;
+
+    switch (product) {
+        case 'coffee':
+            price = 1.50;
+            break;
+        case 'water':
+            price = 1.00;
+            break;
+        case 'coke':
+            price = 1.40;
+            break;
+        case 'snacks':
+            price = 2.00;
+            break;
+        default:
+            return;
+    }
+    let totalPrice = price * quantity;
+    console.log(totalPrice.toFixed(2));
+}
+orders("water", 5)
