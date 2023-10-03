561b8c6fda445291b291a1c21a7a7de2641d41df
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-03 12:07:54 +0300

    Update 1.)SmallestOfThree.js

--- JS-Fundamentals-2023/Functions Exercise/1.)SmallestOfThree.js
+++ JS-Fundamentals-2023/Functions Exercise/1.)SmallestOfThree.js
@@ -1,3 +1,10 @@
 //Task: Write a function that receives three integers and prints the smallest number. Use an appropriate name for the function.
 
 //Solution:
+
+function smallestNum(num1,num2,num3){
+
+    let smallest = Math.min(num1,num2,num3);
+    console.log(smallest);
+}
+smallestNum(2,5,3)
