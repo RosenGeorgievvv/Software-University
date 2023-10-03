5c302f2a490571147b4fddb6eb25e1eb30170d5e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-03 08:51:49 +0300

    Update 3.)CharactersInRange.js

---
+++ JS-Fundamentals-2023/Functions Exercise/3.)CharactersInRange.js
@@ -0,0 +1,3 @@
+//Task: Write a function that receives two characters and prints on a single line all the characters in between them according to the ASCII code. Keep in mind that the second character code might be before the first one inside the ASCII table.
+
+//Solution:
