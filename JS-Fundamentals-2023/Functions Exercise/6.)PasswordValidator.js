321ff541a45f7f5baa3526e623ed6a187eb39c90
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-03 08:54:37 +0300

    Update 6.)PasswordValidator.js

---
+++ JS-Fundamentals-2023/Functions Exercise/6.)PasswordValidator.js
@@ -0,0 +1,20 @@
+//Task: Write a function that checks if a given password is valid. Password validations are:
+
+// · The length should be 6 - 10 characters (inclusive)
+
+// · It should consist only of letters and digits
+
+// · It should have at least 2 digits
+
+// If a password is a valid print: "Password is valid".
+
+// If it is NOT valid, for every unfulfilled rule print a message:
+
+// · "Password must be between 6 and 10 characters"
+
+// · "Password must consist only of letters and digits"
+
+// · "Password must have at least 2 digits"
+
+
+//Solution:
