2d9a0f0742e72aedfc80168b87629d3325b3e08b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-04 12:28:40 +0300

    update

--- JS-Fundamentals-2023/Functions Exercise/5.)PalindromeInt.js
+++ JS-Fundamentals-2023/Functions Exercise/5.)PalindromeInt.js
@@ -1,3 +1,12 @@
 //Task: A palindrome is a number, which reads the same backward as forward, such as 323 or 1001. Write a function, which receives an array of positive integers and checks if each integer is a palindrome or not.
 
 //Solution:
+
+function pali(arr){
+
+let toStr = arr.toString(" ");
+console.log(toStr)
+
+
+}
+pali([123,323,421,121]);
