87809ded8a24be5bdfd5e67a6067f96131ac8f85
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-04 12:33:02 +0300

    Update 4.)LastKSeq.js

---
+++ JS-Fundamentals-2023/ArrayAdvanced/4.)LastKSeq.js
@@ -0,0 +1,14 @@
+//Task: You are given two integers n and k. Write a function that generates and prints the following sequence:
+
+// · The first element is 1.
+
+// · Every following element equals the sum of the previous k elements.
+
+// · The length of the sequence is n elements.
+
+// The input comes as two number arguments. The first element represents the number n, and the second – the number k.
+
+// The output is printed on the console on a single line, separated by space.
+
+
+//Solution:
