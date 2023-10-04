5272e0cfd73898fcbabe0d79e9a54f5ce47c4582
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-04 12:31:38 +0300

    Update 2.)NegativePositiveNum.js

---
+++ JS-Fundamentals-2023/ArrayAdvanced/2.)NegativePositiveNum.js
@@ -0,0 +1,7 @@
+//Task: Write a function that processes the elements in an array one by one and produces a new array. Prepend each negative element at the front of the array (as the first element) and append each positive (or 0) element at the end of the array.
+
+// The input comes as an array of string elements holding numbers.
+
+// The output is printed on the console, each element on a new line.
+
+//Solution:
