fd35bfad0748b71d76d8d50a633195bc3cb2f2b0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-04 12:33:24 +0300

    Update 5.)ProcessOdd.js

---
+++ JS-Fundamentals-2023/ArrayAdvanced/5.)ProcessOdd.js
@@ -0,0 +1,7 @@
+//Task: You are given an array of numbers. Write a function that prints the elements at odd positions from the array, doubled and in reverse order.
+
+// The input comes as an array of number elements.
+
+// The output is printed on the console on a single line, separated by space.
+
+//Solution:
