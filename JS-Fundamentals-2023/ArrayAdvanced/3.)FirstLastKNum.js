36b62d9a390dbe54e85874439e9dcd494cae2848
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-04 12:32:18 +0300

    Update 3.)FirstLastKNum.js

---
+++ JS-Fundamentals-2023/ArrayAdvanced/3.)FirstLastKNum.js
@@ -0,0 +1,8 @@
+//Task: Write a function that prints the first k and the last k elements from an array of numbers.
+
+// The input comes as an array of number elements. The first element represents the number k, all other elements are from the array that needs to be processed.
+
+// The output is printed on the console on two lines. On the first line, print the first k elements, separated by space. On the second line, print the last k elements, separated by space.
+
+
+//Solution:
