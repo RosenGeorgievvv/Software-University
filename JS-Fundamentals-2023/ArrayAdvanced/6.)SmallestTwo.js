f7a25086d1149fbc6ed21256c2dbfa47a67e7d9b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-04 12:33:54 +0300

    Update 6.)SmallestTwo.js

---
+++ JS-Fundamentals-2023/ArrayAdvanced/6.)SmallestTwo.js
@@ -0,0 +1,8 @@
+//Task: Write a function that prints the two smallest elements from an array of numbers.
+
+// The input comes as an array of number elements.
+
+// The output is printed on the console on a single line, separated by space.
+
+
+//Solution:
