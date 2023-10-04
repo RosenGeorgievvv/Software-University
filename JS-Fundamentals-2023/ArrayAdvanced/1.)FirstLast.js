51bea9fbbec88ea5260d234eb4b6fec5df9b35b5
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-04 12:31:02 +0300

    Update 1.)FirstLast.js

---
+++ JS-Fundamentals-2023/ArrayAdvanced/1.)FirstLast.js
@@ -0,0 +1,8 @@
+//Task:Write a function that calculates and prints the sum of the first and the last elements in an array.
+
+// The input comes as an array of string elements holding numbers.
+
+// The output is printed on the console.
+
+
+//Solution:
