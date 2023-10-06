b494f3a9d70f344d4ef9bf3e71d95ab3de154b0a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-06 21:19:09 +0300

    update

--- JS-Fundamentals-2023/Functions Exercise/10.)FactorialDivision.js
+++ JS-Fundamentals-2023/Functions Exercise/10.)FactorialDivision.js
@@ -1,3 +1,22 @@
 //Task: Write a function that receives two integer numbers. Calculate the factorial of each number. Divide the first result by the second and print the division formatted to the second decimal point.
 
 //Solution:
+
+function factorialDivision(num1, num2) {
+  let factorial1 = calcFactorial(num1);
+  let factorial2 = calcFactorial(num2);
+
+  let result = factorial1 / factorial2;
+  console.log(result.toFixed(2));
+  
+  function calcFactorial(num) {
+    let factorial = 1;
+
+    while (num > 1) {
+      factorial *= num;
+      num--;
+    }
+    return factorial;
+  }
+}
+factorialDivision(5, 2);
--- JS-Fundamentals-2023/Functions Exercise/8.)PerfectNumber.js
+++ JS-Fundamentals-2023/Functions Exercise/8.)PerfectNumber.js
@@ -2,5 +2,20 @@
 
 // A perfect number is a positive integer that is equal to the sum of its proper positive divisors. That is the sum of its positive divisors excluding the number itself (also known as its aliquot sum).
 
-
 //Solution:
+
+function perfectNum(num) {
+  let sumProper = 0;
+
+  for (let i = 1; i < num; i++) {
+    if (num % i == 0) {
+      sumProper += i;
+    }
+  }
+  if (sumProper == num) {
+    console.log("We have a perfect number!");
+  } else {
+    console.log("It's not so perfect.");
+  }
+}
+perfectNum(6);
