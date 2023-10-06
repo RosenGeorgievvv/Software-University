37a2a5d8412c3a7dfdf78216328a1e79442dae70
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-06 17:59:15 +0300

    Update 7.)SearchNumber.js

---
+++ JS-Fundamentals-2023/ArrayAdvanced Exercise/7.)SearchNumber.js
@@ -0,0 +1,13 @@
+//Task: You will receive two arrays of integers. The second array is containing exactly three numbers.
+
+// The first number represents the number of elements you have to take from the first array (starting from the first one).
+
+// The second number represents the number of elements you have to delete from the numbers you took (starting from the first one).
+
+// The third number is the number we search in our collection after the manipulations.
+
+// As output print how many times that number occurs in our array in the following format:
+
+// "Number {number} occurs {count} times."
+
+//Solution:
