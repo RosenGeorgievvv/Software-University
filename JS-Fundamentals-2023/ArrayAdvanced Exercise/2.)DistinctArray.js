85751c6eb0490410a2a8430ff85b4183084713f9
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-06 17:56:58 +0300

    Update 2.)DistinctArray.js

---
+++ JS-Fundamentals-2023/ArrayAdvanced Exercise/2.)DistinctArray.js
@@ -0,0 +1,8 @@
+//Task: You will be given an array of integer numbers on the first line of the input.
+
+// Remove all repeating elements from the array.
+
+// Print the result elements separated by a single space.
+
+
+//Solution:
