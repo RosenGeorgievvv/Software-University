e664e6c4b052f4d1b883bbe58e3efeb4111beba0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-06 17:56:35 +0300

    Update 1.)Train.js

---
+++ JS-Fundamentals-2023/ArrayAdvanced Exercise/1.)Train.js
@@ -0,0 +1,16 @@
+//Тask:You will be given an array of strings.
+
+// The first element will be a string containing wagons (numbers). Each number inside the string represents the number of passengers that are currently in a wagon.
+
+// The second element in the array will be the max capacity of each wagon (single number).
+
+// The rest of the elements will be commands in the following format:
+
+// · Add {passengers} – add a wagon to the end with the given number of passengers.
+
+// · {passengers} - find an existing wagon to fit all the passengers (starting from the first wagon)
+
+// At the end, print the final state of the train (all the wagons separated by a space).
+
+
+//Solution:
