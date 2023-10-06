51856bc6479366cbe45641763454483489e763a6
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-06 17:57:52 +0300

    Update 4.)Sorting.js

---
+++ JS-Fundamentals-2023/ArrayAdvanced Exercise/4.)Sorting.js
@@ -0,0 +1,5 @@
+//Task: Write a function that sorts an array of numbers so that the first element is the biggest one, the second is the smallest one, the third is the second biggest one, and the fourth is the second smallest one, and so on.
+
+// Print the elements on one row, separated by a single space.
+
+//Solution:
