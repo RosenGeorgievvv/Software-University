afdedbc7c62f5a293c8b5f4507709476acba4d6a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-06 17:58:50 +0300

    Update 6.)BombNumbers.js

---
+++ JS-Fundamentals-2023/ArrayAdvanced Exercise/6.)BombNumbers.js
@@ -0,0 +1,9 @@
+//Task:Write a function that receives two parameters: sequence of numbers and special bomb number with a certain power.
+
+// Your task is to detonate every occurrence of the special bomb number and according to its power his neighbors from left and right. Detonations are performed from left to right and all detonated numbers disappear.
+
+// The input contains two arrays of numbers. The first contains the initial sequence and the second contains the special bomb number and its power.
+
+// The output is the sum of the remaining elements in the sequence.
+
+//Solution:
