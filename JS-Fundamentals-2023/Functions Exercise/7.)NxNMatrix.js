4283f13b98184e09eb74ff06477e7a0dd8344fc2
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-06 20:53:23 +0300

    Update 7.)NxNMatrix.js

--- JS-Fundamentals-2023/Functions Exercise/7.)NxNMatrix.js
+++ JS-Fundamentals-2023/Functions Exercise/7.)NxNMatrix.js
@@ -1,3 +1,11 @@
 //Task: Write a function that receives a single integer number n and prints nxn matrix with that number.
 
 //Solution:
+
+function nxnMatrix(n) {
+  for (let rowNum = 1; rowNum <= n; rowNum++) {
+    let curRow = `${n} `.repeat(n);
+    console.log(curRow)
+  }
+}
+nxnMatrix(3);
