166182d6b85c91bd5b2962ed3373415c8fe1a9e9
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-06 17:58:23 +0300

    Update 5.)SortBy2Crit.js

---
+++ JS-Fundamentals-2023/ArrayAdvanced Exercise/5.)SortBy2Crit.js
@@ -0,0 +1,7 @@
+//Task: Write a function that orders an array of strings, by their length in ascending order as primary criteria, and by alphabetical value in ascending order as second criteria. The comparison should be case-insensitive.
+
+// The input comes as an array of strings.
+
+// The output is the ordered array of strings, each on a separate line.
+
+//Solution:
