2536d99eed0ae9b6ab7628674ff3e31da7cddb43
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-06 18:43:59 +0300

    Update 2.)AddSubtract.js

--- JS-Fundamentals-2023/Functions Exercise/2.)AddSubtract.js
+++ JS-Fundamentals-2023/Functions Exercise/2.)AddSubtract.js
@@ -4,3 +4,17 @@
 
 //Solution:
 
+function addAndSubtract(num1,num2,num3){
+
+let result1 = sum(num1, num2);
+let result2= subtract(result1, num3)
+    function sum(num1,num2){
+        return num1 + num2;
+    }
+
+    function subtract(num1,num2){
+       return num1 - num2;
+    }
+console.log(result2)
+}
+addAndSubtract(23,6,10);
