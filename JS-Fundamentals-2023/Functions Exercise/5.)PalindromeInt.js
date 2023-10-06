37d5fa58eb42835159014a7cc4c6ced2b661877d
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-06 20:14:00 +0300

    Update 5.)PalindromeInt.js

--- JS-Fundamentals-2023/Functions Exercise/5.)PalindromeInt.js
+++ JS-Fundamentals-2023/Functions Exercise/5.)PalindromeInt.js
@@ -2,10 +2,20 @@
 
 //Solution:
 
-function pali(arr){
+function pali(arr) {
 
+    
 
+  function checkPalindrome(num) {
+    let numAsString = String(num);
+    let reversedNumStr = "";
 
-
+    for(let i = numAsString.length - 1; i >= 0; i--){
+        let curChar = numAsString[i];
+        reversedNumStr += curChar;
+    }
+    let isPalindrome = numAsString == reversedNumStr;
+    return isPalindrome;
+  }
 }
-pali([123,323,421,121]);
+pali([123, 323, 421, 121]);
