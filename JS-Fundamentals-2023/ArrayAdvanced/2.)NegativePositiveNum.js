ef68717075b73a25e40894dd173eacb2e0bd46d0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-07 12:26:24 +0300

    Update 2.)NegativePositiveNum.js

--- JS-Fundamentals-2023/ArrayAdvanced/2.)NegativePositiveNum.js
+++ JS-Fundamentals-2023/ArrayAdvanced/2.)NegativePositiveNum.js
@@ -8,7 +8,14 @@
 
 function fromNegativeToPositive(arr){
 
-
-
+let newArr = [];
+for(let num of arr){
+    if(num < 0){
+        newArr.unshift(num);
+    }else{
+        newArr.push(num);
+    }
+}
+console.log(newArr.join('\n'))
 }
 fromNegativeToPositive(['7', '-2', '8', '9']);
