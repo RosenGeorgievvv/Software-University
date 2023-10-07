00c903b4c24e6fecb1ec15bf7cf6b298c9b6e57e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-07 08:42:09 +0300

    Update 9.)LoadingBar.js

--- JS-Fundamentals-2023/Functions Exercise/9.)LoadingBar.js
+++ JS-Fundamentals-2023/Functions Exercise/9.)LoadingBar.js
@@ -3,3 +3,17 @@
 // Your task is to create a function that visualizes a loading bar depending on the number you have received in the input.
 
 //Solution:
+
+function loadingBar(num) {
+  let percentage = "%".repeat(num / 10);
+  let marks = ".".repeat(10 - num / 10);
+
+  if (num === 100) {
+    console.log(`${num}% Complete!`);
+    console.log(`[${percentage}]`);
+  } else {
+    console.log(`${num}% [${percentage}${marks}]`);
+    console.log(`Still loading...`);
+  }
+}
+loadingBar(30);
