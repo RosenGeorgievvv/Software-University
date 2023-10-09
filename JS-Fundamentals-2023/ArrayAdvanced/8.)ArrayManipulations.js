aec8886834bcd71c812a48e0cdbf1e055752d28d
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-09 14:45:45 +0300

    Update 8.)ArrayManipulations.js

--- JS-Fundamentals-2023/ArrayAdvanced/8.)ArrayManipulations.js
+++ JS-Fundamentals-2023/ArrayAdvanced/8.)ArrayManipulations.js
@@ -14,6 +14,17 @@
 
 // The output is the manipulated array printed on the console on a single line, separated by space.
 
-
-
 //Solution:
+
+function arrManilupations(arr) {
+
+
+    
+}
+arrManilupations([
+  "4 19 2 53 6 43",
+  "Add 3",
+  "Remove 2",
+  "RemoveAt 1",
+  "Insert 8 3",
+]);
