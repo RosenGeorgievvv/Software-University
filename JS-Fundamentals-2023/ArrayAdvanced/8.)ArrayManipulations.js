a05184d0724d64e3699877dca6f9a20a5980ab31
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-09 14:56:25 +0300

    Update 8.)ArrayManipulations.js

--- JS-Fundamentals-2023/ArrayAdvanced/8.)ArrayManipulations.js
+++ JS-Fundamentals-2023/ArrayAdvanced/8.)ArrayManipulations.js
@@ -17,38 +17,45 @@
 //Solution:
 
 function arrManipulations(arr) {
+  let arrToNum = arr.shift().split(" ").map(Number);
 
-    let arrToNum = arr.shift().split(' ').map(Number);
+  for (let i = 0; i < arr.length; i++) {
+    let [command, firstNum, secondNum] = arr[i].split(" ");
+    firstNum = Number(firstNum);
+    secondNum = Number(secondNum);
 
-    for(let i = 0; i < arr.length; i++){
-        let [command, firstNum, secondNum] = arr[i].split(' ');
-        firstNum = Number(firstNum);
-        secondNum = Number(secondNum);
-
-        switch(command){
-            case "Add": add(firstNum); break;
-            case "Remove": remove(firstNum); break;
-            case "RemoveAt": removeAt(firstNum); break;
-            case "Insert": insert(firstNum, secondNum); break;
-        }
+    switch (command) {
+      case "Add":
+        add(firstNum);
+        break;
+      case "Remove":
+        remove(firstNum);
+        break;
+      case "RemoveAt":
+        removeAt(firstNum);
+        break;
+      case "Insert":
+        insert(firstNum, secondNum);
+        break;
     }
+  }
 
-    function add(el){
-        arrToNum.push(el);
-    }
+  function add(el) {
+    arrToNum.push(el);
+  }
 
-    function remove(num){
-        arrToNum = arrToNum.filter(el => el !== num);
-    }
+  function remove(num) {
+    arrToNum = arrToNum.filter((el) => el !== num);
+  }
 
-    function removeAt(index){
-        arrToNum.splice(index, 1);
-    }
+  function removeAt(index) {
+    arrToNum.splice(index, 1);
+  }
 
-    function insert(num, index){
-        arrToNum.splice(index, 0, num);
-    }
-    console.log(arrToNum.join(' '))
+  function insert(num, index) {
+    arrToNum.splice(index, 0, num);
+  }
+  console.log(arrToNum.join(" "));
 }
 arrManipulations([
   "4 19 2 53 6 43",
