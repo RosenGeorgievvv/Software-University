d305214c149a55115cee7732a9160c11b57c9ddd
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-09 14:34:08 +0300

    Update 6.)SmallestTwo.js

--- JS-Fundamentals-2023/ArrayAdvanced/6.)SmallestTwo.js
+++ JS-Fundamentals-2023/ArrayAdvanced/6.)SmallestTwo.js
@@ -4,13 +4,14 @@
 
 // The output is printed on the console on a single line, separated by space.
 
-
 //Solution:
 
-
-function smallestTwoNums(arr){
-
-let sortedArr = arr.sort((a, b) => a - b).slice(0, 2).join(' ');
-console.log(sortedArr);
+function smallestTwoNums(arr) {
+  let sortedArr = arr
+    .sort((a, b) => a - b)
+    .slice(0, 2)
+    .join(" ");
+  console.log(sortedArr);
+  
 }
 smallestTwoNums([30, 15, 50, 5]);
