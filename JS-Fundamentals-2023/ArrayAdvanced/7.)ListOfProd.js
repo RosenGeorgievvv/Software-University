a0349e0d3dfc4efffc663de42661017d8f6c7030
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-09 14:43:52 +0300

    Update 7.)ListOfProd.js

--- JS-Fundamentals-2023/ArrayAdvanced/7.)ListOfProd.js
+++ JS-Fundamentals-2023/ArrayAdvanced/7.)ListOfProd.js
@@ -8,6 +8,5 @@ function productsList(arr) {
     for(let i = 0; i < sortedArr.length; i++){
         console.log(`${i + 1}.${sortedArr[i]}`);
     }
-
 }
 productsList(["Potatoes", "Tomatoes", "Onions", "Apples"]);
