6f7381eaa0c14f91210ed988efe6d26c873f3b70
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-09 14:35:07 +0300

    Update 7.)ListOfProd.js

--- JS-Fundamentals-2023/ArrayAdvanced/7.)ListOfProd.js
+++ JS-Fundamentals-2023/ArrayAdvanced/7.)ListOfProd.js
@@ -1,3 +1,6 @@
 //Task: You will receive an array of products. Print a numbered array of all the products ordered by name.
 
 //Solution:
+
+function productsList(arr) {}
+productsList(["Potatoes", "Tomatoes", "Onions", "Apples"]);
