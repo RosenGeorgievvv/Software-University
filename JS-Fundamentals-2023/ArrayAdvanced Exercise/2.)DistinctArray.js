047059a5e1a1481a74168eba4e5978b672c3180c
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-10 11:39:46 +0300

    Update 2.)DistinctArray.js

--- JS-Fundamentals-2023/ArrayAdvanced Exercise/2.)DistinctArray.js
+++ JS-Fundamentals-2023/ArrayAdvanced Exercise/2.)DistinctArray.js
@@ -4,5 +4,13 @@
 
 // Print the result elements separated by a single space.
 
-
 //Solution:
+
+function distinctArray(arr) {
+
+
+    
+}
+distinctArray([1, 2, 3, 4]);
+distinctArray([7, 8, 9, 7, 2, 3, 4, 1, 2,]);
+distinctArray([20, 8, 12, 13, 4, 4, 8, 5]);
