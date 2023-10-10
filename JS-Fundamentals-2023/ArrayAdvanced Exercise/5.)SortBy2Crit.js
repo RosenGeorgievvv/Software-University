69f5b280ce7aed4e6231ceafe140037a94b2b48f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-10 14:17:56 +0300

    Update 5.)SortBy2Crit.js

--- JS-Fundamentals-2023/ArrayAdvanced Exercise/5.)SortBy2Crit.js
+++ JS-Fundamentals-2023/ArrayAdvanced Exercise/5.)SortBy2Crit.js
@@ -5,3 +5,17 @@
 // The output is the ordered array of strings, each on a separate line.
 
 //Solution:
+
+function sortBy2Crit(arr){
+
+let sorted = (a, b) =>{
+    if(a.length !== b.length){
+        return a.length - b.length;
+    }
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+}
+arr.sort(sorted);
+console.log(arr.join('\n'));
+
+}
+sortBy2Crit(['alpha','beta','gamma'])
