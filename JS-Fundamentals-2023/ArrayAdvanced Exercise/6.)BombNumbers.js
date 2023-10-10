f685674ff045c482075eb66babff02b9f65851ff
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-10 14:27:12 +0300

    Update 6.)BombNumbers.js

--- JS-Fundamentals-2023/ArrayAdvanced Exercise/6.)BombNumbers.js
+++ JS-Fundamentals-2023/ArrayAdvanced Exercise/6.)BombNumbers.js
@@ -7,3 +7,23 @@
 // The output is the sum of the remaining elements in the sequence.
 
 //Solution:
+
+function bombNums(seq, bomb){
+
+    let specialNum = bomb[0];
+    let power = bomb[1];
+
+    while(seq.includes(specialNum)){
+        let current = seq.indexOf(specialNum);
+        let removedElements = power * 2 + 1;
+        let startIndx =current - power;
+
+        if(startIndx < 0) {
+            removedElements += startIndx;
+            startIndx = 0
+        }
+        seq.splice(startIndx, removedElements);
+    }
+    console.log(seq.reduce((a, b) => a + b, 0));
+}
+bombNums([1, 4, 4, 2, 8, 9, 1], [9, 3])
