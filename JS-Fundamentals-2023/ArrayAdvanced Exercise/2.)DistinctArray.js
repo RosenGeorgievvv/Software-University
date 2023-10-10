c0b041be522631599f07a647fbb5aedbcb4a90b3
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-10 11:57:33 +0300

    Update 2.)DistinctArray.js

--- JS-Fundamentals-2023/ArrayAdvanced Exercise/2.)DistinctArray.js
+++ JS-Fundamentals-2023/ArrayAdvanced Exercise/2.)DistinctArray.js
@@ -6,10 +6,31 @@
 
 //Solution:
 
+
+function disArr(arr){
+
+    let finalArr = [];
+
+    for(let i = 0; i < arr.length; i++){
+        let currentElement = arr[i];
+
+        if(finalArr.indexOf(currentElement) === -1){
+            finalArr.push(currentElement);
+        }
+    }
+console.log(finalArr.join(' '))
+}
+disArr([1, 2, 3, 4]);
+disArr([7, 8, 9, 7, 2, 3, 4, 1, 2,]);
+disArr([20, 8, 12, 13, 4, 4, 8, 5]);
+
+
+//short way with methods:
 function distinctArray(arr) {
 
+let  uniqueElements = arr.filter((value, index, myArr) => myArr.indexOf(value) === index).join(' ');
+console.log(uniqueElements)
 
-    
 }
 distinctArray([1, 2, 3, 4]);
 distinctArray([7, 8, 9, 7, 2, 3, 4, 1, 2,]);
