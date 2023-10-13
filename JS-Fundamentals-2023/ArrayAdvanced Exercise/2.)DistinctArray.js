1d731d981af304cf4984900db2a7e0bd3f28d12f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-13 20:29:08 +0300

    Update 2.)DistinctArray.js

--- JS-Fundamentals-2023/ArrayAdvanced Exercise/2.)DistinctArray.js
+++ JS-Fundamentals-2023/ArrayAdvanced Exercise/2.)DistinctArray.js
@@ -35,3 +35,18 @@ console.log(uniqueElements)
 distinctArray([1, 2, 3, 4]);
 distinctArray([7, 8, 9, 7, 2, 3, 4, 1, 2,]);
 distinctArray([20, 8, 12, 13, 4, 4, 8, 5]);
+
+
+//thirdway:
+
+function distinctArray(arr){
+    let uniqueNums = [];
+
+    for(let num of arr){
+        if(!uniqueNums.includes(num))
+        uniqueNums.push(num);
+    }
+
+    console.log(uniqueNums.join(' '));
+}
+distinctArray([20, 8, 12, 13, 4, 4, 8, 5])
