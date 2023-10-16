00d45a8d3ed07b2d45a5ba6874f770508c68d6bd
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-16 19:34:04 +0300

    Update Problem5.js

--- JS-Fundamentals-2023/Exam Preparation/Problem5.js
+++ JS-Fundamentals-2023/Exam Preparation/Problem5.js
@@ -52,3 +52,21 @@
 
 
 //Solution:
+
+
+function treasureHunt(arr){
+
+
+
+}
+treasureHunt(["Gold|Silver|Bronze|Medallion|Cup",
+
+"Loot Wood Gold Coins",
+
+"Loot Silver Pistol",
+
+"Drop 3",
+
+"Steal 3",
+
+"Yohoho!"])
