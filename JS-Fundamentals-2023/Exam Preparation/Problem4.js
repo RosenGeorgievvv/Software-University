e22e83a6d521aa443d92ec3a49b19a4fcb4173fd
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-16 19:33:04 +0300

    Update Problem4.js

--- JS-Fundamentals-2023/Exam Preparation/Problem4.js
+++ JS-Fundamentals-2023/Exam Preparation/Problem4.js
@@ -30,3 +30,57 @@
 
 
 //Solution: 
+
+function computerStore(input){
+
+    let withoutTaxes = 0;
+    let command = input.pop().toLowerCase();
+    let withTaxes = 0;
+    let totalPrice = 0;
+
+    if(input.length === 0){
+        return console.log("Invalid order!");
+    }
+
+    for(let price of input){
+        let currentPrice = Number(price);
+
+        if(price > 0){
+            withoutTaxes += currentPrice;
+        }else{
+            console.log("Invalid price!");
+            continue;
+        }
+    }
+    if(withoutTaxes === 0){
+        return console.log("Invalid order!");
+    }
+    withTaxes = withoutTaxes * 0.2;
+    totalPrice = withTaxes + withoutTaxes;
+
+    if(command === "special"){
+        totalPrice = totalPrice - (totalPrice * 0.1);
+    }
+    console.log(`Congratulations you've just bought a new computer!`);
+    console.log(`Price without taxes: ${withoutTaxes.toFixed(2)}$`);
+    console.log(`Taxes: ${withTaxes.toFixed(2)}$`);
+    console.log("-----------");
+    console.log(`Total price: ${totalPrice.toFixed(2)}$`);
+}
+computerStore([
+
+    '1050',
+    
+    '200',
+    
+    '450',
+    
+    '2',
+    
+    '18.50',
+    
+    '16.86',
+    
+    'special'
+    
+    ])
