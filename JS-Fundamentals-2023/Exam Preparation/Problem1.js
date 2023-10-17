7dde8ef902205e86e4cba8ed70e099f90d3185c8
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-17 21:07:44 +0300

    update

--- JS-Fundamentals-2023/Exam Preparation/Problem1.js
+++ JS-Fundamentals-2023/Exam Preparation/Problem1.js
@@ -63,3 +63,31 @@ function solve(food, hay, cover, weight) {
     }
 }
 solve(10,5,5.2,1);
+
+
+//second way:
+
+function solve(arr) {
+    let food = Number(arr[0]) * 1000;
+    let hay = Number(arr[1]) * 1000;
+    let cover = Number(arr[2]) * 1000;
+    let weight = Number(arr[3]) * 1000;
+    let coverConsumption = weight / 3;
+  
+    for (let i = 1; i <= 30; i++) {
+      food -= 300;
+  
+      if (i % 2 === 0) {
+        hay -= (food * 0.05);
+      }
+      if (i % 3 === 0) {
+        cover -= coverConsumption;
+      }
+      if (food <= 0 || hay <= 0 || cover <= 0) {
+        console.log("Merry must go to the pet store!");
+        return;
+      }
+    }
+    console.log(`Everything is fine! Puppy is happy! Food: ${(food / 1000).toFixed(2)}, Hay: ${(hay / 1000).toFixed(2)}, Cover: ${(cover / 1000).toFixed(2)}.`);
+  }
+  solve(["10", "5", "5.2", "1"]);
--- test.js
+++ test.js
@@ -1,33 +1,24 @@
 function solve(arr) {
- 
-    let guestList = [];
+  let food = Number(arr[0]) * 1000;
+  let hay = Number(arr[1]) * 1000;
+  let cover = Number(arr[2]) * 1000;
+  let weight = Number(arr[3]) * 1000;
+  let coverConsumption = weight / 3;
 
-    for(let i = 0; i < arr.length; i++){
-        let currentRow = arr[i].split(' ');
-        let currentName = currentRow[0];
-        let command = currentRow[2];
+  for (let i = 1; i <= 30; i++) {
+    food -= 300;
 
-        if(command != "not"){
-            if(guestList.includes(currentName) === true){
-                console.log(`${currentName} is already in the list!`);
-                continue;
-            }
-            guestList.push(currentName);
-        }else{
-            if(guestList.includes(currentName) === false){
-                console.log(`${currentName} is not in the list!`);
-                continue;
-            }
-            guestList = guestList.filter(x => x != currentName)
-        }
+    if (i % 2 === 0) {
+      hay -= (food * 0.05);
     }
-console.log(guestList.join('\n'));
+    if (i % 3 === 0) {
+      cover -= coverConsumption;
+    }
+    if (food <= 0 || hay <= 0 || cover <= 0) {
+      console.log("Merry must go to the pet store!");
+      return;
+    }
+  }
+  console.log(`Everything is fine! Puppy is happy! Food: ${(food / 1000).toFixed(2)}, Hay: ${(hay / 1000).toFixed(2)}, Cover: ${(cover / 1000).toFixed(2)}.`);
 }
-
-solve(['Allie is going!',
-
-'George is going!',
-
-'John is not going!',
-
-'George is not going!']);
+solve(["10", "5", "5.2", "1"]);
