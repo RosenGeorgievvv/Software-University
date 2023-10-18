2faedd6993607b67f77d727a2ab93e48dd2df697
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-18 13:16:28 +0300

    Update Problem8.js

--- JS-Fundamentals-2023/Mid Exam Preparation/Problem8.js
+++ JS-Fundamentals-2023/Mid Exam Preparation/Problem8.js
@@ -1,7 +1,7 @@
 //Task: Write a program that recreates the Memory game.
 // On the first line, you will receive a sequence of elements. Each element in the sequence will have a twin. Until the player receives "end" from the console, you will receive strings with two integers separated by a space, representing the indexes of elements in the sequence.
 // If the player tries to cheat and enters two equal indexes or indexes which are out of bounds of the sequence, you should add two matching elements at the middle of the sequence in the following format:
-// "-{number of moves until now}a" 
+// "-{number of moves until now}a"
 // Then print this message on the console:
 // "Invalid input! Adding additional elements to the board"
 // Input
@@ -12,10 +12,17 @@
 // "Congrats! You have found matching elements - {element}!"
 // •	If the player hit two different elements, you should print on the console the following message:
 // "Try again!"
-// •	If the player hit all matching elements before he receives "end" from the console, you should print on the console the following message: 
+// •	If the player hit all matching elements before he receives "end" from the console, you should print on the console the following message:
 // "You have won in {number of moves until now} turns!"
 // •	If the player receives "end" before he hits all matching elements, you should print on the console the following message:
 // "Sorry you lose :(
 // {the current sequence's state}"
 // Constraints
 // •	All elements in the sequence will always have a matching element.
+
+function solve(input) {
+
+
+
+}
+solve(["1 1 2 2 3 3 4 4 5 5", "1 0", "-1 0", "1 0", "1 0", "1 0", "end"]);
