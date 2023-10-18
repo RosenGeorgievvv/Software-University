64dda735b15224f502b6cef9e8a5cf06344ce484
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-18 10:19:34 +0300

    Update 3.)ConvertToObject.js

---
+++ JS-Fundamentals-2023/Objects&Classes/3.)ConvertToObject.js
@@ -0,0 +1,5 @@
+//Task: Write a function that receives a string in JSON format and converts it to an object.
+
+// Loop through all the keys and print them with their values in format: "{key}: {value}"
+
+
