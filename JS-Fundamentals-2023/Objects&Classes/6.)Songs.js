f47abbd40275cdb8009e949936c1cb7e077b3b7a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-18 10:21:14 +0300

    Update 6.)Songs.js

---
+++ JS-Fundamentals-2023/Objects&Classes/6.)Songs.js
@@ -0,0 +1,7 @@
+//Task: Define a class Song, which holds the following information about songs: typeList, name, and time.
+
+// You will receive the input as an array.
+
+// The first element n will be the number of songs. Next n elements will be the songs data in the following format: "{typeList}_{name}_{time}", and the last element will be typeList / "all".
+
+// Print only the names of the songs, which have the same typeList (obtained as the last parameter). If the value of the last element is "all", print the names of all the songs.
