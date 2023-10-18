49889e80343542bf43271a281853a7367f8847b7
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-18 15:01:38 +0300

    Update 4.)Movies.js

--- JS-Fundamentals-2023/Objects&Classes Exercise/4.)Movies.js
+++ JS-Fundamentals-2023/Objects&Classes Exercise/4.)Movies.js
@@ -7,3 +7,5 @@
 // · "{movie name} onDate {date}" – check if the movie exists and then add the date
 
 // At the end print all the movies that have all the info (if the movie has no director, name, or date, don’t print it) in JSON format.
+
+//Solution:
