c2d1a5bdcb19b2e04008a95d575e5cf6414295bb
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-18 15:01:02 +0300

    Update 3.)StoreProvision.js

--- JS-Fundamentals-2023/Objects&Classes Exercise/3.)StoreProvision.js
+++ JS-Fundamentals-2023/Objects&Classes Exercise/3.)StoreProvision.js
@@ -5,3 +5,5 @@
 // Every even index will hold the name of the product and every odd index will hold the quantity of that product. The second array could contain products that are already in the local store. If that happens increase the quantity for the given product. You should store them into an object, and print them in the following format: (product -> quantity)
 
 // All of the arrays’ values will be strings.
+
+//Solution:
