9f0456f88e8aea6b1010798d946bf1e43470cec5
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-18 15:02:47 +0300

    Update 6.)MakeDictionary.js

---
+++ JS-Fundamentals-2023/Objects&Classes Exercise/6.)MakeDictionary.js
@@ -0,0 +1,9 @@
+//Task:You will receive an array with strings in the form of JSON's.
+
+// You have to parse these strings and combine them into one object. Every string from the array will hold terms and a description. If you receive the same term twice, replace it with the new definition.
+
+// Print every term and definition in that dictionary on new line in format:
+
+// `Term: ${term} => Definition: ${definition}`
+
+// Don't forget to sort the dictionary alphabetically by the terms as in real dictionaries.
