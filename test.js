96ed7904fe1440063c07410e24f630e268b3ed9c
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-18 15:32:38 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,37 +1,8 @@
-function solve(input) {
+function solve(arr){
 
-let noTaxes = 0;
-let withTaxes = 0;
-let totalPrice = 0;
+    let filtered = arr.filter((x) => x > 4)
 
-let command = input.pop().toLowerCase();
+    console.log(filtered);
 
-for(let price of input){
-    let currentPrice = Number(price);
-
-    if(price > 0){
-        noTaxes += currentPrice;
-    }else{
-        console.log("Invalid price!");
-        continue;
-    }
 }
-if(input.length === 0){
-   return console.log("Invalid order!");
-}
-if(noTaxes === 0) {
-    return console.log("Invalid order!");
-}
-withTaxes = noTaxes * 0.2;
-totalPrice = withTaxes + noTaxes;
-
-if(command === 'special'){
-    totalPrice = totalPrice - (totalPrice * 0.1);
-}
-console.log(`Congratulations you've just bought a new computer!`);
-    console.log(`Price without taxes: ${noTaxes.toFixed(2)}$`);
-    console.log(`Taxes: ${withTaxes.toFixed(2)}$`);
-    console.log("-----------");
-    console.log(`Total price: ${totalPrice.toFixed(2)}$`);
-}
-solve(["1050", "200", "450", "2", "18.50", "16.86", "special"]);
+solve([2, 3, 4, 5, 6])
