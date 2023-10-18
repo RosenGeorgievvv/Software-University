39ad000f88284e29db54d50c06fecb4694680fab
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-18 15:01:29 +0300

    Update 4.)Movies.js

---
+++ JS-Fundamentals-2023/Objects&Classes Exercise/4.)Movies.js
@@ -0,0 +1,9 @@
+//Task: Write a function that stores information about movies inside an array. The movie's object info must be name, director, and date. You can receive several types of input:
+
+// · "addMovie {movie name}" – add the movie
+
+// · "{movie name} directedBy {director}" – check if the movie exists and then add the director
+
+// · "{movie name} onDate {date}" – check if the movie exists and then add the date
+
+// At the end print all the movies that have all the info (if the movie has no director, name, or date, don’t print it) in JSON format.
