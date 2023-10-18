f71145d136a98d5ed253944193b8f4a92513655b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-18 10:19:02 +0300

    Update 2.)City.js

---
+++ JS-Fundamentals-2023/Objects&Classes/2.)City.js
@@ -0,0 +1,6 @@
+//Task: Write a function that receives a single parameter – an object, containing five properties:
+
+// { name, area, population, country, postcode }
+
+// Loop through all the keys and print them with their values in format: "{key} -> {value}"
+
