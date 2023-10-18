aa9bb9a684c9ba721246ee4862cba25882ed3265
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-18 10:18:24 +0300

    Update 1.)PersonInfo.js

---
+++ JS-Fundamentals-2023/Objects&Classes/1.)PersonInfo.js
@@ -0,0 +1,5 @@
+//Task: Write a function that receives 3 parameters, sets them to an object, and returns that object.
+
+// The input comes as 3 separate strings in the following order: firstName, lastName, age.
+
+
