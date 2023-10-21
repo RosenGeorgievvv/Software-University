cd9e7168632dfecede8ef8e89ec279a63d430eb3
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-21 12:50:24 +0300

    Update test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+function numbers(str){
+
+    let nums = str.split(' ').map(Number);
+    let averageNum = nums.reduce((acc, val) => acc + val) / nums.length;
+   let topFive = nums.filter((num) => num > averageNum).sort((a,b) => b - a).slice(0, 5);
+
+   if(topFive.length == 0){
+    console.log("No");
+   }else{
+    console.log(topFive.join(' '));
+   }
+}
+numbers('10 20 30 40 50')
