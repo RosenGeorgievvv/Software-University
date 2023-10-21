207b3e5c1a52ac6a5036f567ad12489868f1ff11
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-21 20:08:16 +0300

    Update Problem9.js

--- JS-Fundamentals-2023/Mid Exam Preparation/Problem9.js
+++ JS-Fundamentals-2023/Mid Exam Preparation/Problem9.js
@@ -1,5 +1,5 @@
 //Task: Three employees are working in the reception all day. Each of them can handle a different number of students per hour. Your task is to calculate how much time it will take to answer all the questions of a given number of students.
-// First, you will receive 3 lines with integers, representing the number of students that each employee can help per hour. On the following line, you will receive students count as a single integer. 
+// First, you will receive 3 lines with integers, representing the number of students that each employee can help per hour. On the following line, you will receive students count as a single integer.
 // Every fourth hour, all employees have a break, so they don't work for an hour. It is the only break for the employees, because they don't need rest, nor have a personal life. Calculate the time needed to answer all the student's questions and print it in the following format: "Time needed: {time}h."
 // Input / Constraints
 // •	On the first three lines -  each employee efficiency -  integer in the range [1 - 100].
@@ -10,3 +10,21 @@
 // •	Allowed working time/memory: 100ms / 16MB.
 
 //Solution:
+
+function softReception(arr) {
+  arr = arr.map((x) => Number(x));
+  let studentsCount = arr.pop();
+  let allStudentGroups = arr.reduce((acc, val) => acc + val);
+  let hours = 0;
+
+  while(studentsCount > 0){
+    studentsCount -= allStudentGroups;
+    hours++;
+
+    if(hours % 4 === 0){
+        hours++;
+    }
+  }
+  console.log(`Time needed: ${hours}h.`);
+}
+softReception(["5", "6", "4", "20"]);
