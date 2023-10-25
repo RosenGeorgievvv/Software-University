0ff373c20c3dfdac2f9de28e3ab64b8c77887811
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-25 11:30:19 +0300

    Update 1.)PersonInfo.js

--- JS-Fundamentals-2023/Objects&Classes/1.)PersonInfo.js
+++ JS-Fundamentals-2023/Objects&Classes/1.)PersonInfo.js
@@ -5,12 +5,12 @@
 //Solution:
 
 function personInfo(firstName, lastName, age) {
+  let person = {};
 
-    let person = {};
-    person.firstName = firstName;
-    person.lastName = lastName;
-    person.age = age;
+  person.firstName = firstName;
+  person.lastName = lastName;
+  person.age = age;
 
-    return person;
+  return person;
 }
 console.log(personInfo("Peter", "Pan", "20"));
