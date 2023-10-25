8285a0fedaa52c04aaf2bcca86ce033f2e51cad9
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-25 13:12:43 +0300

    Update 2.)City.js

--- JS-Fundamentals-2023/Objects&Classes/2.)City.js
+++ JS-Fundamentals-2023/Objects&Classes/2.)City.js
@@ -5,3 +5,23 @@
 // Loop through all the keys and print them with their values in format: "{key} -> {value}"
 
 //Solution:
+
+function cityInfo(city) {
+
+    let entries = Object.entries(city)
+        for(let [key, value] of entries){
+            console.log(`${key} -> ${value}`);
+        }
+
+}
+cityInfo({
+  name: "Plovdiv",
+
+  area: 389,
+
+  population: 1162358,
+
+  country: "Bulgaria",
+
+  postCode: "4000",
+});
