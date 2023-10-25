0c13e80250388540536e57449d64385ab7874296
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-25 13:29:57 +0300

    Update 3.)ConvertToObject.js

--- JS-Fundamentals-2023/Objects&Classes/3.)ConvertToObject.js
+++ JS-Fundamentals-2023/Objects&Classes/3.)ConvertToObject.js
@@ -2,5 +2,14 @@
 
 // Loop through all the keys and print them with their values in format: "{key}: {value}"
 
-
 //Solution:
+
+function convertToObject(json) {
+  let parseToObject = JSON.parse(json);
+  let entries = Object.entries(parseToObject);
+
+  for (let [key, value] of entries) {
+    console.log(`${key}: ${value}`);
+  }
+}
+convertToObject('{"name": "George", "age": 40, "town": "Sofia"}');
