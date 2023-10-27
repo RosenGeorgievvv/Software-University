336b9bf885b26870982d18b00e4d6cedea6e06ef
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-27 21:08:18 +0300

    Update 7.)ClassVehicle.js

--- JS-Fundamentals-2023/Objects&Classes Exercise/7.)ClassVehicle.js
+++ JS-Fundamentals-2023/Objects&Classes Exercise/7.)ClassVehicle.js
@@ -20,5 +20,23 @@
 
 // In judge post your class (Note: all names should be as described)
 
-
 //Solution:
+
+class Vehicle {
+  constructor(type, model, parts, fuel) {
+    this.type = type;
+    this.model = model;
+    this.parts = parts;
+    this.fuel = fuel;
+    this.parts.quality = this.parts.engine * this.parts.power;
+  }
+
+  drive(fuelLoss) {
+    this.fuel -= fuelLoss;
+  }
+}
+let parts = { engine: 6, power: 100 };
+let vehicle = new Vehicle("a", "b", parts, 200);
+vehicle.drive(100);
+console.log(vehicle.fuel);
+console.log(vehicle.parts.quality);
