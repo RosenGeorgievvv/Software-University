c002414c95bd9214518cb91045262d85defd2ce7
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-27 13:34:26 +0300

    Update 2.)Meetings.js

--- JS-Fundamentals-2023/Associative Arrays/2.)Meetings.js
+++ JS-Fundamentals-2023/Associative Arrays/2.)Meetings.js
@@ -1,4 +1,4 @@
-//Task:
+//Task: Write a function that manages meeting appointments. The input comes as an array of strings. Each string contains a weekday and person’s name. For each successful meeting, print a message. If you receive the same weekday twice, the meeting cannot be scheduled so print a conflicting message. In the end, print a list of all successful meetings.
 
 
 
