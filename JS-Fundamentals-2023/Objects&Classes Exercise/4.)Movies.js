526ccfd6a3fec620ca35f4aef11a4d5d6cf7006f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-27 20:15:00 +0300

    Update 4.)Movies.js

--- JS-Fundamentals-2023/Objects&Classes Exercise/4.)Movies.js
+++ JS-Fundamentals-2023/Objects&Classes Exercise/4.)Movies.js
@@ -9,3 +9,26 @@
 // At the end print all the movies that have all the info (if the movie has no director, name, or date, don’t print it) in JSON format.
 
 //Solution:
+
+function movies() {
+
+
+    
+}
+movies([
+  "addMovie Fast and Furious",
+
+  "addMovie Godfather",
+
+  "Inception directedBy Christopher Nolan",
+
+  "Godfather directedBy Francis Ford Coppola",
+
+  "Godfather onDate 29.07.2018",
+
+  "Fast and Furious onDate 30.07.2018",
+
+  "Batman onDate 01.08.2018",
+
+  "Fast and Furious directedBy Rob Cohen",
+]);
