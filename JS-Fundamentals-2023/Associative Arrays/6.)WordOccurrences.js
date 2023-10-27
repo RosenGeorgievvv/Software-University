60f29f94ab3aaf14a029529ffb4e5583933007c1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-27 13:40:36 +0300

    Update 6.)WordOccurrences.js

--- JS-Fundamentals-2023/Associative Arrays/6.)WordOccurrences.js
+++ JS-Fundamentals-2023/Associative Arrays/6.)WordOccurrences.js
@@ -1,4 +1,4 @@
-//Task:
+//Task:  Write a function that counts the times each word occurs in a text. Print the words sorted by count in descending order. The input comes as an array of strings.
 
 
 //Solution:
