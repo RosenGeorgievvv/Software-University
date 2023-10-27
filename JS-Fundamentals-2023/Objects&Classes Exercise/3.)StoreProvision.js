e8d110a66f4df01aeca9c4c2253e6c6e2fcf9d68
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-27 19:43:35 +0300

    Update 3.)StoreProvision.js

--- JS-Fundamentals-2023/Objects&Classes Exercise/3.)StoreProvision.js
+++ JS-Fundamentals-2023/Objects&Classes Exercise/3.)StoreProvision.js
@@ -9,26 +9,28 @@
 //Solution:
 
 function storeProvision(stock, orderedProducts) {
+  let products = {};
 
-    let products = {};
-
-for(let i = 0; i < stock.length; i += 2){
+  for (let i = 0; i < stock.length; i += 2) {
     let productName = stock[i];
     let qty = Number(stock[i + 1]);
 
     products[productName] = qty;
-}    
-for(let i = 0; i < orderedProducts.length; i += 2){
+  }
+  for (let i = 0; i < orderedProducts.length; i += 2) {
     let productName = orderedProducts[i];
     let qty = Number(orderedProducts[i + 1]);
 
-    if( productName in products){
-        products[productName] += qty;
-    }else{
-        products[productName] = qty;
+    if (productName in products) {
+      products[productName] += qty;
+    } else {
+      products[productName] = qty;
     }
-}
-
+  }
+  let entries = Object.entries(products);
+  for (let entry of entries) {
+    console.log(`${entry[0]} -> ${entry[1]}`);
+  }
 }
 storeProvision(
   ["Chips", "5", "CocaCola", "9", "Bananas", "14", "Pasta", "4", "Beer", "2"],
