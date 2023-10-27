769de31e28f3dbfa8a431ad710f037aae0bc60c8
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-27 13:34:01 +0300

    Update 1.)PhoneBook.js

--- JS-Fundamentals-2023/Associative Arrays/1.)PhoneBook.js
+++ JS-Fundamentals-2023/Associative Arrays/1.)PhoneBook.js
@@ -1,4 +1,4 @@
-//Task:
+//Task: Write a function that stores information about a person’s name and phone number. The input is an array of strings with space-separated name and number. Replace duplicate names. 
 
 
 //Solution:
