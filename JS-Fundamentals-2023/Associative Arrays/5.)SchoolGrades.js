b10c4c651a2df4759452bfd5727bbe695266fbea
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-27 13:40:08 +0300

    Update 5.)SchoolGrades.js

--- JS-Fundamentals-2023/Associative Arrays/5.)SchoolGrades.js
+++ JS-Fundamentals-2023/Associative Arrays/5.)SchoolGrades.js
@@ -1,4 +1,6 @@
-//Task:
+//Task: Write a function that stores students and their grades throughout the year. If a student appears more than once, add the new grades to existing ones. Finally, print the students and their average grades, sorted alphabetically by student name. The input comes as an array of strings.
+
+// Note: The average grades must be fixed to the second decimal place.
 
 
 
