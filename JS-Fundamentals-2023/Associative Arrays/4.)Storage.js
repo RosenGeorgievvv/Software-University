5fc72f25915fe81a75e6ff130ad341d161045f1b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-27 13:35:44 +0300

    Update 4.)Storage.js

--- JS-Fundamentals-2023/Associative Arrays/4.)Storage.js
+++ JS-Fundamentals-2023/Associative Arrays/4.)Storage.js
@@ -1,3 +1,3 @@
-//Task:
+//Task: Write a function that takes a certain number of items and their quantity. If the same item appears more than once, add the new amount to the existing one. In the end, print all the items and their amount without sorting them. The input comes as an array of strings. Try using a Map().
 
 //Solution:
