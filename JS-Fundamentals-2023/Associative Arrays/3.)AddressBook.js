69f3f8d85b510e0596eb7d703b008cd2e9718c96
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-27 13:35:17 +0300

    Update 3.)AddressBook.js

--- JS-Fundamentals-2023/Associative Arrays/3.)AddressBook.js
+++ JS-Fundamentals-2023/Associative Arrays/3.)AddressBook.js
@@ -1,4 +1,4 @@
-//Task:
+//Task:Write a function that stores information about a person’s name and his address. The input comes as an array of strings. Each string contains the name and the address separated by a colon. If you receive the same name twice just replace the address. In the end, print the full list, sorted alphabetically by the person’s name.
 
 
 
