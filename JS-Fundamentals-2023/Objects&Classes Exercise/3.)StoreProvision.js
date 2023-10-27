925047bdb25893a179272099b2196067d9b7e423
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-27 15:16:22 +0300

    Update 3.)StoreProvision.js

--- JS-Fundamentals-2023/Objects&Classes Exercise/3.)StoreProvision.js
+++ JS-Fundamentals-2023/Objects&Classes Exercise/3.)StoreProvision.js
@@ -7,3 +7,15 @@
 // All of the arrays’ values will be strings.
 
 //Solution:
+
+function storeProvision(arr1, arr2) {
+
+
+
+    
+}
+storeProvision(
+  ["Chips", "5", "CocaCola", "9", "Bananas", "14", "Pasta", "4", "Beer", "2"],
+
+  ["Flour", "44", "Oil", "12", "Pasta", "7", "Tomatoes", "70", "Bananas", "30"]
+);
