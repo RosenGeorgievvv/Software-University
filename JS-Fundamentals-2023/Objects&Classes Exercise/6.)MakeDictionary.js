f44413e9bb281c50655e99d201d31c6e0cdaf08a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-28 10:14:39 +0300

    Update 6.)MakeDictionary.js

--- JS-Fundamentals-2023/Objects&Classes Exercise/6.)MakeDictionary.js
+++ JS-Fundamentals-2023/Objects&Classes Exercise/6.)MakeDictionary.js
@@ -9,3 +9,32 @@
 // Don't forget to sort the dictionary alphabetically by the terms as in real dictionaries.
 
 //Solution:
+
+function createDictionary(terms) {
+  let dictionary = {};
+
+  for (let termDefinition of terms) {
+    let parsedData = JSON.parse(termDefinition);
+
+    for (let term in parsedData) {
+      dictionary[term] = parsedData[term];
+    }
+  }
+
+  let sortedTerms = Object.keys(dictionary).sort();
+
+  for (let term of sortedTerms) {
+    console.log(`Term: ${term} => Definition: ${dictionary[term]}`);
+  }
+}
+createDictionary([
+  '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+
+  '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+
+  '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+
+  '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+
+  '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}',
+]);
