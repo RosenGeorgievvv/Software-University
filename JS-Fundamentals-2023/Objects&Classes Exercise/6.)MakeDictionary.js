ab9ed30876109b55d705b4b716ba33ff2e31959e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-28 10:15:16 +0300

    Update 6.)MakeDictionary.js

--- JS-Fundamentals-2023/Objects&Classes Exercise/6.)MakeDictionary.js
+++ JS-Fundamentals-2023/Objects&Classes Exercise/6.)MakeDictionary.js
@@ -25,7 +25,6 @@ function createDictionary(terms) {
   for (let term of sortedTerms) {
     console.log(`Term: ${term} => Definition: ${dictionary[term]}`);
   }
-  
 }
 createDictionary([
   '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
