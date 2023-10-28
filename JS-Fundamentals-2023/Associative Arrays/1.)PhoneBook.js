c4befb0eb8ba6b966d1f038d1579928a0bc2fba6
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-28 15:14:10 +0300

    update

--- JS-Fundamentals-2023/Associative Arrays/1.)PhoneBook.js
+++ JS-Fundamentals-2023/Associative Arrays/1.)PhoneBook.js
@@ -1,4 +1,28 @@
-//Task: Write a function that stores information about a person’s name and phone number. The input is an array of strings with space-separated name and number. Replace duplicate names. 
-
+//Task: Write a function that stores information about a person’s name and phone number. The input is an array of strings with space-separated name and number. Replace duplicate names.
 
 //Solution:
+
+function assocArr(arr) {
+
+    let phonebookList = {};
+
+    for(let line of arr){
+        let tokens = line.split(' ');
+        let name = tokens[0];
+        let number = tokens[1];
+        phonebookList[name] = number;
+    }
+    for(let key in phonebookList){
+        console.log(`${key} -> ${phonebookList[key]}`);
+    }
+
+}
+assocArr([
+  "Tim 0834212554",
+
+  "Peter 0877547887",
+
+  "Bill 0896543112",
+
+  "Tim 0876566344",
+]);
