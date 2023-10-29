ebd76814435396b412a21fb54d7ccd2d789ad415
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-29 20:03:24 +0200

    Update 8.)CenturiesToMinutes.js

--- JS-Fundamentals-2023/Data Types/8.)CenturiesToMinutes.js
+++ JS-Fundamentals-2023/Data Types/8.)CenturiesToMinutes.js
@@ -3,12 +3,16 @@
 //Solution:
 
 function convert(input) {
+  let years = input * 100;
+  let days = Math.trunc(years * 365.2422);
+  let hours = 24 * days;
+  let minutes = 60 * hours;
 
-    let years = input*100;
-    let days = Math.trunc(years * 365.2422);
-    let hours = 24 * days;
-    let minutes = 60 * hours;
-
-    console.log(`${input} centuries = ${years} `+`years = ${days} ` + `days = ${hours} ` + `hours = ${minutes} minutes`)
+  console.log(
+    `${input} centuries = ${years} ` +
+      `years = ${days} ` +
+      `days = ${hours} ` +
+      `hours = ${minutes} minutes`
+  );
 }
 convert(1);
