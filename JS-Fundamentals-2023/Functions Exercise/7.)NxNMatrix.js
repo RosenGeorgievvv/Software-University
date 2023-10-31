65e2257e4d5608f4ff37b9c61ec6abe8ca9f2bd7
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-10-31 12:27:38 +0200

    Update 7.)NxNMatrix.js

--- JS-Fundamentals-2023/Functions Exercise/7.)NxNMatrix.js
+++ JS-Fundamentals-2023/Functions Exercise/7.)NxNMatrix.js
@@ -5,7 +5,7 @@
 function nxnMatrix(n) {
   for (let rowNum = 1; rowNum <= n; rowNum++) {
     let curRow = `${n} `.repeat(n);
-    console.log(curRow)
+    console.log(curRow);
   }
 }
 nxnMatrix(3);
