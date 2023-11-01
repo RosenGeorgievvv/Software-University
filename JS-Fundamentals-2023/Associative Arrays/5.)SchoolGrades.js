457db55b5e644c419e1a6ad6402def900bfd29e0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-01 16:00:25 +0200

    update

--- JS-Fundamentals-2023/Associative Arrays/5.)SchoolGrades.js
+++ JS-Fundamentals-2023/Associative Arrays/5.)SchoolGrades.js
@@ -2,6 +2,34 @@
 
 // Note: The average grades must be fixed to the second decimal place.
 
-
-
 //Solution:
+
+function schoolGrades(arr) {
+    let studentGrades = new Map();
+  
+    for (let entry of arr) {
+      let [name, ...grades] = entry.split(' ');
+      let studentName = name;
+      let studentGradesList = grades.map(grade => parseInt(grade, 10));
+  
+      if (studentGrades.has(studentName)) {
+        studentGrades.set(studentName, [
+          ...studentGrades.get(studentName),
+          ...studentGradesList
+        ]);
+      } else {
+        studentGrades.set(studentName, studentGradesList);
+      }
+    }
+    let sortedStudentGrades = new Map(
+      [...studentGrades].sort(([a], [b]) => a.localeCompare(b))
+    );
+  
+    for (let [name, grades] of sortedStudentGrades) {
+      let average = (
+        grades.reduce((sum, grade) => sum + grade, 0) / grades.length
+      ).toFixed(2);
+      console.log(`${name}: ${average}`);
+    }
+  }
+schoolGrades(["Lilly 4 6 6 5", "Tim 5 6", "Tammy 2 4 3", "Tim 6 6"]);
--- JS-Fundamentals-2023/Associative Arrays/6.)WordOccurrences.js
+++ JS-Fundamentals-2023/Associative Arrays/6.)WordOccurrences.js
@@ -1,4 +1,27 @@
 //Task:  Write a function that counts the times each word occurs in a text. Print the words sorted by count in descending order. The input comes as an array of strings.
 
-
 //Solution:
+
+function wordOccurrences(arr) {
+
+
+    
+}
+wordOccurrences([
+  "Here",
+  "is",
+  "the",
+  "first",
+  "sentence",
+
+  "Here",
+  "is",
+  "another",
+  "sentence",
+  "And",
+
+  "finally",
+  "the",
+  "third",
+  "sentence",
+]);
