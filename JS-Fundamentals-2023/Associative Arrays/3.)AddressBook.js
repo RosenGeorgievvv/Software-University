662b4283b1bd97c2b6befa5c07b4b72667837eb9
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-01 15:22:15 +0200

    Update 3.)AddressBook.js

--- JS-Fundamentals-2023/Associative Arrays/3.)AddressBook.js
+++ JS-Fundamentals-2023/Associative Arrays/3.)AddressBook.js
@@ -3,9 +3,24 @@
 //Solution:
 
 function addressBook(arr) {
+  let book = {};
 
+  for (let item of arr) {
+    let [name, address] = item.split(":");
 
-    
+    book[name] = address;
+  }
+
+  let entries = Object.entries(book);
+  entries.sort((a, b) => a[0].localeCompare(b[0]));
+
+  let sorted = {};
+  for (let [name, address] of entries) {
+    sorted[name] = address;
+  }
+  for (let [name, address] of Object.entries(sorted)) {
+    console.log(name, "->", address);
+  }
 }
 addressBook([
   "Tim:Doe Crossing",
