7b6f5552ad8b9897ecefaa0e714c7b449d4a0de4
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-01 15:53:05 +0200

    Update 4.)Storage.js

--- JS-Fundamentals-2023/Associative Arrays/4.)Storage.js
+++ JS-Fundamentals-2023/Associative Arrays/4.)Storage.js
@@ -3,9 +3,19 @@
 //Solution:
 
 function storage(arr) {
+  let myMap = new Map();
 
-    
+  for (let items of arr) {
+    let [item, quantity] = items.split(" ");
 
-
+    if (myMap.has(item)) {
+      myMap.set(item, myMap.get(item) + parseInt(quantity, 10));
+    } else {
+      myMap.set(item, parseInt(quantity, 10));
+    }
+  }
+  for (let [item, quantity] of myMap) {
+    console.log(`${item} -> ${quantity}`);
+  }
 }
 storage(["tomatoes 10", "coffee 5", "olives 100", "coffee 40"]);
