70f87238f79bca21d05904c427208fcb306d3f25
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-01 16:01:52 +0200

    Update 6.)WordOccurrences.js

--- JS-Fundamentals-2023/Associative Arrays/6.)WordOccurrences.js
+++ JS-Fundamentals-2023/Associative Arrays/6.)WordOccurrences.js
@@ -3,9 +3,22 @@
 //Solution:
 
 function wordOccurrences(arr) {
+  let map = new Map();
+  let words = 0;
+  for (let word of arr) {
+    if (!map.has(word)) {
+      words = 1;
+    } else {
+      words = map.get(word) + 1;
+    }
+    map.set(word, words);
+  }
+  let sorted = Array.from(map);
+  sorted.sort((a, b) => b[1] - a[1]);
 
-
-    
+  for (let [word, words] of sorted) {
+    console.log(`${word} -> ${words} times`);
+  }
 }
 wordOccurrences([
   "Here",
