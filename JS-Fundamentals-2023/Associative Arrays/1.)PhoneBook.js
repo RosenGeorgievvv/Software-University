660ed09dea08da8bcaae8cb18f79506cb4856e16
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-01 12:44:19 +0200

    Update 1.)PhoneBook.js

--- JS-Fundamentals-2023/Associative Arrays/1.)PhoneBook.js
+++ JS-Fundamentals-2023/Associative Arrays/1.)PhoneBook.js
@@ -24,3 +24,25 @@ assocArr([
 
   "Tim 0876566344",
 ]);
+
+
+//second way:
+
+function solve(arr){
+
+  let parsedInput = arr.map(item => item.split(' '));
+
+  let phoneBook = Object.fromEntries(parsedInput);
+
+  console.log(Object.entries(phoneBook).map(entry => entry.join(' -> ')).join('\n'));
+
+}
+solve([
+  "Tim 0834212554",
+
+  "Peter 0877547887",
+
+  "Bill 0896543112",
+
+  "Tim 0876566344",
+]);
