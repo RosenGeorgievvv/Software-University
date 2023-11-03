a52d596bce10c1f5416dd3a04d0e4409933a3fb0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-03 22:39:45 +0200

    Update 4.)PartyTime.js

--- JS-Fundamentals-2023/Associative Arrays Exercise/4.)PartyTime.js
+++ JS-Fundamentals-2023/Associative Arrays Exercise/4.)PartyTime.js
@@ -10,5 +10,61 @@
 
 // Hint: Guest names are not unique. Only the first match is removed when receiving a name.
 
-
 //Solution:
+
+function partyGuests(input) {
+    
+  function isVIP(name) {
+    return !isNaN(name[0]);
+  }
+
+  let guests = {
+    VIP: new Set(),
+    regular: new Set(),
+  };
+
+  let isPartyStarted = false;
+
+  for (let name of input) {
+    if (name === "PARTY") {
+      isPartyStarted = true;
+      continue;
+    }
+
+    if (!isPartyStarted) {
+      if (isVIP(name)) {
+        guests.VIP.add(name);
+      } else {
+        guests.regular.add(name);
+      }
+    } else {
+      if (isVIP(name)) {
+        guests.VIP.delete(name);
+      } else {
+        guests.regular.delete(name);
+      }
+    }
+  }
+
+  let allGuests = new Set([...guests.VIP, ...guests.regular]);
+
+  console.log(allGuests.size);
+  for (let guest of guests.VIP) {
+    console.log(guest);
+  }
+  for (let guest of guests.regular) {
+    console.log(guest);
+  }
+}
+
+partyGuests([
+  "7IK9Yo0h",
+  "9NoBUajQ",
+  "Ce8vwPmE",
+  "SVQXQCbc",
+  "tSzE5t0p",
+  "PARTY",
+  "9NoBUajQ",
+  "Ce8vwPmE",
+  "SVQXQCbc",
+]);
