8e00bcdaa42f5eef2b35b377dbb144b3a98383d0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-03 16:26:06 +0200

    Update 2.)OddOccurrences.js

---
+++ JS-Fundamentals-2023/Associative Arrays Exercise/2.)OddOccurrences.js
@@ -0,0 +1,5 @@
+//Task: Write a function that extracts the elements of a sentence, if it appears an odd number of times (case-insensitive).
+
+// The input comes as a single string. The words will be separated by a single space.
+
+//Solution:
