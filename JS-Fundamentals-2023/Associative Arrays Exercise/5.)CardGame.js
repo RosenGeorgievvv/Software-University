ce45874107c08cdfae45e1dbefcedec54edd6ac1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-03 21:29:56 +0200

    Update 5.)CardGame.js

--- JS-Fundamentals-2023/Associative Arrays Exercise/5.)CardGame.js
+++ JS-Fundamentals-2023/Associative Arrays Exercise/5.)CardGame.js
@@ -10,5 +10,23 @@
 
 // {personName}: {value}
 
-
 //Solution:
+
+function cardGame(arr) {
+
+
+    
+}
+cardGame([
+  "Peter: 2C, 4H, 9H, AS, QS",
+
+  "Tomas: 3H, 10S, JC, KD, 5S, 10S",
+
+  "Andrea: QH, QC, QS, QD",
+
+  "Tomas: 6H, 7S, KC, KD, 5S, 10C",
+
+  "Andrea: QH, QC, JS, JD, JC",
+
+  "Peter: JD, JD, JD, JD, JD, JD",
+]);
