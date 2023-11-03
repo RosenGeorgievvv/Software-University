2a70a81f5360807d5a7e12b3e453dc57c7eaba0a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-03 16:34:02 +0200

    Create 7.)MinerTask.js

---
+++ JS-Fundamentals-2023/Associative Arrays Exercise/7.)MinerTask.js
@@ -0,0 +1,9 @@
+//Task: You are given an array of strings. Every odd string is representing a resource (e.g. Gold, Silver, Copper, and so on), and every even – quantity. Your task is to collect the resources and print them each on a new line.
+
+// Print the resources and their quantities in the format:
+
+// {resource} –> {quantity}
+
+// The quantities inputs will be in the range [1 … 2 000 000 000].
+
+//Solution:
