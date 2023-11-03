5739cf23d277c403036022c56fbe4debeca21ab4
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-03 22:34:22 +0200

    Update 3.)Piccolo.js

--- JS-Fundamentals-2023/Associative Arrays Exercise/3.)Piccolo.js
+++ JS-Fundamentals-2023/Associative Arrays Exercise/3.)Piccolo.js
@@ -10,5 +10,47 @@
 
 // If the parking lot is empty, print: "Parking Lot is Empty".
 
-
 //Solution:
+
+function piccolo(input) {
+  let parkingLot = new Set();
+
+  for (let entry of input) {
+    let [direction, carNumber] = entry.split(", ");
+
+    if (direction === "IN") {
+      parkingLot.add(carNumber);
+    } else if (direction === "OUT") {
+      parkingLot.delete(carNumber);
+    }
+  }
+
+  let sortedCarNumbers = Array.from(parkingLot).sort();
+
+  if (sortedCarNumbers.length === 0) {
+    console.log("Parking Lot is Empty");
+  } else {
+    console.log(sortedCarNumbers.join("\n"));
+  }
+}
+piccolo([
+  "IN, CA2844AA",
+
+  "IN, CA1234TA",
+
+  "OUT, CA2844AA",
+
+  "IN, CA9999TT",
+
+  "IN, CA2866HI",
+
+  "OUT, CA1234TA",
+
+  "IN, CA2844AA",
+
+  "OUT, CA2866HI",
+
+  "IN, CA9876HH",
+
+  "IN, CA2822UU",
+]);
