e2660042655a8386ded8d214a8738a60e429d41c
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-03 16:27:12 +0200

    Update 3.)Piccolo.js

---
+++ JS-Fundamentals-2023/Associative Arrays Exercise/3.)Piccolo.js
@@ -0,0 +1,14 @@
+//Task:Write a function that:
+
+// · Records a car number for every car that enters the parking lot
+
+// · Removes a car number when the car goes out
+
+// · Input will be an array of strings in format [direction, carNumber]
+
+// Print the output with all car numbers which are in the parking lot sorted in ascending by number.
+
+// If the parking lot is empty, print: "Parking Lot is Empty".
+
+
+//Solution:
