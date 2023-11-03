2db266e460cd65cffe90fcf4ca2c0e198293cf1e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-03 16:25:26 +0200

    Update 1.)WordsTracker.js

---
+++ JS-Fundamentals-2023/Associative Arrays Exercise/1.)WordsTracker.js
@@ -0,0 +1,8 @@
+//Task: Write a function that receives an array of words and finds occurrences of given words in that sentence.
+
+// The input will come as an array of strings. The first string will contain the words you will be looking for separated by a space. All strings after that will be the words in which you will check for a match.
+
+// Print for each word how many times it occurs. The words should be sorted by count in descending.
+
+
+//Solution:
