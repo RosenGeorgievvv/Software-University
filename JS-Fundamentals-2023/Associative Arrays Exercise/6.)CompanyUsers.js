327c067a8b25e457ad91851d868f921b0ffd3425
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-03 16:31:30 +0200

    Update 6.)CompanyUsers.js

---
+++ JS-Fundamentals-2023/Associative Arrays Exercise/6.)CompanyUsers.js
@@ -0,0 +1,24 @@
+//Task: Write a function, which keeps the information about companies and their employees.
+
+// You will receive an array of strings containing the company name and employee's id. Add each employee to the given company. Keep in mind that a company cannot have two employees with the same id.
+
+// When you finish reading data, order the companies by their name in ascending order.
+
+// Print the company name and each employee's id in the following format:
+
+// {companyName}
+
+// -- {id1}
+
+// -- {id2}
+
+// -- {idN}
+
+// Input / Constraints
+
+// · The input come as array of strings, each in the format: "{companyName} -> {employeeId}".
+
+// · The input always will be valid.
+
+
+//Solution:
