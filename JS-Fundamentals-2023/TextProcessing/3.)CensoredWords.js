a8571d6c2afa7021a8a992e7a7af5116216a3d97
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-08 12:12:24 +0200

    Update 3.)CensoredWords.js

--- JS-Fundamentals-2023/TextProcessing/3.)CensoredWords.js
+++ JS-Fundamentals-2023/TextProcessing/3.)CensoredWords.js
@@ -1,3 +1,15 @@
 //Task: Write a function that receives a text as a first parameter and a single word as a second. Find all occurrences of that word in the text and replace them with the corresponding count of '*'.
 
 //Solution:
+
+function censoredWords(text, word) {
+  while (text.includes(word)) {
+    text = text.replace(word, "*".repeat(word.length));
+  }
+  console.log(text);
+}
+censoredWords(
+  "A small sentence with some words",
+
+  "small"
+);
