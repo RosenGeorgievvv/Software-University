a4c56b28dfa0cab67764f0667266858a6db1b21b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-09 11:09:02 +0200

    Update 2.)ModernTImesOf#.js

--- JS-Fundamentals-2023/TextProcessing Exercise/2.)ModernTImesOf#.js
+++ JS-Fundamentals-2023/TextProcessing Exercise/2.)ModernTImesOf#.js
@@ -3,3 +3,6 @@
 // Find all special words starting with #. If the found special word does not consist only of letters, then it is invalid and should not be printed.
 
 // Finally, print out all the special words you found without the label (#) on a new line.
+
+
+//Solution:
