ad47d57ecfebd13bfbe4426483af2c1fefbc6707
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-09 21:26:33 +0200

    Update 6.)PascalCaseSplitter.js

---
+++ JS-Fundamentals-2023/TextProcessing Exercise/6.)PascalCaseSplitter.js
@@ -0,0 +1,5 @@
+//Task: You will receive a single string.
+
+// This string is written in PascalCase format. Your task here is to split this string by every word in it.
+
+// Print them joined by comma and space.
