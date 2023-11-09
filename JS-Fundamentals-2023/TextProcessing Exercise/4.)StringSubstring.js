9ceee8a960127e3c8a4c7c68846f86150b437cc0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-09 16:28:39 +0200

    Update 4.)StringSubstring.js

---
+++ JS-Fundamentals-2023/TextProcessing Exercise/4.)StringSubstring.js
@@ -0,0 +1,5 @@
+//Task: The input will be given as two separated strings (a word as a first parameter and a text as a second).
+
+// Write a function that checks given text for containing a given word. The comparison should be case insensitive. Once you find a match, print the word and stop the program.
+
+// If you don't find the word print: "{word} not found!"
