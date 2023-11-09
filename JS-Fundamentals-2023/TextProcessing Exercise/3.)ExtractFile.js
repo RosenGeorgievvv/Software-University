e4a647909d5a7c77f4e1abc9efd465ec53537e70
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-09 12:36:55 +0200

    Update 3.)ExtractFile.js

--- JS-Fundamentals-2023/TextProcessing Exercise/3.)ExtractFile.js
+++ JS-Fundamentals-2023/TextProcessing Exercise/3.)ExtractFile.js
@@ -1,3 +1,6 @@
 //Task: Write a function that receives a single string - the path to a file (the '\' character is escaped)
 
 // Your task is to subtract the file name and its extension. (Beware of files like template.bak.pptx, as template.bak should be the file name, while pptx is the extension).
+
+
+//Solution:
