3427f4da890d09f5dc3c0393f54d4a80e01c8a2d
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-09 11:27:56 +0200

    Update 2.)ModernTImesOf#.js

--- JS-Fundamentals-2023/TextProcessing Exercise/2.)ModernTImesOf#.js
+++ JS-Fundamentals-2023/TextProcessing Exercise/2.)ModernTImesOf#.js
@@ -4,12 +4,15 @@
 
 // Finally, print out all the special words you found without the label (#) on a new line.
 
-
 //Solution:
 
-function modernTimes(str){
+function modernTimes(str) {
+  let splittedString = str.split(" ");
 
-    
-
+  for (let word of splittedString) {
+    if (word.includes("#") && word.length > 1) {
+        console.log(word.substring(1));
+    }
+  }
 }
-modernTimes('Nowadays everyone uses # to tag a #special word in #socialMedia')
+modernTimes("Nowadays everyone uses # to tag a #special word in #socialMedia");
