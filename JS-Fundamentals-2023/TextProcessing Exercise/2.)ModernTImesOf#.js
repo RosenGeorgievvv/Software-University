9f08fa31281aac3764ff33650f30f6af4e4e0967
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-09 11:08:58 +0200

    Update 2.)ModernTImesOf#.js

---
+++ JS-Fundamentals-2023/TextProcessing Exercise/2.)ModernTImesOf#.js
@@ -0,0 +1,5 @@
+//Task: The input will be a single string.
+
+// Find all special words starting with #. If the found special word does not consist only of letters, then it is invalid and should not be printed.
+
+// Finally, print out all the special words you found without the label (#) on a new line.
