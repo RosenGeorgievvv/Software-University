217447c8388b32cc4cfc4e5c825754bb29808b46
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-11 11:53:51 +0200

    Update 1.)RevealWords.js

--- JS-Fundamentals-2023/TextProcessing Exercise/1.)RevealWords.js
+++ JS-Fundamentals-2023/TextProcessing Exercise/1.)RevealWords.js
@@ -9,17 +9,34 @@
 //Solution:
 
 function revealWords(str, template) {
-
-let word = str.split(', ');
-let result = template.split(' ').map(t => {
-    if(t.includes('*')){
-        let a = t.length;
-        let matching = word.find(w => w.length === a);
-        return matching ? matching : t;
+  let word = str.split(", ");
+  let result = template.split(" ").map((t) => {
+    if (t.includes("*")) {
+      let a = t.length;
+      let matching = word.find((w) => w.length === a);
+      return matching ? matching : t;
     }
     return t;
-});
-console.log(result.join(' '));
+  });
+  console.log(result.join(" "));
 }
 revealWords(
-  "great", "softuni is ***** place for learning new programming languages");
+  "great",
+  "softuni is ***** place for learning new programming languages"
+);
+
+//second way:
+
+function revealWords(wordsStr, text) {
+  let words = wordsStr.split(", ");
+  for (let word of words) {
+    let repeatCount = word.length;
+    let template = "*".repeat(repeatCount);
+    text = text.replace(template, word);
+  }
+  console.log(text);
+}
+revealWords(
+  "great",
+  "softuni is ***** place for learning new programming languages"
+);
