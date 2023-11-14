105d528a4ba6f7a04e1fd85d640ae8a73354be47
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-14 14:31:08 +0200

    Create 3.)SoftUniBarIncome.js

---
+++ JS-Fundamentals-2023/RegularExpressions Exercise/3.)SoftUniBarIncome.js
@@ -0,0 +1,22 @@
+//Task: Let`s take a break and visit the game bar at SoftUni. It is about time for the people behind the bar to go home and you are the person who has to draw the line and calculate the money from the products that were sold throughout the day. Until you receive a line with the text "end of shift" you will be given lines of input. But before processing that line you have to do some validations first.
+
+// Each valid order should have a customer, product, count, and a price:
+
+// · Valid customer's name should be surrounded by '%' and must start with a capital letter, followed by lower-case letters
+
+// · Valid product contains any word character and must be surrounded by '<' and '>'
+
+// · Valid count is an integer, surrounded by '|'
+
+// · Valid price is any real number followed by '$'
+
+// The parts of a valid order should appear in the order given: customer, product, count, and price.
+
+// Between each part there can be other symbols, except ('|', '$', '%' and '.')
+
+// For each valid line print on the console: "{customerName}: {product} - {totalPrice}"
+
+// When you receive "end of shift" print the total amount of money for the day rounded to 2 decimal places in the following format: "Total income: {income}".
+
+
+//Solution:
