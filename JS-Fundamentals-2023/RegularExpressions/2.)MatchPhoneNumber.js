ec937c25f10f4f4e0277901b2553bae109123f46
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-14 13:12:16 +0200

    Update 2.)MatchPhoneNumber.js

--- JS-Fundamentals-2023/RegularExpressions/2.)MatchPhoneNumber.js
+++ JS-Fundamentals-2023/RegularExpressions/2.)MatchPhoneNumber.js
@@ -1,3 +1,4 @@
 //Task: Write a regular expression to match a valid phone number from Sofia. After you find all valid phones, print them on the console, separated by a comma and a space ", ".
 
 //Solution:
+
