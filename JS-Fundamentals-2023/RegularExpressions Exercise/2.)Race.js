773dda5d9b2769f6a80378af9536ced0ec683046
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-14 17:21:50 +0200

    Update 2.)Race.js

--- JS-Fundamentals-2023/RegularExpressions Exercise/2.)Race.js
+++ JS-Fundamentals-2023/RegularExpressions Exercise/2.)Race.js
@@ -6,6 +6,27 @@
 
 // 3rd place: {third racer}"
 
-
 //Solution:
 
+function race(input) {
+
+
+    
+}
+race([
+  "George, Peter, Bill, Tom",
+
+  "G4e@55or%6g6!68e!!@ ",
+
+  "R1@!3a$y4456@",
+
+  "B5@i@#123ll",
+
+  "G@e54o$r6ge#",
+
+  "7P%et^#e5346r",
+
+  "T$o553m&6",
+
+  "end of race",
+]);
