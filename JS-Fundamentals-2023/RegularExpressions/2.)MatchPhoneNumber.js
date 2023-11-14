25a0105d4f87621d134a8cb5f92db20a8e943ecf
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-14 14:58:23 +0200

    update

--- JS-Fundamentals-2023/RegularExpressions/2.)MatchPhoneNumber.js
+++ JS-Fundamentals-2023/RegularExpressions/2.)MatchPhoneNumber.js
@@ -3,15 +3,12 @@
 //Solution:
 
 function matchPhoneNumber(input) {
+  let regexp = /(?<!\d)[+]359([ -])2\1\d{3}\1\d{4}\b/g;
+  let validNames = [];
 
-    let regexp = /(?<!\d)[+]359([ -])2\1\d{3}\1\d{4}\b/g;
-    let validNames = [];
-
-    while((validName = regexp.exec(input)) !== null){
-        validNames.push(validName[0]);
-    }
-    console.log(validNames.join(', '));
-
-
+  while ((validName = regexp.exec(input)) !== null) {
+    validNames.push(validName[0]);
+  }
+  console.log(validNames.join(", "));
 }
 matchPhoneNumber("+359 2 222 2222", "+359-2-222-2222");
--- JS-Fundamentals-2023/RegularExpressions/3.)MatchDates.js
+++ JS-Fundamentals-2023/RegularExpressions/3.)MatchDates.js
@@ -4,7 +4,17 @@
 
 function matchDates(input){
 
-    let regExp = /(?<day>\d{2})([\.\-\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})/g
+    let regExp = /(?<day>\d{2})([\.\-\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})/g;
+
+    let match;
+
+    while((match = regExp.exec(input)) !== null){
+        let day = match.groups.day;
+        let month = match.groups.month;
+        let year = match.groups.year;
+
+        console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
+    }
 
 }
-matchDates()
+matchDates('13/Jul/1928', '10-Nov-1934', '25.Dec.1937')
