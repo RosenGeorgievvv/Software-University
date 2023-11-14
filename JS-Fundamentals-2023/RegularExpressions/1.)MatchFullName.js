6abdc21490f9b3f40eb2794b1746a23b58bbbae8
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-14 14:48:05 +0200

    update

--- JS-Fundamentals-2023/RegularExpressions/1.)MatchFullName.js
+++ JS-Fundamentals-2023/RegularExpressions/1.)MatchFullName.js
@@ -13,3 +13,15 @@ function matchFullName(input) {
   console.log(validNames.join(" "));
 }
 matchFullName("Ivan Ivanov");
+
+
+//secondway:
+
+// function matchFullName(text) {
+//     let regEx = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
+//     let matches = text.match(regEx);
+
+//     for (let name of matches) {
+//         console.log(name);
+//     }
+// }
--- JS-Fundamentals-2023/RegularExpressions/2.)MatchPhoneNumber.js
+++ JS-Fundamentals-2023/RegularExpressions/2.)MatchPhoneNumber.js
@@ -4,7 +4,13 @@
 
 function matchPhoneNumber(input) {
 
-    
+    let regexp = /(?<!\d)[+]359([ -])2\1\d{3}\1\d{4}\b/g;
+    let validNames = [];
+
+    while((validName = regexp.exec(input)) !== null){
+        validNames.push(validName[0]);
+    }
+    console.log(validNames.join(', '));
 
 
 }
