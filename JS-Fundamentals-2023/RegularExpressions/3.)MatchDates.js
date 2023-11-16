e630a842d3639c6ed1d4177a3285a30005348239
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-16 12:46:40 +0200

    Update 3.)MatchDates.js

--- JS-Fundamentals-2023/RegularExpressions/3.)MatchDates.js
+++ JS-Fundamentals-2023/RegularExpressions/3.)MatchDates.js
@@ -18,5 +18,24 @@ function matchDates(input) {
 }
 matchDates("13/Jul/1928", "10-Nov-1934", "25.Dec.1937");
 
+// the match can be assigned in the variable and the while can be changed in the following way:
 
+// function solve(input){
+
+//   let pattern = /(?<day>\d{2})([\.\-\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})\b/g;
+
+// declare the match variable and check if the input matches the regexp
+//   let match = pattern.exec(input);
+
+//   while(match !== null){
+//       let day = match.groups.day;
+//       let month = match.groups.month;
+//       let year = match.groups.year;
+
+//       console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
+
+// We have to call it again. If we do not perform this action, we will run into an infinity loop.
+//       match = pattern.exec(input);
+//   }
+// }
 
