ea17dc3a3afb6718bcd9e4b257dff79f9d8e08b4
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-20 16:34:16 +0200

    Update 6.)P!rates.js

--- JS-Fundamentals-2023/Final Exam Preparation/6.)P!rates.js
+++ JS-Fundamentals-2023/Final Exam Preparation/6.)P!rates.js
@@ -63,3 +63,24 @@
 // · The town names in the events will always be valid towns that should be on your list.
 
 //Solution:
+
+function pirates(input) {
+
+
+    
+}
+pirates([
+  "Tortuga||345000||1250",
+
+  "Santo Domingo||240000||630",
+
+  "Havana||410000||1100",
+
+  "Sail",
+
+  "Plunder=>Tortuga=>75000=>380",
+
+  "Prosper=>Santo Domingo=>180",
+
+  "End",
+]);
