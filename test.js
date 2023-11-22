dda20de24bdb71ad804194a96187c3600071228f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-22 15:21:03 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,21 +1,4 @@
-function secretChat(arr) {
-    let message = arr.shift();
-  
-    for (let i = 0; i < arr.length; i++) {
-      let currentRow = arr[i].split(":|:");
-  
-      if (currentRow[0] === "ChangeAll") {
-        let substr = currentRow[1];
-        let str = currentRow[2];
-  
-        while (message.includes(substr)) {
-          let index = message.indexOf(substr);
-          let cut = message.substring(index, index + substr.length);
-          message = message.replace(cut, str);
-        }
-        console.log(message);
-      } 
-  }
+
 solve([
   "heVVodar!gniV",
 
