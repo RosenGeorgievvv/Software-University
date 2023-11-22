d133c273781eb844de4d81497ffdb4c3e6254e82
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-22 18:06:30 +0200

    Update 6.)ThePianist.js

--- JS-Fundamentals-2023/Final Exam Preparation/6.)ThePianist.js
+++ JS-Fundamentals-2023/Final Exam Preparation/6.)ThePianist.js
@@ -1,5 +1,20 @@
 //Task:
 
-
-
 //Solution:
+
+function thePianist(arr) {
+
+    
+}
+thePianist([
+  "3",
+  "Fur Elise|Beethoven|A Minor",
+  "Moonlight Sonata|Beethoven|C# Minor",
+  "Clair de Lune|Debussy|C# Minor",
+  "Add|Sonata No.2|Chopin|B Minor",
+  "Add|Hungarian Rhapsody No.2|Liszt|C# Minor",
+  "Add|Fur Elise|Beethoven|C# Minor",
+  "Remove|Clair de Lune",
+  "ChangeKey|Moonlight Sonata|C# Major",
+  "Stop",
+]);
