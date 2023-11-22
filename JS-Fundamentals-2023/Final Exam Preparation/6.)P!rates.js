fdac8b61cbbef30a5e540459d4214e97a491a821
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-22 17:49:47 +0200

    Update 6.)P!rates.js

--- JS-Fundamentals-2023/Final Exam Preparation/6.)P!rates.js
+++ JS-Fundamentals-2023/Final Exam Preparation/6.)P!rates.js
@@ -155,16 +155,10 @@ function pirates(input) {
 }
 pirates([
   "Tortuga||345000||1250",
-
   "Santo Domingo||240000||630",
-
   "Havana||410000||1100",
-
   "Sail",
-
   "Plunder=>Tortuga=>75000=>380",
-
   "Prosper=>Santo Domingo=>180",
-
   "End",
 ]);
