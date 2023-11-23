799d36bf74f38165799261b79a4c327489961956
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-23 16:07:30 +0200

    Update test.js

--- test.js
+++ test.js
@@ -9,13 +9,6 @@ function imitationGame(arr) {
         let replaceStr = secondEl;
         message = message.split(removeStr).join(replaceStr);
       }
-    } else if (command === "Insert") {
-      let index = Number(firstEl);
-      let value = secondEl;
-
-      let firstCut = message.substr(0, index);
-      let secondCut = message.substr(index);
-      message = firstCut.concat(value, secondCut);
     } 
 }
 imitationGame(["zzHe", "ChangeAll|z|l", "Insert|2|o", "Move|3", "Decode"]);
