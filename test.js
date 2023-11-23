f0bb3fc1377c22227356d9b4a40ed37a206747e1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-23 16:07:38 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,6 @@
 function imitationGame(arr) {
   let message = arr.shift();
 
-  for (let line of arr) {
-    let [command, firstEl, secondEl] = line.split("|");
-    if (command === "ChangeAll") {
-      if (message.includes(firstEl)) {
-        let removeStr = firstEl;
-        let replaceStr = secondEl;
-        message = message.split(removeStr).join(replaceStr);
-      }
-    } 
+
 }
 imitationGame(["zzHe", "ChangeAll|z|l", "Insert|2|o", "Move|3", "Decode"]);
