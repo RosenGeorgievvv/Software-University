a9a0404a2bec84e66b241010bd9863a938a2043c
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-23 16:40:49 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
-function adAstra(str) {
-  let textString = str[0];
-  let calsPerDay = 2000;
 
-  let pattern = /(\||#)([A-Za-z ]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d+)\1/g;
-}
-
-adAstra([
-  "#Bread#19/03/21#4000#|Invalid|03/03.20||Apples|08/10/20|200||Carrots|06/08/20|500||Not right|6.8.20|5|",
-]);
