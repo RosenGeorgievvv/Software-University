e55a4019c151975963d6c2492c66165d2985ce0b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-11-23 16:36:45 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,31 @@ function adAstra(str) {
 let textString = str[0];
 let calsPerDay = 2000;
 
+let pattern = /(\||#)([A-Za-z ]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d+)\1/g;
 
+let possibleMatch;
+let allFood = [];
+
+while((possibleMatch = pattern.exec(textString))){
+    allFood.push(possibleMatch);
+}
+
+let allCalories = 0;
+
+for(let food of allFood){
+    let currentCalories = Number(food[4]);
+    allCalories += currentCalories;
+}
+let foodForAllDays = allCalories / calsPerDay;
+console.log(`You have food to last you for: ${Math.floor(foodForAllDays)} days!`);
+
+for(let items of allFood){
+    let product = items[2];
+    let date = items[3];
+    let cals = items[4];
+
+    console.log(`Item: ${product}, Best before: ${date}, Nutrition: ${cals}`);
+}
 
 
 
