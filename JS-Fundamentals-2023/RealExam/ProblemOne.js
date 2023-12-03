7927c8666563e6aa1c5823f99890d059fe71f464
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-03 10:22:03 +0200

    update

---
+++ JS-Fundamentals-2023/RealExam/ProblemOne.js
@@ -0,0 +1,19 @@
+function passwordValidator(arr) {
+
+    
+}
+passwordValidator([
+  "invalidpassword*",
+
+  "Add 2 p",
+
+  "Replace i -50",
+
+  "Replace * 10",
+
+  "Make Upper 2",
+
+  "Validation",
+
+  "Complete",
+]);
