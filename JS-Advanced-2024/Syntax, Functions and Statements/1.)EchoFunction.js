7298fd05df4b452f5218683c8402f22be68b41e8
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-12 11:21:36 +0200

    Update 1.)EchoFunction.js

--- JS-Advanced-2024/Syntax, Functions and Statements/1.)EchoFunction.js
+++ JS-Advanced-2024/Syntax, Functions and Statements/1.)EchoFunction.js
@@ -1,3 +1,9 @@
 //Task: Write a JS function that takes one string parameter and prints on two lines the length of the parameter and then the unchanged parameter itself.
 
 //Solution:
+
+function echoFunc(str){
+
+
+}
+echoFunc('Hello, JavaScript!')
