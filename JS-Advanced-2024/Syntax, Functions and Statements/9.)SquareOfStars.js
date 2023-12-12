c3faa524a2d054bd73eef2b813f9645e82fbf514
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-12 11:07:32 +0200

    Update 9.)SquareOfStars.js

---
+++ JS-Advanced-2024/Syntax, Functions and Statements/9.)SquareOfStars.js
@@ -0,0 +1,3 @@
+//Task: Write a function that prints a rectangle made of stars with variable size, depending on an input parameter. If there is no parameter specified, the rectangle should always be of size 5. Look at the examples to get an idea.
+// The input comes as a single number argument.
+// The output is a series of lines printed on the console, forming a rectangle of variable size.
