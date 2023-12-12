62e49c3a77dfe535c83f4a483416fa1c0bcfe563
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-12 11:04:23 +0200

    Update 4.)CircleArea.js

--- JS-Advanced-2024/Syntax, Functions and Statements/4.)CircleArea.js
+++ JS-Advanced-2024/Syntax, Functions and Statements/4.)CircleArea.js
@@ -1,3 +1,6 @@
-//Task:
+//Task: Write a function that takes a single argument as an input. Check the type of input argument. If it is a number, assume it is the radius of a circle and calculate the circle area. Print the area rounded to two decimal places.
+// If the argument type is NOT a number, print the following text on the console: 
+// `We can not calculate the circle area, because we receive a {type of argument}.`
+
 
 //Solution:
