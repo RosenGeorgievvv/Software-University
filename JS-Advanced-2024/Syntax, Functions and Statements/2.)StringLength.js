977f2639bbf10c1bb5b5d59543b900e948c5daea
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-12 10:53:41 +0200

    Update 2.)StringLength.js

---
+++ JS-Advanced-2024/Syntax, Functions and Statements/2.)StringLength.js
@@ -0,0 +1,3 @@
+//Task: Write a JS function that takes three string arguments as an input. Calculate the sum of the length of the strings and the average length of the strings rounded down to the nearest integer.
+
+//Solution:
