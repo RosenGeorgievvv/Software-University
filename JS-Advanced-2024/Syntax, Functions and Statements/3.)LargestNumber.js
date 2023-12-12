97bfe5be1eafa8ab693585f51ed10e34cea11c25
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-12 10:55:05 +0200

    Update 3.)LargestNumber.js

--- JS-Advanced-2024/Syntax, Functions and Statements/3.)LargestNumber.js
+++ JS-Advanced-2024/Syntax, Functions and Statements/3.)LargestNumber.js
@@ -1,1 +1,3 @@
 //Task: Write a function that takes three number arguments as input and finds the largest of them. Print the following text on the console: `The largest number is {number}.`.
+
+//Solution:
