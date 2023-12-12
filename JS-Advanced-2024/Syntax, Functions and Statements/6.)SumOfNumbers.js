cd8c7e11b6ecd9c8f652811c1b1805da9ae06239
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-12 11:05:31 +0200

    Update 6.)SumOfNumbers.js

--- JS-Advanced-2024/Syntax, Functions and Statements/6.)SumOfNumbers.js
+++ JS-Advanced-2024/Syntax, Functions and Statements/6.)SumOfNumbers.js
@@ -1,3 +1,6 @@
 //Task: Write a JS function that takes two numbers n and m as an input and prints the sum of all numbers from n to m.
 // The input comes as two string elements that need to be parsed as numbers.
 // The output should return the sum.
+
+
+//Solution:
