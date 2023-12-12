341473d3f29eff348d869a7c0c394e375e2c7136
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-12 11:06:43 +0200

    Update 7.)DayOfWeek.js

--- JS-Advanced-2024/Syntax, Functions and Statements/7.)DayOfWeek.js
+++ JS-Advanced-2024/Syntax, Functions and Statements/7.)DayOfWeek.js
@@ -1,3 +1,6 @@
 //Task: Write a function that prints a number between 1 and 7 when a day of the week is passed to it as a string and an error message if the string is not recognized.
 // The input comes as a single-string argument.
 // The output should be returned as a result.
+
+
+//Solution:
