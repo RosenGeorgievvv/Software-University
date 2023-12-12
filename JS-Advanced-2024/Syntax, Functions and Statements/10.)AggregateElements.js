71ceca2139da943f5d9ff77e66a55a000dfee9de
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-12 11:08:11 +0200

    Update 10.)AggregateElements.js

---
+++ JS-Advanced-2024/Syntax, Functions and Statements/10.)AggregateElements.js
@@ -0,0 +1,6 @@
+//Task: Write a program that performs different operations on an array of elements. Implement the following operations:
+// •	Sum(ai) - calculates the sum of all elements from the input array
+// •	Sum(1/ai) - calculates the sum of the inverse values (1/ai) of all elements from the array
+// •	Concat(ai) - concatenates the string representations of all elements from the array
+// The input comes as an array of number elements.
+// The output should be printed on the console on a new line for each of the operations.
