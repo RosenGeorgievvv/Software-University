affb6858d1d3aaed4624d45cf5ce2204db2f1415
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-12 11:07:16 +0200

    Update 8.)DaysInAMonth.js

--- JS-Advanced-2024/Syntax, Functions and Statements/8.)DaysInAMonth.js
+++ JS-Advanced-2024/Syntax, Functions and Statements/8.)DaysInAMonth.js
@@ -1,3 +1,6 @@
 //Task: Write a JavaScript function to get the number of days in a month.
 // The input comes as two numeric parameters. The first element is the month, the second is the year.
 // The output must return the number of days in a month for a given year.
+
+
+//Solution:
