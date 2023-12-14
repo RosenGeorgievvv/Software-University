41f7932d445b4df541f108e80a83a50bd96fdbe0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-14 19:22:46 +0200

    Update 4.)CircleArea.js

--- JS-Advanced-2024/Syntax, Functions and Statements/4.)CircleArea.js
+++ JS-Advanced-2024/Syntax, Functions and Statements/4.)CircleArea.js
@@ -8,12 +8,13 @@ function circleArea(input) {
   let inputType = typeof input;
   let result;
 
-  
   if (inputType === "number") {
     result = Math.pow(input, 2) * Math.PI;
     console.log(result.toFixed(2));
   } else {
-    console.log( `We can not calculate the circle area, because we receive a ${inputType}.`);
+    console.log(
+      `We can not calculate the circle area, because we receive a ${inputType}.`
+    );
   }
 }
 circleArea(5);
