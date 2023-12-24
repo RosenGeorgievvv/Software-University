2c6048ccb6ffc4f47754fe226b99621c5fbb9f3c
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-24 16:12:09 +0200

    Update 5.)TimeToWalk.js

--- JS-Advanced-2024/Syntax, Functions and Statements Exercise/5.)TimeToWalk.js
+++ JS-Advanced-2024/Syntax, Functions and Statements Exercise/5.)TimeToWalk.js
@@ -1,6 +1,5 @@
-//Task: Write a function that calculates how long it takes a student to get to university.  The function takes three numbers:
+//Task: Write a function that calculates how long it takes a student to get to university. The function takes three numbers:
 // The first is the number of steps the student takes from their home to the university
 // Тhe second number is the length of the student's footprint in meters
 // Тhe third number is the student speed in km/h
 // Every 500 meters the student rests and takes a 1-minute break.
-// Calculate how long the student walks from home to university and print on the console the result in the following format: `hours:minutes:seconds`.
