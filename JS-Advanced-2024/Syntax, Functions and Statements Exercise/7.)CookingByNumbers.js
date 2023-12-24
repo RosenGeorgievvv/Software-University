646043da5a3781998ea52965317068329b584e29
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-24 16:14:19 +0200

    Update 7.)CookingByNumbers.js

--- JS-Advanced-2024/Syntax, Functions and Statements Exercise/7.)CookingByNumbers.js
+++ JS-Advanced-2024/Syntax, Functions and Statements Exercise/7.)CookingByNumbers.js
@@ -1,1 +1,7 @@
-//Task:
+//Task: Write a program that receives 6 parameters which are a number and a list of five operations. Perform the operations sequentially by starting with the input number and using the result of every operation as a starting point for the next one. Print the result of every operation in order. The operations can be one of the following:
+// chop - divide the number by two
+// dice - square root of a number
+// spice - add 1 to the number
+// bake - multiply number by 3
+// fillet - subtract 20% from the number
+// The input comes as 6 string elements. The first element is the starting point and must be parsed to a number. The remaining 5 elements are the names of the operations to be performed.
