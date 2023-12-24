8ba64e44fd6966e994621e1479eb8eba2886ae35
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-24 16:08:14 +0200

    Update 3.)SameNumbers.js

---
+++ JS-Advanced-2024/Syntax, Functions and Statements Exercise/3.)SameNumbers.js
@@ -0,0 +1,5 @@
+//Task: Write a function that takes an integer number as an input and check if all the digits in a given number are the same or not.
+// Print on the console true if all numbers are the same and false if not. On the next line print the sum of all digits.
+// The input comes as an integer number.
+
+//Solution:
