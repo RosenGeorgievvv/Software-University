340d388f3401220a479b5c8df8a6de50e2cca9e2
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-24 10:27:13 +0200

    Update 7.)DayOfWeek.js

--- JS-Advanced-2024/Syntax, Functions and Statements/7.)DayOfWeek.js
+++ JS-Advanced-2024/Syntax, Functions and Statements/7.)DayOfWeek.js
@@ -7,5 +7,14 @@
 
 function dayOfWeek(str){
 
+    switch(str){
+        case "Monday": console.log('1'); break;
+        case "Tuesday": console.log('2'); break;
+        case "Wednesday": console.log('3'); break;
+        case "Thursday": console.log('4'); break;
+        case "Friday": console.log('5'); break;
+       default: console.log("error"); break;
+    }
+
 }
 dayOfWeek('Monday')
