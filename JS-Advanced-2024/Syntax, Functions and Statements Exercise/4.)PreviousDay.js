7462274710662c4948fe27ca41024d9dcd862dc3
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-24 16:09:42 +0200

    Update 4.)PreviousDay.js

---
+++ JS-Advanced-2024/Syntax, Functions and Statements Exercise/4.)PreviousDay.js
@@ -0,0 +1,5 @@
+//Task: Write a JS function that calculates the date of the previous day by given year, month, and day.
+// The input comes as three numeric parameters. The first element is the year, the second is the month and the third is the day.
+// The output must be the return date of the previous day in the format: `{year}-{month}-{day}`
+
+//Solution:
