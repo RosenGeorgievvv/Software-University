042566625d2c5edcea1d65178c760c81820614d8
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-24 16:04:47 +0200

    Update 1.)Fruit.js

---
+++ JS-Advanced-2024/Syntax, Functions and Statements Exercise/1.)Fruit.js
@@ -0,0 +1,6 @@
+//Task: Write a function that calculates how much money you need to buy fruit. You will receive a string for the type of fruit you want to buy, a number for weight in grams, and another number for the price per kilogram. 
+// Print the following text on the console:  
+// `I need ${money} to buy {weight} kilograms {fruit}.`
+// Print the weight and the money rounded to two decimal places.
+
+//Solution:
