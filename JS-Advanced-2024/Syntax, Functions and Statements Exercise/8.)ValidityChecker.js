f82fa6830406f4988ed7033fbc22fe34a5323c0a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-27 12:00:19 +0200

    Update 8.)ValidityChecker.js

--- JS-Advanced-2024/Syntax, Functions and Statements Exercise/8.)ValidityChecker.js
+++ JS-Advanced-2024/Syntax, Functions and Statements Exercise/8.)ValidityChecker.js
@@ -16,6 +16,7 @@ function compareAndPrint(x1, y1, x2, y2) {
         return Number.isInteger(distance);
     }
 
+    
     if (isDistanceValid(x1, y1, 0, 0)) {
         console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
     } else {
