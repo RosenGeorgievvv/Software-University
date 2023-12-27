c255fbf4c7eaa12cad85ab6e68c976175d172c69
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-27 11:59:54 +0200

    update

--- JS-Advanced-2024/Syntax, Functions and Statements Exercise/8.)ValidityChecker.js
+++ JS-Advanced-2024/Syntax, Functions and Statements Exercise/8.)ValidityChecker.js
@@ -1,3 +1,37 @@
 //Task: Write a program that receives a total of 4 parameters in the format x1, y1, x2, y2. Check if the distance between each point (x, y) and the beginning of the Cartesian coordinate system (0, 0) is valid. A distance between two points is considered valid if it is an integer value. 
 
 //Solution:
+
+function compareAndPrint(x1, y1, x2, y2) {
+    const distance1 = calculateDistance(x1, y1, 0, 0);
+    const distance2 = calculateDistance(x2, y2, 0, 0);
+    const distance3 = calculateDistance(x1, y1, x2, y2);
+
+    function calculateDistance(x1, y1, x2, y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+    
+    function isDistanceValid(x1, y1, x2, y2) {
+        const distance = calculateDistance(x1, y1, x2, y2);
+        return Number.isInteger(distance);
+    }
+
+    if (isDistanceValid(x1, y1, 0, 0)) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
+
+    if (isDistanceValid(x2, y2, 0, 0)) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+
+    if (isDistanceValid(x1, y1, x2, y2)) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+}
+compareAndPrint(3, 0, 0, 4)
