e9c24baf1dc40d0e8196254c8f30eff67c74726d
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-27 12:01:43 +0200

    Update 7.)CookingByNumbers.js

--- JS-Advanced-2024/Syntax, Functions and Statements Exercise/7.)CookingByNumbers.js
+++ JS-Advanced-2024/Syntax, Functions and Statements Exercise/7.)CookingByNumbers.js
@@ -9,31 +9,7 @@
 //Solution:
 
 function cookingTask(input) {
-    let num = Number(input[0]);
- 
-    for (let i = 1; i < input.length; i++) {
- 
-        if (input[i] === 'chop') {
-            num = num / 2;
-            console.log(num);
-        }
-        else if (input[i] === 'dice') {
-            num = Math.sqrt(num);
-            console.log(num);
-        }
-        else if (input[i] === 'spice') {
-            num += 1;
-            console.log(num);
-        }
-        else if (input[i] === 'bake') {
-            num *= 3;
-            console.log(num);
-        }
-        else  {
-            num = num - 0.2 * num;
-            console.log(num);
-        }
-    }
+
 }
 
 cookingTask('32', 'chop', 'chop', 'chop', 'chop', 'chop');
