c7b683162e4bfb4ff79760487e500a30c33092d0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-29 18:39:04 +0200

    Update 2.)LastKNumbersSequence.js

--- JS-Advanced-2024/Arrays and Nested Arrays /2.)LastKNumbersSequence.js
+++ JS-Advanced-2024/Arrays and Nested Arrays /2.)LastKNumbersSequence.js
@@ -7,8 +7,14 @@
 
 //Solution:
 
-function lastKNumbers(n, k){
+function lastKNumbers(n, k) {
 
-
+    let result = [1];
+    for (let i = 1; i < n; i++) {
+        let startingNumber = Math.max(0, i - k);
+        let currentNumber = result.slice(startingNumber, startingNumber + k).reduce((acc, val) => acc + val, 0);
+        result.push(currentNumber);
+    }
+    console.log(`[${result.join(', ')}]`);
 }
 lastKNumbers(6, 3);
