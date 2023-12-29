e71ca65d12c84c0ae0b33e0fb75006bb1f9da631
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-29 12:53:06 +0200

    Update 2.)LastKNumbersSequence.js

---
+++ JS-Advanced-2024/Arrays and Nested Arrays /2.)LastKNumbersSequence.js
@@ -0,0 +1,8 @@
+//Task: You are given two integers n and k. Write a JS function that generates and return the following sequence:
+// The first element is 1
+// Every following element equals the sum of the previous k elements
+// The length of the sequence is n elements
+// The input comes as two number arguments. The first element represents the number n, and the second – the number k.
+// The output is the return value of your function and should be an array of numbers.
+
+//Solution:
