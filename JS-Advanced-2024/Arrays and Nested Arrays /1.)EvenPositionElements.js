8795695254e2c63e119d7de058a0a40a1427dd24
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-29 12:50:31 +0200

    Update 1.)EvenPositionElements.js

---
+++ JS-Advanced-2024/Arrays and Nested Arrays /1.)EvenPositionElements.js
@@ -0,0 +1,4 @@
+//Task: Write a function that finds the elements at even positions in an array.
+// The input comes as an array of string elements.
+// The output is printed on the console. Collect all elements in a string, separated by space.
+
