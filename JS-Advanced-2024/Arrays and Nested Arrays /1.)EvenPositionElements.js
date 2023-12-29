d68e64360a00a740060404c75e2859b1886b3ec7
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-12-29 18:17:53 +0200

    Update 1.)EvenPositionElements.js

--- JS-Advanced-2024/Arrays and Nested Arrays /1.)EvenPositionElements.js
+++ JS-Advanced-2024/Arrays and Nested Arrays /1.)EvenPositionElements.js
@@ -6,6 +6,10 @@
 
 function evenElements(arr){
 
-
+    let result = " ";
+    for(let i = 0; i < arr.length; i+=2){
+        result += arr[i] + " "
+    }
+console.log(result);
 }
 evenElements(['20', '30', '40', '50', '60'])
