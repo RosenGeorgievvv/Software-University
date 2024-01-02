bc528271196ea69c7a26f255667326a731df2855
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-02 21:19:05 +0200

    Update 9.)BiggestElement.js

--- JS-Advanced-2024/Arrays and Nested Arrays /9.)BiggestElement.js
+++ JS-Advanced-2024/Arrays and Nested Arrays /9.)BiggestElement.js
@@ -1,1 +1,3 @@
-//Task:
+//Task: Write a function that finds the biggest element inside a matrix.
+// The input comes as an array of arrays, containing number elements (2D matrix of numbers).
+// The output is the return value of your function. Find the biggest element and return it.
