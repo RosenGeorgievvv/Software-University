c5c06261753125b156df286bb39b04c00b0238f0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-02 21:19:34 +0200

    Update 10.)DiagonalSums.js

--- JS-Advanced-2024/Arrays and Nested Arrays /10.)DiagonalSums.js
+++ JS-Advanced-2024/Arrays and Nested Arrays /10.)DiagonalSums.js
@@ -1,1 +1,3 @@
-//Task:
+//Task: A square matrix of numbers comes as an array of arrays, each array holding numbers. Write a function that finds the sum at the main and the secondary diagonals.
+// The input comes as an array of arrays, containing number elements (2D matrix of numbers).
+// The output is printed on the console, on a single line separated by space. First print the sum at the main diagonal, then the sum at the secondary diagonal.
