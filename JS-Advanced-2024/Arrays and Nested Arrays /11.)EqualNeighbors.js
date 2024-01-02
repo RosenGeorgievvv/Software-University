8fb70bc8dd947b72edbe8b9b8b5823496a3d5e1c
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-02 21:20:23 +0200

    Update 11.)EqualNeighbors.js

--- JS-Advanced-2024/Arrays and Nested Arrays /11.)EqualNeighbors.js
+++ JS-Advanced-2024/Arrays and Nested Arrays /11.)EqualNeighbors.js
@@ -1,1 +1,3 @@
-//Task:
+//Task: Write a function that finds the number of equal neighbour pairs inside a matrix of variable size and type (numbers or strings).
+// The input comes as an array of arrays, containing string elements (2D matrix of strings).
+// The output is the return value of your function. Save the number of equal pairs you find and return it.
