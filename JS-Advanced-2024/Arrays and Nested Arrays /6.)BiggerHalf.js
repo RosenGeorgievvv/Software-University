71f5628b1be811d446d0ff6b8cb4fcc843030fff
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-02 21:16:44 +0200

    Update 6.)BiggerHalf.js

--- JS-Advanced-2024/Arrays and Nested Arrays /6.)BiggerHalf.js
+++ JS-Advanced-2024/Arrays and Nested Arrays /6.)BiggerHalf.js
@@ -1,1 +1,3 @@
-//Task:
+//Task: You are given an array of numbers. Write a JS function that sorts the array in ascending order and returns a new array, containing only the second half of the input. If there is an odd number of elements in the input, always take the bigger half. For example, if the input array contains 4 elements, the output should be 2, and if the input is 5 – the output is 3.
+// The input comes as an array of number elements.
+// The output is the return value of the function and should be an array of numbers.
