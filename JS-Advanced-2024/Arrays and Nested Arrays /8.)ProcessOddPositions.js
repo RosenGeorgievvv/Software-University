f541f066b17496a8fadfeb2387efe11d458facc6
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-02 21:17:50 +0200

    Update 8.)ProcessOddPositions.js

--- JS-Advanced-2024/Arrays and Nested Arrays /8.)ProcessOddPositions.js
+++ JS-Advanced-2024/Arrays and Nested Arrays /8.)ProcessOddPositions.js
@@ -1,1 +1,3 @@
-//Task:
+//Task: You are given an array of numbers. Write a JS function that returns the elements at odd positions from the array, doubled and in reverse order.
+// The input comes as an array of number elements.
+// The output is the return on the console on a single line, separated by space.
