d26ff13f019f09fdeba9d8a675cd3c17348049cb
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-02 21:17:20 +0200

    Update 7.)PieceOfPie.js

--- JS-Advanced-2024/Arrays and Nested Arrays /7.)PieceOfPie.js
+++ JS-Advanced-2024/Arrays and Nested Arrays /7.)PieceOfPie.js
@@ -1,1 +1,5 @@
-//Task:
+//Task: Write a function that receives three parameters – an array of pie flavors as strings, two target flavors as strings. The result of the function should be a new array, containing a section of the original array, starting at the first flavor parameter, and ending at (and including) the second flavor parameter.
+// The input comes as three arguments:
+// An array of strings, representing pie flavours
+// Two more strings, representing the start and end of the section, respectively
+// The output is the return value of the function and should be an array of strings.
