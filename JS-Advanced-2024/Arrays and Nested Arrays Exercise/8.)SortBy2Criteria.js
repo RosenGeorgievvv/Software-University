856d7288a7c63a60207c5efd2c0615d7a23571c5
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-04 20:48:19 +0200

    Update 8.)SortBy2Criteria.js

--- JS-Advanced-2024/Arrays and Nested Arrays Exercise/8.)SortBy2Criteria.js
+++ JS-Advanced-2024/Arrays and Nested Arrays Exercise/8.)SortBy2Criteria.js
@@ -1,1 +1,5 @@
-//Task:
+//Task: Write a function that orders a given array of strings, by a length in ascending order as primary criteria, and by alphabetical value in ascending order as second criteria. The comparison should be case-insensitive.
+
+// The input comes as an array of strings.
+
+// The output is the elements of the ordered array of strings, printed each on a new line.
