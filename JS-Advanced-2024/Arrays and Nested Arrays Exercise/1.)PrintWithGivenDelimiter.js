139ed9094a6cd50bee0c881310b36d9b8ffc279f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-04 20:44:43 +0200

    Update 1.)PrintWithGivenDelimiter.js

--- JS-Advanced-2024/Arrays and Nested Arrays Exercise/1.)PrintWithGivenDelimiter.js
+++ JS-Advanced-2024/Arrays and Nested Arrays Exercise/1.)PrintWithGivenDelimiter.js
@@ -1,1 +1,2 @@
-//Task:
+//Task: The input comes as two parameters – an array of strings and a string. The second parameter is the delimiter.
+// The output is the elements of the array, printed on the console, each element separated from the others by the given delimiter.
