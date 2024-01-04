0f297fd692cb01871b9da240c3a16f98c08b6815
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-04 20:46:56 +0200

    Update 5.)ExtractFromArray.js

--- JS-Advanced-2024/Arrays and Nested Arrays Exercise/5.)ExtractFromArray.js
+++ JS-Advanced-2024/Arrays and Nested Arrays Exercise/5.)ExtractFromArray.js
@@ -1,1 +1,5 @@
-//Task:
+//Task: Write a function that extracts only those numbers that form a non-decreasing subset. In other words, you start from the first element and continue to the end of the given array of numbers. Any number which is LESS THAN the current biggest one is ignored, alternatively if it’s equal or higher than the current biggest one you set it as the current biggest one and you continue to the next number.
+
+// The input comes as an array of numbers.
+
+// The output is the processed array after the filtration, which should be a non-decreasing subset. Return the array of numbers.
