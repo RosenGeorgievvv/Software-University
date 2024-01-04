6d318968d2d3a1315023da087e545fee1731714d
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-04 20:48:43 +0200

    Update 9.)MagicMatrices.js

--- JS-Advanced-2024/Arrays and Nested Arrays Exercise/9.)MagicMatrices.js
+++ JS-Advanced-2024/Arrays and Nested Arrays Exercise/9.)MagicMatrices.js
@@ -1,1 +1,5 @@
-//Task:
+//Task: Write a function that checks if a given matrix of numbers is magical. A matrix is magical if the sums of the cells of every row and every column are equal.
+
+// The input comes as an array of arrays, containing numbers (number 2D matrix). The input numbers will always be positive.
+
+// The output is a Boolean result indicating whether the matrix is magical or not.
