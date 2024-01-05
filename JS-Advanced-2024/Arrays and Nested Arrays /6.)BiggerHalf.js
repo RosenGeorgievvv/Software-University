e13315b50e648b796971710970a41869f45915a0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-05 16:45:57 +0200

    Update 6.)BiggerHalf.js

--- JS-Advanced-2024/Arrays and Nested Arrays /6.)BiggerHalf.js
+++ JS-Advanced-2024/Arrays and Nested Arrays /6.)BiggerHalf.js
@@ -7,7 +7,11 @@
 
 function biggerHalf(arr){
 
-    
+    arr.sort((a, b) => a - b);
+    let mid = Math.floor(arr.length / 2);
+    let result = arr.slice(mid);
+
+    return result
     
 }
 biggerHalf([4, 7, 2, 5])
