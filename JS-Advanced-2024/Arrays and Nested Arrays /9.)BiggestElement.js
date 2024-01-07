9799f48d08ccb5524d653c30c342edefe7c146d7
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-07 12:00:02 +0200

    Update 9.)BiggestElement.js

--- JS-Advanced-2024/Arrays and Nested Arrays /9.)BiggestElement.js
+++ JS-Advanced-2024/Arrays and Nested Arrays /9.)BiggestElement.js
@@ -4,9 +4,18 @@
 
 //Solution:
 
-function biggestEl(arr){
+function biggestEl(arr) {
 
+    let result = Number.NEGATIVE_INFINITY;
 
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] > result) {
+                result = arr[i][j];
+            }
+        }
+    }
+    return result
 }
-biggestEl([[20, 50, 10],
-    [8, 33, 145]])
+console.log(biggestEl([[20, 50, 10],
+[8, 33, 145]]))
