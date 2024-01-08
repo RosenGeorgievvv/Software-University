86eb03d4386982a920cc8a8bc7f40e41346b1806
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-08 20:46:29 +0200

    Update 3.)AddRemoveElement.js

--- JS-Advanced-2024/Arrays and Nested Arrays Exercise/3.)AddRemoveElement.js
+++ JS-Advanced-2024/Arrays and Nested Arrays Exercise/3.)AddRemoveElement.js
@@ -12,6 +12,7 @@ function addRemove(arr){
     let result = [];
     let currentNum = 1;
 
+    
     for (let command of arr) {
         if (command === "add") {
             result.push(currentNum);
