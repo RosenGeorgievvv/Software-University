892eaa0613b618a6d914cc13fb1ffaa62474641b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-09 19:31:26 +0200

    Update 5.)MathOperations.js

--- JS-Advanced-2024/Syntax, Functions and Statements/5.)MathOperations.js
+++ JS-Advanced-2024/Syntax, Functions and Statements/5.)MathOperations.js
@@ -20,7 +20,6 @@ function mathOperations(numOne, numTwo, str) {
         case "*": sum = numOne * numTwo; break;
         case "%": sum = numOne % numTwo; break;
         case "**": sum = numOne ** numTwo; break;
-        
     }
     console.log(sum);
 }
