3ff5a6262d922a1f3b6d530369c141ece7667f07
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-14 14:35:43 +0200

    Update 6.)ListOfNames.js

--- JS-Advanced-2024/Arrays and Nested Arrays Exercise/6.)ListOfNames.js
+++ JS-Advanced-2024/Arrays and Nested Arrays Exercise/6.)ListOfNames.js
@@ -4,6 +4,11 @@
 
 function namesList(arr){
 
+    arr.sort((a, b) => a.localeCompare(b));
+
+   for(let i = 0; i < arr.length; i++){
+    console.log(`${i + 1}.${arr[i]}`);
+   }
 
 }
 namesList(["John",
