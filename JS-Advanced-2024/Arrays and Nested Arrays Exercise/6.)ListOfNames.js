fd2b82e4677e363b739a83bd588a68062598c54d
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-14 14:31:02 +0200

    Update 6.)ListOfNames.js

--- JS-Advanced-2024/Arrays and Nested Arrays Exercise/6.)ListOfNames.js
+++ JS-Advanced-2024/Arrays and Nested Arrays Exercise/6.)ListOfNames.js
@@ -1,3 +1,15 @@
 //Task: You will receive an array of names. Sort them alphabetically in ascending order and print a numbered list of all the names, each on a new line.
 
 //Solution:
+
+function namesList(arr){
+
+
+}
+namesList(["John",
+
+"Bob",
+
+"Christina",
+
+"Ema"])
