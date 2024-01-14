9f864af5879d46b2ae0168a5a314f5e97d07935b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-14 14:51:22 +0200

    Update 7.)SortingNumbers.js

--- JS-Advanced-2024/Arrays and Nested Arrays Exercise/7.)SortingNumbers.js
+++ JS-Advanced-2024/Arrays and Nested Arrays Exercise/7.)SortingNumbers.js
@@ -8,7 +8,6 @@ function sortNumbers(arr){
 
     arr.sort((a, b) => a - b);
 
-    
  let result = arr.reduce((acc, value, index, array) => {
      let nextValue = index % 2 === 0 ? array[index / 2] : array[array.length - 1 - (index - 1) / 2];
         return acc.concat(nextValue);
