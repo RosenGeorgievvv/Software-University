d73b88d861cde8c6795d490e2363a190556459ac
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-15 18:06:48 +0200

    Update 4.)ObjectFactory.js

--- JS-Advanced-2024/Object and Composition/4.)ObjectFactory.js
+++ JS-Advanced-2024/Object and Composition/4.)ObjectFactory.js
@@ -30,3 +30,16 @@
 
 
 //Solution:
+
+function factory(library, orders){
+    let result = [];
+
+    for(let order of orders){
+        let current = Object.assign({}, order.template);
+        for(let part of order.parts){
+            current[part] = library[part];
+        }
+        result.push(current)
+    }
+    return result;
+}
