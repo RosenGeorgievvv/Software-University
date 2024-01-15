df93954d73d3de2a909a9cbe9636120dc14ddd94
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-15 11:06:35 +0200

    Update 6.)JSONtoHMTLTable.js

---
+++ JS-Advanced-2024/Object and Composition/6.)JSONtoHMTLTable.js
@@ -0,0 +1,18 @@
+//Task: You’re tasked with creating an HTML table of students and their scores. You will receive a single string representing an array of objects, the table’s headings should be equal to the object’s keys, while each object’s values should be a new entry in the table. Any text values in an object should be escaped, to avoid introducing dangerous code into the HTML.
+
+// Input
+
+// The input comes with a single string argument (the array of objects).
+
+// Output
+
+// The output should be printed on the console – for each entry row in the input print the object representing it.
+
+// Note: Objects’ keys will always be the same. Check more information for the HTML Entity here.
+
+// HTML
+
+// You are provided with an HTML file to test your table in the browser.
+
+
+//Solution:
