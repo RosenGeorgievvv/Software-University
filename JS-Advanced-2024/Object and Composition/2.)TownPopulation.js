9236ac43536120984015c2a6ad57d4bd2662b9ec
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-15 11:17:01 +0200

    Update 2.)TownPopulation.js

--- JS-Advanced-2024/Object and Composition/2.)TownPopulation.js
+++ JS-Advanced-2024/Object and Composition/2.)TownPopulation.js
@@ -12,3 +12,30 @@
 
 
 //Solution:
+
+function townDetails(arr){
+
+    const towns = {};
+
+    for(let toStr of arr){
+        let [name, population] = toStr.split(" <-> ");
+        population = Number(population);
+
+        if(towns[name] != undefined){
+            population += towns[name];
+        }
+        towns[name] = population;
+    }
+    for(let town in towns){
+console.log(`${town} : ${towns[town]}`);
+}
+}
+townDetails(['Sofia <-> 1200000',
+
+'Montana <-> 20000',
+
+'New York <-> 10000000',
+
+'Washington <-> 2345000',
+
+'Las Vegas <-> 1000000'])
