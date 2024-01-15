cb5c67fccf4b8033ad171ac766c27c5586596483
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-15 11:02:42 +0200

    Update 1.)CityRecord.js

---
+++ JS-Advanced-2024/Object and Composition/1.)CityRecord.js
@@ -0,0 +1,3 @@
+//Task: You will receive a city’s name (string), population (number), and treasury (number) as arguments, which you will need to set as properties of an object and return it.
+
+//Solution:
