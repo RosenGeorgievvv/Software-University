56c2352c66d81e69d8d959f18038436c519553fa
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-15 11:03:58 +0200

    Update 2.)TownPopulation.js

---
+++ JS-Advanced-2024/Object and Composition/2.)TownPopulation.js
@@ -0,0 +1,14 @@
+//Task: You have been tasked to create a registry for different towns and their population.
+
+// Input
+
+// The input comes as array of strings. Each element will contain data for a town and its population in the following format: "{townName} <-> {townPopulation}"
+
+// If you receive the same town twice, you should add the given population to the current one.
+
+// Output
+
+// As output, you must print all the towns and their population.
+
+
+//Solution:
