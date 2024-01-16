d12ec42406a6e484a7470c861d9b54238a5062e5
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-16 12:43:24 +0200

    Create 9.)Heroes.js

---
+++ JS-Advanced-2024/Object and Composition Exercise/9.)Heroes.js
@@ -0,0 +1,11 @@
+//Task: Create a function that returns an object with 2 methods (mage and fighter). This object should be able to create heroes (fighters and mages). Every hero has a state.
+
+// · Fighters have a name, health = 100, and stamina = 100 and every fighter can fight. When he fights his stamina decreases by 1 and the following message is printed on the console:
+
+// `${fighter's name} slashes at the foe!`
+
+// · Mages also have state (name, health = 100 and mana = 100). Every mage can cast spells. When a spell is cast the mage's mana decreases by 1 and the following message is printed on the console:
+
+// `${mage's name} cast ${spell}`
+
+//Solution:
