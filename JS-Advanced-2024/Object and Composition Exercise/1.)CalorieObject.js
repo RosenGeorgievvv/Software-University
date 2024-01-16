19265af7505569d556fd7c80e73b596043677b77
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-16 12:35:30 +0200

    Update 1.)CalorieObject.js

--- JS-Advanced-2024/Object and Composition Exercise/1.)CalorieObject.js
+++ JS-Advanced-2024/Object and Composition Exercise/1.)CalorieObject.js
@@ -1,1 +1,8 @@
-//Task:
+//Task: Write a function that composes an object by given properties. The input comes as an array of strings. Every even index of the array represents the name of the food. Every odd index is a number that is equal to the calories in 100 grams of the given product. Assign each value to its corresponding property, and finally print the object.
+
+// The input comes as an array of string elements.
+
+// The output should be printed on the console.
+
+
+//Solution:
