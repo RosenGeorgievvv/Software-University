5067ccfa7bab912570cb52a1ba4de1ab4b5467ce
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-16 12:42:21 +0200

    Create 8.)Rectangle.js

---
+++ JS-Advanced-2024/Object and Composition Exercise/8.)Rectangle.js
@@ -0,0 +1,12 @@
+//Task: Write a function that creates and returns a rectangle object. The rectangle needs to have a width (Number), height (Number), and color (String) properties, which are set via arguments during creation, and a calcArea() method, that calculates and returns the rectangle’s area.
+
+// Input
+
+// The function will receive three valid parameters – width (Number), height (Number), and color (String).
+
+// Output
+
+// Your function must return an object with all properties and methods as described. The calcArea() method of the object should return a number. The first letter in the color must be upperCase().
+
+
+//Solution:
