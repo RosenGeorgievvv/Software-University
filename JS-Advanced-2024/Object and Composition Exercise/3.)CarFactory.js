0f8620bc0919c327a92fd2d2558683d002dfe254
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-16 12:37:26 +0200

    Update 3.)CarFactory.js

--- JS-Advanced-2024/Object and Composition Exercise/3.)CarFactory.js
+++ JS-Advanced-2024/Object and Composition Exercise/3.)CarFactory.js
@@ -1,1 +1,38 @@
-//Task:
+//Task: Write a program that assembles a car by giving requirements out of existing components. The client will place an order in the form of an object describing the car. You need to determine which parts to use to fulfill the client’s order. You have the following parts in storage:
+
+// An engine has power (given in horsepower) and volume (given in cubic centimeters). Both of these values are numbers. When selecting an engine, pick the smallest possible that still meets the requirements.
+
+// Small engine: { power: 90, volume: 1800 }
+
+// Normal engine: { power: 120, volume: 2400 }
+
+// Monster engine: { power: 200, volume: 3500 }
+
+// A carriage has a type and color. Both of these values are strings. You have two types of carriages in storage and can paint them any color.
+
+// Hatchback: { type: 'hatchback', color: <as required> }
+
+// Coupe: { type: 'coupe', color: <as required> }
+
+// The wheels will be represented by an array of 4 numbers, each number represents the diameter of the wheel in inches. The size can only be an odd number. Round down any requirements you receive to the nearest odd number.
+
+// Input
+
+// You will receive an object as an argument to your function. The format will be as follows:
+
+// { model: <model name>,
+
+// power: <minimum power>,
+
+// color: <color>,
+
+// carriage: <carriage type>,
+
+// wheelsize: <size> }
+
+// Output
+
+// Return the resulting car object as a result of your function. See the examples for details.
+
+
+//Solution:
