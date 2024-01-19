15672d9b5078787fc0733ea7db191909230b0e03
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-19 10:28:40 +0200

    Update EditElement.js

--- JS-Advanced-2024/DOM Introduction/01.Edit Element/EditElement.js
+++ JS-Advanced-2024/DOM Introduction/01.Edit Element/EditElement.js
@@ -3,6 +3,7 @@ function editElement(ref, match, replacer) {
 const content = ref.textContent;
 
 const matcher = new RegExp(match, 'g');
+
 const edited = content.replace(matcher, replacer);
 ref.textContent = edited
 }
