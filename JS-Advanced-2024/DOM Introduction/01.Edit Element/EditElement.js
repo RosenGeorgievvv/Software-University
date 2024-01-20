c1fdb14b573ea91a5f9f01757fcb2a71b80f7fb7
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-20 11:17:50 +0200

    Update EditElement.js

--- JS-Advanced-2024/DOM Introduction/01.Edit Element/EditElement.js
+++ JS-Advanced-2024/DOM Introduction/01.Edit Element/EditElement.js
@@ -5,3 +5,10 @@ const editing = content.replace(matching, replacer);
 ref.textContent = editing;
 
 }
+
+//secondway:
+
+function editElement(ref, match, replacer) {
+let content = ref.textContent;
+content.split(match).join(replacer)
+    }
