a814fde9ec7bb13f106e85ff790145e479691b18
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-20 12:00:56 +0200

    Update ShowMore.js

--- JS-Advanced-2024/DOM Introduction/04.Show More/ShowMore.js
+++ JS-Advanced-2024/DOM Introduction/04.Show More/ShowMore.js
@@ -2,3 +2,12 @@ function showText() {
     document.getElementById('text').style.display = 'inline';
     document.getElementById('more').style.display = 'none'
 }
+
+function showText() {
+    let text = document.querySelector("#text");
+    text.setAttribute('style', 'display:inline;');
+    
+    let more = document.querySelector("#more");
+    more.setAttribute('style', 'display:none;');
+    
+}
