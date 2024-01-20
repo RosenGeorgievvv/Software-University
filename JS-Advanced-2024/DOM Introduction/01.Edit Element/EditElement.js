46c77ea7968c65c1561b8d27ec05ac5cf9da2a53
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-20 11:17:57 +0200

    Update EditElement.js

--- JS-Advanced-2024/DOM Introduction/01.Edit Element/EditElement.js
+++ JS-Advanced-2024/DOM Introduction/01.Edit Element/EditElement.js
@@ -1,14 +1,14 @@
 function editElement(ref, match, replacer) {
-const content = ref.textContent;
-const matching = new RegExp(match, 'g');
-const editing = content.replace(matching, replacer);
-ref.textContent = editing;
+    const content = ref.textContent;
+    const matching = new RegExp(match, 'g');
+    const editing = content.replace(matching, replacer);
+    ref.textContent = editing;
 
 }
 
 //secondway:
 
 function editElement(ref, match, replacer) {
-let content = ref.textContent;
-content.split(match).join(replacer)
-    }
+    let content = ref.textContent;
+    content.split(match).join(replacer)
+}
