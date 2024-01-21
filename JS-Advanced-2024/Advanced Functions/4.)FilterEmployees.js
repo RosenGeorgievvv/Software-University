a6dbd8789831d7db87492ef616dcd8034bc35795
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-21 12:58:02 +0200

    Update 4.)FilterEmployees.js

--- JS-Advanced-2024/Advanced Functions/4.)FilterEmployees.js
+++ JS-Advanced-2024/Advanced Functions/4.)FilterEmployees.js
@@ -1,1 +1,1 @@
-//Task:
+//Task: Write a program that filters the employees of your company. You should print the result in a specific format. You will receive 2 parameters (data, criteria). You should parse the input, find all employees that fulfill the criteria, and print them.
