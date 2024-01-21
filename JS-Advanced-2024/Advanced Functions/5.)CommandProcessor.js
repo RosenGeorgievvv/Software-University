71882e8515c4f09324b6f2fd4a4fe853f59d1d65
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-21 12:58:44 +0200

    Update 5.)CommandProcessor.js

---
+++ JS-Advanced-2024/Advanced Functions/5.)CommandProcessor.js
@@ -0,0 +1,11 @@
+//Task: Write a program that keeps a string inside its context and can execute different commands that modify or print the string on the console.
+
+// append(string) - append the given string at the end of the internal string
+
+// removeStart(n) - remove the first n characters from the string, n is an integer
+
+// removeEnd(n) - remove the last n characters from the string, n is an integer
+
+// print - print the stored string on the console
+
+//Solution:
