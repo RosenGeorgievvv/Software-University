9e93ea6dc268ddcff15688b187d8f856b202f4e3
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-21 12:59:14 +0200

    Update 6.)ListProcessor.js

---
+++ JS-Advanced-2024/Advanced Functions/6.)ListProcessor.js
@@ -0,0 +1,10 @@
+//Task: Using a closure, create an inner object to process list commands. The commands supported should be the following:
+
+// · add <string> - adds the following string in an inner collection.
+
+// · remove <string> - removes all occurrences of the supplied <string> from the inner collection.
+
+// · print - prints all elements of the inner collection joined by ",".
+
+
+//Solution:
