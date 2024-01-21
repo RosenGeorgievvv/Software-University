e54203f6b0d8d26048b14c06d6aaec3313258cec
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-21 12:59:38 +0200

    Create 7.)Cars.js

---
+++ JS-Advanced-2024/Advanced Functions/7.)Cars.js
@@ -0,0 +1,12 @@
+//Task: Write a closure that can create and modify objects. All created objects should be kept and be accessible by name. You should support the following functionality:
+
+// · create <name> - creates an object with the supplied <name>
+
+// · create <name> inherits <parentName> - creates an object with the given <name>, that inherits from the parent object with the <parentName>
+
+// · set <name> <key> <value> - sets the property with key equal to <key> to <value> in the object with the supplied <name>.
+
+// · print <name> - prints the object with the supplied <name> in the format "<key1>:<value1>,<key2>:<value2>…" - the printing should also print all inherited properties from parent objects. Inherited properties should come after own properties.
+
+
+//Solution:
