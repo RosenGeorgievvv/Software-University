c0cbc4601d98fdf12f3cc579970916c3dd161c2f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-22 19:48:53 +0200

    Update solution.js

--- JS-Advanced-2024/DOM Introduction Exercise/06. Format the Text/solution.js
+++ JS-Advanced-2024/DOM Introduction Exercise/06. Format the Text/solution.js
@@ -5,11 +5,11 @@ function solve() {
 
   let inputSplit = inputArea.split('.').filter((para) => para.length > 0);
 
-  for(let i = 0; i < inputSplit.length; i+= 3){
+  for (let i = 0; i < inputSplit.length; i += 3) {
     let arr = [];
 
-    for(let j = 0; j < 3; j++){
-      if(inputSplit[i + j]){
+    for (let j = 0; j < 3; j++) {
+      if (inputSplit[i + j]) {
         arr.push(inputSplit[i + j]);
       }
     }
