9f5027ecb4dcac66c4de0766821f48f73e35259d
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-24 20:39:33 +0200

    Update app.js

--- JS-Advanced-2024/DOM Manipulations and Events/01. List-Of-Items/app.js
+++ JS-Advanced-2024/DOM Manipulations and Events/01. List-Of-Items/app.js
@@ -1,3 +1,15 @@
 function addItem() {
-   let placeInput = document.getElementById("newItemText")
+   let placeInput = document.getElementById("newItemText");
+
+   if(placeInput.value.length === 0){
+    return;
+   }
+
+   let newLiElement = document.createElement('li');
+
+   newLiElement.appendChild(document.createTextNode(placeInput));
+   document.getElementById('items').appendChild(newLiElement);
+
+   document.getElementById('newItemText').value = ''
+
 }
