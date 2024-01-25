78a444e678d2eccef333078e0748e22603ba2400
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-25 15:46:29 +0200

    Update

--- JS-Advanced-2024/DOM Manipulations and Events/05. Highlight-Active/app.js
+++ JS-Advanced-2024/DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,3 +1,23 @@
 function focused() {
-    console.log('TODO:...');
+
+    let div = document.getElementsByTagName('div')[0];
+
+    Array.from(div.getElementsByTagName('input')).forEach(el =>{
+        el.addEventListener('focus', focusOn);
+    });
+
+    Array.from(div.getElementsByTagName('input')).forEach(el =>{
+        el.addEventListener('blur', focusOut);
+    });
+
+    function focusOn(e){
+        let parentElement = e.target.parentNode;
+        parentElement.classList.add("focused");
+    }
+
+    function focusOut(e){
+        let parentElement = e.target.parentNode;
+        parentElement.classList.remove('focused')
+    }
+
 }
--- JS-Advanced-2024/DOM Manipulations and Events/06. Dynamic-Validation/app.js
+++ JS-Advanced-2024/DOM Manipulations and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    console.log('TODO:...');
+   
+    let regexp = /^([\w\-.]+)@([a-z]+)(\.[a-z]+)+$/;
+    let input = document.getElementById('email');
+
+    input.addEventListener('change', email);
+
+    function email(e){
+        if(regexp.test(e.target.value)){
+            e.target.removeAttribute('class');
+            return;
+        }
+        e.target.className = 'error';
+    }
 }
