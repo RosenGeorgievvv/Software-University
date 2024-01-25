807d1bb30c6a44c0d43dca95f8d862b2ca10f633
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-25 15:29:54 +0200

    Update app.js

--- JS-Advanced-2024/DOM Manipulations and Events/04. Mouse-Gradient/app.js
+++ JS-Advanced-2024/DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,16 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+
+    let gradientElement = document.getElementById('gradient');
+
+    gradientElement.addEventListener('mousemove', move);
+    gradientElement.addEventListener('mouseout', out);
+
+    function move(e){
+        let width = e.target.clientWidth;
+        let position = e.offsetX / ( width - 1);
+        let percent = Math.trunc(position * 100);
+        document.getElementById('result').textContent = percent + "%";
+    }
+
+    
 }
