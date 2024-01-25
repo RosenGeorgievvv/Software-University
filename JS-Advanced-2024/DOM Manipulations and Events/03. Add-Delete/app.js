a661b73b04c1458e3c76cc4eca7c44f413e083c5
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-25 15:24:01 +0200

    Update app.js

--- JS-Advanced-2024/DOM Manipulations and Events/03. Add-Delete/app.js
+++ JS-Advanced-2024/DOM Manipulations and Events/03. Add-Delete/app.js
@@ -1,3 +1,26 @@
 function addItem() {
-    //TODO...
+  
+    let newItemText = document.getElementById('newItemText')
+
+    let newLiElement = document.createElement('li');
+    newLiElement.textContent = newItemText.value;
+
+    let list = document.getElementById('items');
+    list.appendChild(newLiElement)
+
+    document.getElementById('newItemText').value = ''
+
+// Delete operation:
+   let deleteButton = document.createElement('a');
+   deleteButton.textContent = '[Delete]';
+   deleteButton.href = "#";
+   deleteButton.addEventListener('click', onDelete);
+   newLiElement.appendChild(deleteButton);
+
+   function onDelete(e){
+    let deleteBtn = e.target;
+    let liElement = deleteBtn.parentElement;
+    liElement.remove();
+   }
+
 }
