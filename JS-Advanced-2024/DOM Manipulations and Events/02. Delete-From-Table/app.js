0f3bf9a521d2993d6cd130b3fb9dc12e18549f1d
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-25 14:37:32 +0200

    Update app.js

--- JS-Advanced-2024/DOM Manipulations and Events/02. Delete-From-Table/app.js
+++ JS-Advanced-2024/DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -1,13 +1,22 @@
 function deleteByEmail() {
-let email = document.querySelector('[name="email"]');
-let rows = Array.from(document.querySelectorAll("#customers tbody tr"));
+    let email = document.querySelector('[name="email"]');
+    let rows = Array.from(document.querySelectorAll("#customers tbody tr"));
+    let success = false;
 
-
-for(let row of rows){
-    if(row.children[1].textContent === email.value){
-        row.remove();
-
+    for (let row of rows) {
+        if (row.children[1].textContent === email.value) {
+            row.remove();
+            success = true;
+        }
     }
-}
 
+    let output = document.getElementById('result');
+
+    if(success){
+        output.textContent = 'Deleted.';
+    }else{
+        output.textContent = 'Not found.'
+    }
+
+    document.querySelector('[name="email"]').value = ''
 }
