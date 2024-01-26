a9c9969c22dfaa508b11d42ee12b624b6ce1b332
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-26 10:49:52 +0200

    Update

--- JS-Advanced-2024/DOM Manipulations and Events Exercise/03. Locked-Profile/app.js
+++ JS-Advanced-2024/DOM Manipulations and Events Exercise/03. Locked-Profile/app.js
@@ -1,20 +1,20 @@
 function lockedProfile() {
-   let buttons = Array.from(document.querySelectorAll('button'));
+    let buttons = Array.from(document.querySelectorAll('button'));
 
-   buttons.forEach(x => x.addEventListener('click', onClickHandler));
+    buttons.forEach(x => x.addEventListener('click', onClickHandler));
 
-   function onClickHandler(e){
-    let hiddenInfo = e.currentTarget.parentElement.querySelector('div');
-    let currentRadioBtn = e.target.parentElement.querySelector("input[type='radio']:checked");
+    function onClickHandler(e) {
+        let hiddenInfo = e.currentTarget.parentElement.querySelector('div');
+        let currentRadioBtn = e.target.parentElement.querySelector("input[type='radio']:checked");
 
-    if(currentRadioBtn.value === 'unlock'){
-        if(e.currentTarget.textContent === 'show more'){
-            e.currentTarget.textContent = "Hide it";
-            hiddenInfo.style.display = 'block'
-        }else{
-            e.currentTarget.textContent = "Show more";
-            hiddenInfo.style.display = 'none';
+        if (currentRadioBtn.value === 'unlock') {
+            if (e.currentTarget.textContent === 'Show more') {
+                e.currentTarget.textContent = "Hide it";
+                hiddenInfo.style.display = 'block'
+            } else {
+                e.currentTarget.textContent = "Show more";
+                hiddenInfo.style.display = 'none';
+            }
         }
     }
-   }
 }
--- JS-Advanced-2024/DOM Manipulations and Events Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS-Advanced-2024/DOM Manipulations and Events Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,15 +1,15 @@
 function encodeAndDecodeMessages() {
     let textArea = document.getElementsByTagName('textarea');
     [...document.getElementsByTagName('button')].forEach(btn => btn.addEventListener('click', message));
- 
+
     function message(btn) {
         if (btn.target.textContent === 'Encode and send it') {
-            let encoded = [...textArea[0].value].map(e => String.fromCharCode(e.charCodeAt(0) + 1)).join('');
-            textArea[1].value = encoded;
+            let encd = [...textArea[0].value].map(e => String.fromCharCode(e.charCodeAt(0) + 1)).join('');
+            textArea[1].value = encd;
             textArea[0].value = '';
         } else if (btn.target.textContent === 'Decode and read it') {
-            let decoded = [...textArea[1].value].map(e => String.fromCharCode(e.charCodeAt(0) - 1)).join('');
-            textArea[1].value = decoded;
+            let dcd = [...textArea[1].value].map(e => String.fromCharCode(e.charCodeAt(0) - 1)).join('');
+            textArea[1].value = dcd;
         }
-    }
+    } 
 }
