c08129e9efb7565ee5d5bf907cc9a731e1d88939
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-27 14:48:00 +0200

    Create 5.)FunctionalSum.js

---
+++ JS-Advanced-2024/Advanced Functions Exercise/5.)FunctionalSum.js
@@ -0,0 +1,11 @@
+//Task: Write a function that adds a number passed to it to an internal sum and returns itself with its internal sum set to the new value, so it can be chained functionally.
+
+// Hint: Overwrite toString() of the function.
+
+// Input
+
+// Your function needs to take one numeric argument.
+
+// Output
+
+// Your function needs to return itself with an updated context.
