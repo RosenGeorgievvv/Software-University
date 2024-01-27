dfa0de08cec54485f2f108ce6e982e0374d61013
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-27 13:08:58 +0200

    Update 2.)Add.js

--- JS-Advanced-2024/Advanced Functions/2.)Add.js
+++ JS-Advanced-2024/Advanced Functions/2.)Add.js
@@ -1,3 +1,15 @@
 //Task: Write a program that keeps a number inside its context and returns a new function that adds a given number to the previous one.
 
 //Solution:
+
+function solve(number){
+    let result = number;
+    return function(num){
+        return result + num;
+    }
+}
+let add7 = solve(7);
+
+console.log(add7(2));
+
+console.log(add7(3));
