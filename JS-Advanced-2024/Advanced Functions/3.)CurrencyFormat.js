194f79a7437252b6a689baeab45e88a5e22a0033
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-01-27 13:14:25 +0200

    Update 3.)CurrencyFormat.js

--- JS-Advanced-2024/Advanced Functions/3.)CurrencyFormat.js
+++ JS-Advanced-2024/Advanced Functions/3.)CurrencyFormat.js
@@ -9,9 +9,15 @@ function formatterFunction(separator, symbol, firstSymbol, formatter){
     return (sum) => formatter(separator, symbol, firstSymbol, sum);
 }
 
+function currencyFormatter(separator, symbol, symbolFirst, value) {
+    let result = Math.trunc(value) + separator;
+    result += value.toFixed(2).substr(-2, 2);
+    if (symbolFirst) return symbol + ' ' + result;
+    else return result + ' ' + symbol;
+}
 
 
-let dollarFormatter = createFormatter(',', '$', true, currencyFormatter);
+let dollarFormatter = formatterFunction(',', '$', true, currencyFormatter);
 console.log(dollarFormatter(5345));   // $ 5345,00
 console.log(dollarFormatter(3.1429)); // $ 3,14
 console.log(dollarFormatter(2.709));  // $ 2,71
