03d162d82b33bcf8c461ef90c93e7383650a213d
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-01 13:38:13 +0200

    Create 3.)DeckOfCards.js

---
+++ JS-Advanced-2024/Unit Testing And Error Handling/3.)DeckOfCards.js
@@ -0,0 +1,10 @@
+//Task: Write a function that takes a deck of cards as an array of strings and prints them as a sequence of cards (space separated). Use the solution from the previous task to generate the cards.
+
+// Print `Invalid card: ${card}` when an invalid card definition is passed as input.
+
+// Input / Output
+
+// The function takes an array of strings as a parameter. Print the list of cards as string, separated by space.
+
+//Solution:
+
