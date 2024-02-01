22305e08377f1dd0ad9b067eeb27d87c49512d84
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-01 13:50:37 +0200

    update

---
+++ JS-Advanced-2024/Unit Testing And Error Handling/4.)SumOfNumbers.js
@@ -0,0 +1,32 @@
+//Task: You are required to submit only the unit tests for the object/function you are testing.
+
+// 4. Sum of Numbers
+
+// Write tests to check the functionality of the following code:
+
+// sumNumbers.js
+
+// function sum(arr) {
+
+// let sum = 0;
+
+// for (let num of arr){
+
+// sum += Number(num);
+
+// }
+
+// return sum;
+
+// }
+
+// Your tests will be supplied with a function named 'sum()'. It should meet the following requirements:
+
+// · Take an array of numbers as an argument
+
+// · Return the sum of the values of all elements inside the array
+
+
+
+//Solution:
+
