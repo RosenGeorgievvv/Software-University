4b3c1ceccdc8ef936459c0028c771b48c679a02f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-01 14:03:30 +0200

    Update 4.)sumNumbersTest.js

--- JS-Advanced-2024/Unit Testing And Error Handling/tests/4.)sumNumbersTest.js
+++ JS-Advanced-2024/Unit Testing And Error Handling/tests/4.)sumNumbersTest.js
@@ -3,6 +3,10 @@ import {sum} from '../4.)SumOfNumbers';
 
 describe('SumOfNumbers', () =>{
     it('[1,2,3] returns 6', ()=>{
-        assert.equal(su)
-    })
-})
+        assert.equal(sum([1,2,3]), 6)
+    });
+
+    it('[1,2,3] returns 6', () => { 
+        assert.equal(sum([]), 0)
+    });
+});
