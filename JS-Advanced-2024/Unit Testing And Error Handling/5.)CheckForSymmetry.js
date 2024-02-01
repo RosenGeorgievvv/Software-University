d1a092087804a99c5893bc08a198774df420d3ce
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-01 14:08:33 +0200

    Create 5.)CheckForSymmetry.js

---
+++ JS-Advanced-2024/Unit Testing And Error Handling/5.)CheckForSymmetry.js
@@ -0,0 +1,20 @@
+//Task: Unit test of the below code:
+
+function isSymmetric(arr) {
+
+    if (!Array.isArray(arr)){
+    
+    return false; // Non-arrays are non-symmetric
+    
+    }
+    
+    let reversed = arr.slice(0).reverse(); // Clone and reverse
+    
+    let equal = (JSON.stringify(arr) == JSON.stringify(reversed));
+    
+    return equal;
+    
+    }
+
+
+    //Solution:
