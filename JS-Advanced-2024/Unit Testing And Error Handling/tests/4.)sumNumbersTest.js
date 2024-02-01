b9ebf7bd7dce5fe1a7fe8afc06271b1b15936668
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-01 14:01:44 +0200

    Update 4.)sumNumbersTest.js

--- JS-Advanced-2024/Unit Testing And Error Handling/tests/4.)sumNumbersTest.js
+++ JS-Advanced-2024/Unit Testing And Error Handling/tests/4.)sumNumbersTest.js
@@ -1,2 +1,8 @@
-import { expect } from 'chai';
-import {sum} from '../4.)SumOfNumbers'
+import { assert } from 'chai';
+import {sum} from '../4.)SumOfNumbers';
+
+describe('SumOfNumbers', () =>{
+    it('[1,2,3] returns 6', ()=>{
+        assert.equal(su)
+    })
+})
