db60ad0003533c06423008ae86290968ebdb8562
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-05 21:56:07 +0200

    Update 3.)Circle.js

---
+++ JS-Advanced-2024/Classes/3.)Circle.js
@@ -0,0 +1,6 @@
+//Task: Write a class that represents a Circle. It has only one data property - its radius, and it is set through the constructor. The class needs to have getter and setter methods for its diameter - the setter needs to calculate the radius and change it and the getter needs to use the radius to calculate the diameter and return it.
+
+// The circle also has a getter area(), which calculates and returns its area.
+
+
+//Solution:
