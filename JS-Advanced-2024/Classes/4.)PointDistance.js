f399da1e9f36894e9cb3c8a815305a79c27df95f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-05 23:27:21 +0200

    Update 4.)PointDistance.js

--- JS-Advanced-2024/Classes/4.)PointDistance.js
+++ JS-Advanced-2024/Classes/4.)PointDistance.js
@@ -2,3 +2,21 @@
 
 
 //Solution:
+
+class Point{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    static distance(a1, a2){
+        const ax = a1.x - a2.x;
+        const ay = a1.y - a2.y;
+        return Math.sqrt(ax ** 2 + ay ** 2);
+    }
+}
+
+let a1 = new Point(5, 5);
+
+let a2 = new Point(9, 8);
+
+console.log(Point.distance(a1, a2));
