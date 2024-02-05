1ecb4d413a53d57009d3e096cb37935a499940ff
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-05 23:19:38 +0200

    Update 2.)GetPersons.js

--- JS-Advanced-2024/Classes/2.)GetPersons.js
+++ JS-Advanced-2024/Classes/2.)GetPersons.js
@@ -1,3 +1,31 @@
 //Task: Write a function that returns an array of Person objects. Use the class from the previous task, create the following instances, and return them in an array.
 
 //Solution:
+
+function getPersonInfo(){
+    class Person{
+
+        firstName;
+        lastName;
+        age;
+        email;
+    
+        constructor(firstName, lastName, age, email){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+        toString(){
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        }
+    }
+
+    return [
+        new Person('Anna', 'Simpson', 22, 'anna@yahoo.com'),
+        new Person('SoftUni'),
+        new Person('Stephan', 'Johnson', 25),
+        new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com')
+    ];
+}
+console.log(getPersonInfo)
