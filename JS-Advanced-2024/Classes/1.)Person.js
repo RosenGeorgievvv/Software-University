71dbe2f73d9b602e68902f29d1df782b20393726
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-05 21:54:23 +0200

    Update 1.)Person.js

---
+++ JS-Advanced-2024/Classes/1.)Person.js
@@ -0,0 +1,24 @@
+//Task: Write a class that represents a personal record. It has the following properties, all set from the constructor:
+
+// · firstName
+
+// · lastName
+
+// · age
+
+// · email
+
+// And a method toString(), which prints a summary of the information. See the example for formatting details.
+
+// Input
+
+// The constructor function will receive valid parameters.
+
+// Output
+
+// The toString()method should return a string in the following format:
+
+// `{firstName} {lastName} (age: {age}, email: {email})`
+
+// Submit the class definition as is, without wrapping it in any function.
+
