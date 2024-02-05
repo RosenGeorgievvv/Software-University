9e320ef38c56971ca7d3054bcf318144440780c9
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-05 23:23:42 +0200

    Update 3.)Circle.js

--- JS-Advanced-2024/Classes/3.)Circle.js
+++ JS-Advanced-2024/Classes/3.)Circle.js
@@ -34,7 +34,6 @@ console.log(`Radius: ${c.radius}`);
 console.log(`Diameter: ${c.diameter}`);
 console.log(`Area: ${c.area}`);
 c.diameter = 1.6;
-
 console.log(`Radius: ${c.radius}`);
 
 console.log(`Diameter: ${c.diameter}`);
