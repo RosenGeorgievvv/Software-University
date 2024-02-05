f67fe689eed2486311126986ece9a48b830c1f1b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-05 23:23:16 +0200

    Update 3.)Circle.js

--- JS-Advanced-2024/Classes/3.)Circle.js
+++ JS-Advanced-2024/Classes/3.)Circle.js
@@ -4,3 +4,42 @@
 
 
 //Solution:
+
+class Circle{
+
+    radius;
+
+    constructor(radius){
+        this.radius = radius;
+    }
+    get radius(){
+        return this.radius;
+    }
+    set radius(value){
+        this.radius = value;
+    }
+    get diameter(){
+        return 2 * this.radius;
+    }
+    set diameter(value){
+        this.radius = value / 2;
+    }
+    get area(){
+        return Math.PI * (this.radius ** 2);
+    }
+}
+let c = new Circle(2);
+
+console.log(`Radius: ${c.radius}`);
+
+console.log(`Diameter: ${c.diameter}`);
+
+console.log(`Area: ${c.area}`);
+
+c.diameter = 1.6;
+
+console.log(`Radius: ${c.radius}`);
+
+console.log(`Diameter: ${c.diameter}`);
+
+console.log(`Area: ${c.area}`);
