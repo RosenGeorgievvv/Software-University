dcccba91bcf4d7922a83dd17a9fd6d90021b4606
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-05 21:56:39 +0200

    Update 4.)PointDistance.js

--- JS-Advanced-2024/Classes/4.)PointDistance.js
+++ JS-Advanced-2024/Classes/4.)PointDistance.js
@@ -1,5 +1,4 @@
 //Task: Write a JS class that represents a Point. It has x and y coordinates as properties, that are set through the constructor, and a static method for finding the distance between two points, called distance().
 
 
-
 //Solution:
