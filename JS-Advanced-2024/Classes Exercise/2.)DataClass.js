b329f0d655c769a287c0e5e6da2f5c0c2431a9a5
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-06 12:26:56 +0200

    Update 2.)DataClass.js

---
+++ JS-Advanced-2024/Classes Exercise/2.)DataClass.js
@@ -0,0 +1,18 @@
+//Task: Write a class Request that holds data about an HTTP request. It has the following properties:
+
+// · method (String)
+
+// · uri (String)
+
+// · version (String)
+
+// · message (String)
+
+// · response (String)
+
+// · fulfilled (Boolean)
+
+// The first four properties (method, uri, version, message) are set through the constructor, in the listed order. The response property is initialized to undefined and the fulfilled property is initially set to false.
+
+
+//Solution:
