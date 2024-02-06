a4a6f89816b92fb869b84c731b355639d7fb822a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-06 12:26:17 +0200

    Update 1.)Rectangle.js

--- JS-Advanced-2024/Classes Exercise/1.)Rectangle.js
+++ JS-Advanced-2024/Classes Exercise/1.)Rectangle.js
@@ -2,3 +2,21 @@
 
 
 //Solution:
+
+
+class Rectangle{
+    constructor(width, height, color){
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    calcArea(){
+        return this.height * this.width;
+    }
+}
+let rect = new Rectangle(4, 5, 'Red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
