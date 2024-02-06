03564d58a138ae474afe3594032d18e4183b48a1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-06 12:36:32 +0200

    Create 4.)SortedList.js

---
+++ JS-Advanced-2024/Classes Exercise/4.)SortedList.js
@@ -0,0 +1,15 @@
+//Task: Implement a class List, which keeps a list of numbers, sorted in ascending order. It must support the following functionality:
+
+// · add(element) - adds a new element to the collection
+
+// · remove(index) - removes the element at position index
+
+// · get(index) - returns the value of the element at position index
+
+// · size - number of elements stored in the collection
+
+// The correct order of the elements must be kept at all times, regardless of which operation is called. Removing and retrieving elements shouldn’t work if the provided index points outside the length of the collection (either throw an error or do nothing). Note the size of the collection is not a function.
+
+
+//Solution: 
+
