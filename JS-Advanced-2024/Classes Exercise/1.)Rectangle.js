7df707fa497529106bd04d15309c49fd0c3d221f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-06 12:21:07 +0200

    Update 1.)Rectangle.js

--- JS-Advanced-2024/Classes Exercise/1.)Rectangle.js
+++ JS-Advanced-2024/Classes Exercise/1.)Rectangle.js
@@ -1,1 +1,4 @@
-//Task: 
+//Task: Write a class Rectangle for a rectangle object. It needs to have a width (Number), height (Number), and color (String) properties, which are set from the constructor, and a calcArea() method, that calculates and returns the rectangle’s area.
+
+
+//Solution:
