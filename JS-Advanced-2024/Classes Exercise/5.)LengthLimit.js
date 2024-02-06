7e380371b880d08f1c3bd5911ccd0e6a8564136e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-06 12:56:14 +0200

    Update 5.)LengthLimit.js

--- JS-Advanced-2024/Classes Exercise/5.)LengthLimit.js
+++ JS-Advanced-2024/Classes Exercise/5.)LengthLimit.js
@@ -14,3 +14,26 @@
 //Solution: 
 
 
+class Stringer{
+
+    constructor(str, length) {
+        this.innerString  = str;
+        this.innerLength = length;
+    }
+
+    increase(length) {
+        this.innerLength += length;
+    }
+
+    decrease(length){
+        this.innerLength -= length;
+        if (this.innerLength < 0) this.innerLength = 0;
+    }
+
+    toString(){
+        if (this.innerLength >= this.innerString.length){
+            return this.innerString;
+        }
+        return `${this.innerString.substring(0,this.innerLength)}...`;
+    }
+}
