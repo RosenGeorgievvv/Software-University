67b7b6c91de6b31968102c917b6a083a0c85e668
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-08 12:43:08 +0200

    Update

--- JS-Advanced-2024/Exam Preparation1/01. Hotel reservation-Resources/app.js
+++ JS-Advanced-2024/Exam Preparation1/01. Hotel reservation-Resources/app.js
@@ -2,6 +2,14 @@ window.addEventListener('load', solve);
 
 function solve() {
 
+    let inputFields = Array.from(document.querySelectorAll('input')).slice(0, 5);
+    let nextButton = document.getElementById('next-btn');
+    
+    let infoList = document.querySelector('.info-list');
+    let confirmList = document.querySelector('.confirm-list');
+    let verification = document.getElementById('verification');
+
+    
     }
 
 
