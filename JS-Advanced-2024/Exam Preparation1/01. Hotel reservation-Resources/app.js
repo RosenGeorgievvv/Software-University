e9d33e31cb5a39daa12fcb91bf68123f4d88e459
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-10 20:42:40 +0200

    Update app.js

--- JS-Advanced-2024/Exam Preparation1/01. Hotel reservation-Resources/app.js
+++ JS-Advanced-2024/Exam Preparation1/01. Hotel reservation-Resources/app.js
@@ -12,7 +12,6 @@ function solve() {
     function onClick(e) {
         e.preventDefault();
 
-
         let [firstName, lastName, checkIn, checkOut, peopleCount] = inputFields;
         if (!firstName.value || !lastName.value || !checkIn.value || !checkOut.value || !peopleCount.value) {
             return
