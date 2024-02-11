22fb57d1e08f2c61de454b0c6de4b4f1bcb763ef
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-11 13:46:54 +0200

    Update 02.Online Shop - Task.js

--- JS-Advanced-2024/Exam Preparation1/02.Online Shop - Task.js
+++ JS-Advanced-2024/Exam Preparation1/02.Online Shop - Task.js
@@ -13,6 +13,5 @@ class OnlineShop {
 const myOnlineShop = new OnlineShop(500)
 
 console.log(myOnlineShop.loadingStore('headphones', 10, 200));
-
 console.log(myOnlineShop.loadingStore('laptop', 5, 200));
 console.log(myOnlineShop.loadingStore('TV', 40, 500));
