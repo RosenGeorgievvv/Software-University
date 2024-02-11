7f1a2db26cf0dd7dfb4ebcc5aaae161a800d7da7
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-11 13:50:40 +0200

    Update 02.Online Shop - Task.js

--- JS-Advanced-2024/Exam Preparation1/02.Online Shop - Task.js
+++ JS-Advanced-2024/Exam Preparation1/02.Online Shop - Task.js
@@ -11,7 +11,14 @@ class OnlineShop {
     }
 
     loadingStore(product, quantity, spaceRequired){
-        
+        if(this.warehouseSpace < spaceRequired){
+            throw new Error('Not enough space in the warehouse.');
+        }
+        this.products.push({
+            product,
+            quantity
+        });
+        this.warehouseSpace -= spaceRequired;
     }
 
 }
