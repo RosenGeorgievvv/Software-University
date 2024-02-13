bca5542762358f4aa0ae39e668fe8fa78a66959c
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-13 11:59:01 +0200

    Update app.js

--- JS-Advanced-2024/Exam Preparation2/01. Ski lift-Resources/app.js
+++ JS-Advanced-2024/Exam Preparation2/01. Ski lift-Resources/app.js
@@ -1,21 +1,34 @@
 window.addEventListener('load', solve);
 
 function solve() {
-   
-const firstNameInput = document.getElementById('first-name');
-const lastNameInput = document.getElementById('last-name');
-const peopleCount = document.getElementById('people-count');
-const dateFrom = document.getElementById('from-date');
-const daysCount = document.getElementById('days-count');
-const nextButton = document.getElementById('next-btn');
+
+    const firstNameInput = document.getElementById('first-name');
+    const lastNameInput = document.getElementById('last-name');
+    const peopleCount = document.getElementById('people-count');
+    const dateFrom = document.getElementById('from-date');
+    const daysCount = document.getElementById('days-count');
+    const nextButton = document.getElementById('next-btn');
+
+    nextButton.addEventListener('click', onClick);
 
 
-if(!firstNameInput.value || !lastNameInput.value || !peopleCount.value || !dateFrom.value || !daysCount.value){
-    return;
+    function onClick(e) {
+        e.preventDefault();
+        let firstName = firstNameInput.value;
+        let lastName = lastNameInput.value;
+        let people = peopleCount.value;
+        let date = dateFrom.value;
+        let days = daysCount.value;
+
+        if (!firstName || !lastName || !people || !date || !days) {
+            return;
+        }
+
+    }
+
+
 }
 
-}
 
 
-    
-    
+
