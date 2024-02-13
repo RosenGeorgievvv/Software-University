97f03d4b01b539d15676c5cc41365681bb7a6db1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-13 14:40:58 +0200

    Update app.js

--- JS-Advanced-2024/Exam Preparation2/01. Ski lift-Resources/app.js
+++ JS-Advanced-2024/Exam Preparation2/01. Ski lift-Resources/app.js
@@ -1,66 +1,99 @@
 window.addEventListener('load', solve);
 
 function solve() {
-
-    const firstNameInput = document.getElementById('first-name');
-    const lastNameInput = document.getElementById('last-name');
-    const peopleCount = document.getElementById('people-count');
-    const dateFrom = document.getElementById('from-date');
-    const daysCount = document.getElementById('days-count');
-    const nextButton = document.getElementById('next-btn');
-    const infoTicketUl = document.querySelector("#info-ticket ul");
-
-    nextButton.addEventListener('click', onClick);
-
-
-    function onClick(e) {
+    const [fName, lName, people, date, days] = document.querySelectorAll('input');
+    const nextStepBtn = document.getElementById('next-btn');
+    nextStepBtn.addEventListener('click', nextStepHandler);
+    const ticketPreview = document.querySelector('.ticket-info-list');
+    const confirmTicket = document.querySelector('.confirm-ticket');
+    const form = document.querySelector('.container-text form');
+    const mainElement = document.getElementById('main');
+    const bodyElement = document.getElementById('body');
+    let savedData = {};
+ 
+    function nextStepHandler(e) {
         e.preventDefault();
-        let firstName = firstNameInput.value;
-        let lastName = lastNameInput.value;
-        let people = peopleCount.value;
-        let date = dateFrom.value;
-        let days = daysCount.value;
-
-        if (!firstName || !lastName || !people || !date || !days) {
+        if (!fName.value || !lName.value || !people.value || !date.value || !days.value) {
+            alert('Fill all inputs!');
             return;
         }
-
-        let ticket = createTicket(firstName, lastName, people, date, days);
-        infoTicketUl.appendChild(ticket);
-        toggleNextBtn();
-
-        firstNameInput.value = '';
-        lastNameInput.value = '';
-        peopleCount.value = '';
-        dateFrom.value = '';
-        daysCount.value = '';
-
+        savedData = {
+            fName: fName.value,
+            lName: lName.value,
+            people: people.value,
+            date: date.value,
+            days: days.value,
+        }
+ 
+        let liContainer = customHtmlElement('li', '', '', 'ticket');
+        let article = customHtmlElement('article', '', liContainer);
+        customHtmlElement('h3', `Name: ${fName.value} ${lName.value}`, article);
+        customHtmlElement('p', `From date: ${date.value}`, article);
+        customHtmlElement('p', `For ${days.value} days`, article);
+        customHtmlElement('p', `For ${people.value} people`, article);
+        let editBtn = customHtmlElement('button', 'Edit', liContainer, 'edit-btn');
+        editBtn.addEventListener('click', editHandler);
+        let continueBtn = customHtmlElement('button', 'Continue', liContainer, 'continue-btn');
+        continueBtn.addEventListener('click', continueHandler);
+ 
+        ticketPreview.appendChild(liContainer);
+        form.reset();
+        nextStepBtn.disabled = true;
     }
-
-    function createTicket(firstName, lastName, people, date, days){
-        let li = document.createElement('li');
-        li.classList.add('ticket');
-        li.innerHTML += "<article>"
-        li.innerHTML += `<h3>Name: ${firstName} ${lastName}</h3>`
-        li.innerHTML += `<p>From date: ${date}</p>`
-        li.innerHTML += `<p>For ${days} days</p>`
-        li.innerHTML += `<p>For ${people} people</p>`
-        li.innerHTML += "</article>";
-        li.innerHTML += `<button class="edit-btn">Edit</button>`
-        li.innerHTML += `<button class="continue-btn">Continue</button>`
-
-
-
-
-        return li
+ 
+    function editHandler(e) {
+        fName.value = savedData.fName;
+        lName.value = savedData.lName;
+        people.value = savedData.people;
+        date.value = savedData.date;
+        days.value = savedData.days;
+        nextStepBtn.disabled = false;
+        e.target.parentNode.remove();
     }
-
-    function toggleNextBtn(){
-        nextButton.disabled = !nextButton.disabled
+    
+    function continueHandler(e) {
+        confirmTicket.appendChild(e.target.parentNode);
+        Array.from(confirmTicket.querySelectorAll('button')).forEach(btn => { btn.remove() });
+ 
+        let confirmBtn = customHtmlElement('button', 'Confirm', confirmTicket.children[0], 'confirm-btn');
+        confirmBtn.textContent = 'Confirm';
+        confirmBtn.addEventListener('click', confirmHandler);
+ 
+        let cancelBtn = customHtmlElement('button', 'Cancel', confirmTicket.children[0], 'cancel-btn');
+        cancelBtn.addEventListener('click', cancelHandler);
     }
-
+ 
+    function confirmHandler(e) {
+        mainElement.remove();
+        customHtmlElement('h1', 'Thank you, have a nice day!', bodyElement, '', 'thank-you');
+        let backBtn = customHtmlElement('button', 'Back', bodyElement, '', 'back-btn');
+        backBtn.addEventListener('click', () => {
+            document.location.reload();
+        })
+    }
+    
+    function cancelHandler(e) {
+        e.target.parentNode.remove();
+        nextStepBtn.disabled = false;
+    }
+ 
+    function customHtmlElement(type, content, parent, className, id) {
+        let newElement = document.createElement(type);
+        if (content) {
+            newElement.textContent = content;
+        }
+        if (className) {
+            newElement.classList.add(className);
+        }
+        if (parent) {
+            parent.appendChild(newElement);
+        }
+        if (id) {
+            newElement.id = id;
+        }
+        return newElement;
+    }
 }
-
 
 
 
