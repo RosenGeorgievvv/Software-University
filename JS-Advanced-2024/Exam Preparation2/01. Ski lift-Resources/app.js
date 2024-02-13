5a7dced827f04b8e14567f628e703173824cc7d1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-13 11:43:04 +0200

    Update app.js

--- JS-Advanced-2024/Exam Preparation2/01. Ski lift-Resources/app.js
+++ JS-Advanced-2024/Exam Preparation2/01. Ski lift-Resources/app.js
@@ -2,6 +2,14 @@ window.addEventListener('load', solve);
 
 function solve() {
    
+const firstNameInput = document.getElementById('first-name');
+const lastNameInput = document.getElementById('last-name');
+const peopleCount = document.getElementById('people-count');
+const dateFrom = document.getElementById('from-date');
+const daysCount = document.getElementById('days-count');
+const nextButton = document.getElementById('next-btn');
+
+
 }
 
 
