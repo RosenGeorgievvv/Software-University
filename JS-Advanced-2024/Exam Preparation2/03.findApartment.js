9bb49bdc566f295188f2e1a5fee17a61247f6a69
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-16 17:07:52 +0200

    Update 03.findApartment.js

--- JS-Advanced-2024/Exam Preparation2/03.findApartment.js
+++ JS-Advanced-2024/Exam Preparation2/03.findApartment.js
@@ -15,7 +15,6 @@ const findNewApartment = {
     }
   },
 
-
   isLargeEnough(apartments, minimalSquareMeters) {
     let resultArr = [];
     if (!Array.isArray(apartments) || typeof minimalSquareMeters !== "number" || apartments.length == 0) {
