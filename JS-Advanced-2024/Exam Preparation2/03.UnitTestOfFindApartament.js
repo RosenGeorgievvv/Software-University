056a603f070cca945cb20bc2d35adfbe4092bf3a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-18 13:13:28 +0200

    Update 03.UnitTestOfFindApartament.js

--- JS-Advanced-2024/Exam Preparation2/03.UnitTestOfFindApartament.js
+++ JS-Advanced-2024/Exam Preparation2/03.UnitTestOfFindApartament.js
@@ -20,6 +20,7 @@ describe("Tests for findNewApartment object", function() {
         });
     });
 
+    
     describe("isLargeEnough function", function() {
         it("Should return array of apartments with areas equal or bigger than minimalSquareMeters", function() {
             expect(findNewApartment.isLargeEnough([50, 60, 70], 60)).to.deep.equal([60, 70]);
@@ -29,7 +30,7 @@ describe("Tests for findNewApartment object", function() {
             expect(findNewApartment.isLargeEnough([40, 45], 50)).to.have.lengthOf(0);
         });
 
-        
+
         it("Should throw an error for invalid input", function() {
             expect(() => { findNewApartment.isLargeEnough("not an array", 50) }).to.throw(Error, "Invalid input!");
             expect(() => { findNewApartment.isLargeEnough([], 50) }).to.throw(Error, "Invalid input!");
