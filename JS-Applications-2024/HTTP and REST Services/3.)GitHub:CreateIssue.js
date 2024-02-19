d89be6ade0746b261eeb48c67170e4cd4f85d179
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-19 08:59:27 +0200

    Create 3.)GitHub:CreateIssue.js

---
+++ JS-Applications-2024/HTTP and REST Services/3.)GitHub:CreateIssue.js
@@ -0,0 +1,6 @@
+//Task: This time we have to create an issue (data should be send to the server). Send a "POST" request to the server with the following JSON as body (send it as application/json):
+
+// You need to use your GitHub account credentials to submit issues. Under the Authorization tab, select Basic and enter your username and password. Send the request to the URI from the previous task, but without the :id.
+
+
+//Solution:
