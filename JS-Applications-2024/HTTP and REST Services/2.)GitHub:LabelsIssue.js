dfa0bdd1160e642547fa855f7f2c0888a92c1a97
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-19 08:55:58 +0200

    Create 2.)GitHub:LabelsIssue.js

---
+++ JS-Applications-2024/HTTP and REST Services/2.)GitHub:LabelsIssue.js
@@ -0,0 +1,3 @@
+//Task: Get the first issue from repository with name "test-nakov-repo". Send a GET request to https://api.github.com/repos/testnakov/test-nakov-repo/issues/:id, where :id is the issue.
+
+//Solution:
