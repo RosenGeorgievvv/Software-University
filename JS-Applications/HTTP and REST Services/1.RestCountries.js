cd6346245c1bc2f852809a4dbc3b4d6be3ad3f7c
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-20 19:04:44 +0200

    Update 1.RestCountries.js

--- JS-Applications/HTTP and REST Services/1.RestCountries.js
+++ JS-Applications/HTTP and REST Services/1.RestCountries.js
@@ -27,6 +27,7 @@ https://restcountries.com/v2/name/Bulgaria
             "Republic of Bulgaria",
             "Република България"
         ],
+        
         "subregion": "Eastern Europe",
         "region": "Europe",
         "population": 6927288,
