7058bebb15f4bd5ed2cbab9c827476584e039dc1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-29 19:09:42 +0200

    Update app.js

--- JS-Applications/Remote Data and Authentication/Exercise/02.Phonebook/app.js
+++ JS-Applications/Remote Data and Authentication/Exercise/02.Phonebook/app.js
@@ -1,22 +1,41 @@
 function attachEvents() {
     const url = "http://localhost:3030/jsonstore/phonebook";
-    const ulRef = document.getElementById('')
+    const ulRef = document.getElementById('phonebook')
     document.getElementById('btnLoad').addEventListener('click', onLoadAllRecords)
+    document.getElementById("btnCreate").addEventListener('click', onCreateRecord);
 
-    async function onLoadAllRecords(e){
+    async function onCreateRecord(e){
+        let personRef = document.getElementById('person');
+        let phoneRef = document.getElementById('phone');
+    }
+
+
+    async function onLoadAllRecords(e) {
 
         let response = await fetch(url)
         let data = await response.json();
+        ulRef.innerHTML = '';
         Object.values(data).forEach(rec => {
             createAndAppendLi(rec);
         })
     }
 
-    function createAndAppendLi(data){
+    function createAndAppendLi(data) {
         let li = document.createElement('li');
         li.textContent = `${data.person}: ${data.phone}`;
+        let btn = document.createElement('button');
+        btn.textContent = "Delete";
+        btn.dataset.id = data._id;
+        btn.addEventListener('click', onDelete);
+        li.appendChild(btn);
+        ulRef.appendChild(li);
 
 
+    }
+    async function onDelete(e) {
+        let id = e.target.dataset.id;
+        await fetch(url + "/" + id, { method: "DELETE" });
+        onLoadAllRecords();
     }
 }
 
