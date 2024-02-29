d93676f82173a9483c79c89ff64adc71cb207266
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-29 18:21:52 +0200

    Update app.js

--- JS-Applications/Remote Data and Authentication/Exercise/01.Messenger/app.js
+++ JS-Applications/Remote Data and Authentication/Exercise/01.Messenger/app.js
@@ -2,8 +2,19 @@ function attachEvents() {
   document.getElementById('refresh').addEventListener('click', onLoadMsg);
   document.getElementById('submit').addEventListener('click', onSubmit)
 
+function onSubmit(e){
+    let nameRef = document.querySelector("input[name='author']");
+    let textRef = document.querySelector("input[name='content']");
+    let name = nameRef.value;
+    let text = textRef.value;
 
+    
 
+}
+
+function onLoadMsg(e){
+
+}
 
 
 }
