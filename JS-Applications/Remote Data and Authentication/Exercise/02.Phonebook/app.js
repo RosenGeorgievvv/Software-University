944cb6e320efd7c0c539ed779e58302e582289dd
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-29 18:51:06 +0200

    Update app.js

--- JS-Applications/Remote Data and Authentication/Exercise/02.Phonebook/app.js
+++ JS-Applications/Remote Data and Authentication/Exercise/02.Phonebook/app.js
@@ -1,5 +1,12 @@
 function attachEvents() {
-    
+    const url = "http://localhost:3030/jsonstore/phonebook";
+    document.getElementById('btnLoad').addEventListener('click', onLoadAllRecords)
+
+    async function onLoadAllRecords(e){
+
+        let response = await fetch(url)
+        let data = await response.json();
+    }
 }
 
 attachEvents();
