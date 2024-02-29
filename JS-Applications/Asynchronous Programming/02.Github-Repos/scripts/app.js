613a9c17ec5233acec6eb7701718c65752c2a176
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-29 18:08:24 +0200

    Update

--- JS-Applications/Asynchronous Programming/02.Github-Repos/scripts/app.js
+++ JS-Applications/Asynchronous Programming/02.Github-Repos/scripts/app.js
@@ -1,35 +1,64 @@
 const list = document.getElementById('repos')
 
 function loadRepos() {
-	const username = document.getElementById('username').value;
+// 	const username = document.getElementById('username').value;
 
-	const url = `https://api.github.com/users/${username}/repos`;
+// 	const url = `https://api.github.com/users/${username}/repos`;
 
-	fetch(url)
-	.then(onHeaders)
-	.then(onSuccess)
-	.catch(onError);
+// 	fetch(url)
+// 	.then(onHeaders)
+// 	.then(onSuccess)
+// 	.catch(onError);
 
-}
-function onHeaders(response){
-	if(!response.ok){
-		throw "Error"
+// }
+// function onHeaders(response){
+// 	if(!response.ok){
+// 		throw "Error"
+// 	}
+// 	return response.json();
+// }
+
+// function onSuccess(data){
+// 	list.replaceChildren(...data.map(createListItem))
+// }
+// function onError(error){
+// list.textContent = error;
+// }
+
+// function createListItem({html_url, full_name}){
+// 	const item = document.createElement('li');
+// 	const anchor = document.createElement('a');
+// 	anchor.href = html_url;
+// 	anchor.textContent = full_name;
+// 	item.appendChild(anchor);
+// 	return item;
+
+const username = document.getElementById('username').value;
+
+const url = `https://api.github.com/users/${username}/repos`;
+
+fetch(url).then(res =>{
+	if(res.status !== 200){
+		throw new Error();
+
 	}
-	return response.json();
-}
+	return res.json();
+}).then(data =>{
+	list.replaceChildren(...data.map(createItems));
+}).catch(error => console.log(error));
 
-function onSuccess(data){
-	list.replaceChildren(...data.map(createListItem))
-}
-function onError(error){
-list.textContent = error;
-}
-
-function createListItem({html_url, full_name}){
+function createItems({html_url, full_name}){
 	const item = document.createElement('li');
 	const anchor = document.createElement('a');
 	anchor.href = html_url;
 	anchor.textContent = full_name;
 	item.appendChild(anchor);
 	return item;
+}
 }         
+
+
+
+
+
+
