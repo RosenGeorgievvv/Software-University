225fa4509ca7c2d51911ba5a12ff3efafaccd2ff
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-02-29 18:22:37 +0200

    Update app.js

--- JS-Applications/Remote Data and Authentication/Exercise/01.Messenger/app.js
+++ JS-Applications/Remote Data and Authentication/Exercise/01.Messenger/app.js
@@ -1,22 +1,22 @@
 function attachEvents() {
-  document.getElementById('refresh').addEventListener('click', onLoadMsg);
-  document.getElementById('submit').addEventListener('click', onSubmit)
-const url = "http://localhost:3030/jsonstore/messenger";
+    document.getElementById('refresh').addEventListener('click', onLoadMsg);
+    document.getElementById('submit').addEventListener('click', onSubmit)
+    const url = "http://localhost:3030/jsonstore/messenger";
 
 
-function onSubmit(e){
-    let nameRef = document.querySelector("input[name='author']");
-    let textRef = document.querySelector("input[name='content']");
-    let name = nameRef.value;
-    let text = textRef.value;
+    function onSubmit(e) {
+        let nameRef = document.querySelector("input[name='author']");
+        let textRef = document.querySelector("input[name='content']");
+        let name = nameRef.value;
+        let text = textRef.value;
 
 
 
-}
+    }
 
-function onLoadMsg(e){
+    function onLoadMsg(e) {
 
-}
+    }
 
 
 }
