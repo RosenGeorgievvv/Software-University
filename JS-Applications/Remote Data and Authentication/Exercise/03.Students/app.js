ace7fcb8fff9f34755f0ebfa987af71f00ab2ab0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-03-01 18:31:38 +0200

    Update

---
+++ JS-Applications/Remote Data and Authentication/Exercise/03.Students/app.js
@@ -0,0 +1,27 @@
+
+const url = "http://localhost:3030/jsonstore/collections/students";
+const submitBtn = document.getElementById('submit');
+
+
+
+submitBtn.addEventListener('click', onSubmitStudent);
+
+
+function onSubmitStudent({firstName, lastName, facultyNumber, grade}){
+
+
+}
+
+function createElement(type, text){
+
+}
+
+async function sendStudent(e){
+    e.preventDefault();
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: { 'Content-type': 'application/json'},
+        body: JSON.stringify({firstName, lastName, facultyNumber, grade})
+    })
+}
