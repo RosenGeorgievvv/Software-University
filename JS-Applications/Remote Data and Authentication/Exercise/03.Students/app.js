cff8869e33aa52c2f96e7d6106541761f68ec103
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-03-02 11:58:02 +0200

    Update

--- JS-Applications/Remote Data and Authentication/Exercise/03.Students/app.js
+++ JS-Applications/Remote Data and Authentication/Exercise/03.Students/app.js
@@ -1,58 +1,56 @@
+(() => {
+    getData();
+    document.querySelector('#submit').addEventListener('click', sendData)
+})();
 
-const url = "http://localhost:3030/jsonstore/collections/students";
-const submitBtn = document.getElementById('submit');
-
-submitBtn.addEventListener('click', onSubmitStudent);
-
-async function getStudents(){
+async function getData() {
     const tBody = document.querySelector('tbody');
-    tBody.innerHTML = '';
+    tBody.innerHTML = ''
     const response = await fetch('http://localhost:3030/jsonstore/collections/students');
     const data = await response.json();
 
-    Object.values(data).map(onSubmitStudent).forEach((eachStudent) => tBody.append(eachStudent));
+    Object.values(data).map(createStudents).forEach((eachStudent) => tBody.append(eachStudent));
 }
 
-function onSubmitStudent({firstName, lastName, facultyNumber, grade}){
+function createStudents({ facultyNumber, firstName, grade, lastName }) {
     const trElement = document.createElement('tr');
-    const tdFirstName = document.createElement('th', firstName);
-    const tdLastName = document.createElement('th', lastName);
-    const tdFacNumber = document.createElement('th', facultyNumber);
-    const tdGrade = document.createElement('th', grade);
-    trElement.append(tdFirstName, tdLastName, tdFacNumber, tdGrade);
-    
+    const tdFirstName = createElement('th', firstName);
+    const tdLastName = createElement('th', lastName);
+    const tdFacultyNumber = createElement('th', facultyNumber);
+    const tdGrade = createElement('th', grade);
+    trElement.append(tdFirstName, tdLastName, tdFacultyNumber, tdGrade);
+
     return trElement;
 }
 
-function createElement(type, text){
+function createElement(type, text) {
     const element = document.createElement(type);
     element.textContent = text;
-    
-    return element;
 
+    return element;
 }
 
-async function sendStudent(e){
-    e.preventDefault();
+async function sendData(event) {
+    event.preventDefault();
 
-    const form = Array.from(document.querySelectorAll('div>input'));
+    const form = Array.from(document.querySelectorAll('div>input'))
     const values = form.map((v) => v.value);
 
     form.map((input) => (input.value = ''));
     const [firstName, lastName, facultyNumber, grade] = values;
 
-    if(isNaN(grade) || isNaN(facultyNumber)){
+    if (isNaN(grade) || isNaN(facultyNumber)) {
         return alert('Grade and Faculty Number must be Number');
     }
 
-    if(firstName == '' || lastName == '' || facultyNumber == '' || grade == ''){
+    if (firstName == '' || lastName == '' || facultyNumber == '' || grade == '') {
         alert('All fields are required!');
     }
 
-    const response = await fetch(url, {
-        method: "POST",
-        headers: { 'Content-type': 'application/json'},
-        body: JSON.stringify({firstName, lastName, facultyNumber, grade})
+    const response = await fetch('http://localhost:3030/jsonstore/collections/students', {
+        method: 'post',
+        headers: { 'Content-Type': 'applications/json' },
+        body: JSON.stringify({ firstName, lastName, facultyNumber, grade })
     })
-    return getStudents();
+    return getData();
 }
