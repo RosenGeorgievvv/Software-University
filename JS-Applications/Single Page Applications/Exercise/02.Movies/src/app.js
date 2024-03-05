b49bda6e30c18b81f83e07fde6704957ebda7b8c
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-03-05 19:25:28 +0200

    Update app.js

--- JS-Applications/Single Page Applications/Exercise/02.Movies/src/app.js
+++ JS-Applications/Single Page Applications/Exercise/02.Movies/src/app.js
@@ -3,3 +3,19 @@ document.querySelectorAll('section').forEach(sec => sec.style.display = 'none');
 const userNav = document.querySelector("li.user");
 const guestNav = document.querySelector('li.guest');
 
+const userData = JSON.parse(sessionStorage.getItem("userData"));
+
+function updateNav(){
+  if(userData){
+    userNav.forEach(li =>{
+        li.style.display = 'block'
+    });
+    
+    guestNav.forEach(li =>{
+        li.style.display = ' none'
+    });
+  }else{
+    userNav.style.display = 'none';
+    guestNav.style.display = 'block';
+  }
+}
