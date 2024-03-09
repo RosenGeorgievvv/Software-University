ef6160f37f3272f101eaba4dd913c6f8705d69d7
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-03-09 11:45:33 +0200

    Update app.js

--- JS-Applications/Asynchronous Programming/Exercise/01.Bus-Stop/app.js
+++ JS-Applications/Asynchronous Programming/Exercise/01.Bus-Stop/app.js
@@ -4,24 +4,5 @@ function getInfo() {
     let stopName = document.getElementById('stopName');
     let busesList = document.getElementById('buses');
     
-    busesList.innerHTML = '';
-    stopName.textContent = '';
-
- 
-
-    fetch(baseUrl + `/${inputBusStop.value}`)
-        .then((res) => res.json())
-        .then((data) => {
-            stopName.textContent = data.name;
-            let numberOfBuses = Object.keys(data.buses).length;
-            let newLi = document.createElement('li');
-
-
-            for (const [key, value] of Object.entries(data.buses)) {
-                let newLi = document.createElement('li');
-                newLi.textContent = `Bus ${key} arrives in ${value} minutes`;
-                busesList.appendChild(newLi);
-            }
-        })
-        .catch((error) => stopName.textContent = 'Error');
+    
 }
