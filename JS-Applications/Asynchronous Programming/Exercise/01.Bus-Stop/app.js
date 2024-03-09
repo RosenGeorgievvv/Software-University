9864d49514e467915bb14da1abf81f9050b8eb53
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-03-09 12:46:33 +0200

    Update

--- JS-Applications/Asynchronous Programming/Exercise/01.Bus-Stop/app.js
+++ JS-Applications/Asynchronous Programming/Exercise/01.Bus-Stop/app.js
@@ -1,8 +1,29 @@
-function getInfo() {
-    let baseUrl = 'http://localhost:3030/jsonstore/bus/businfo/';
-    let inputBusStop = document.getElementById('stopId');
-    let stopName = document.getElementById('stopName');
-    let busesList = document.getElementById('buses');
-    
-    
+async function getInfo() {
+  const stopIdRef = document.getElementById('stopId').value;
+  const stopNameRef = document.getElementById('stopName');
+  const busesListRef = document.getElementById('buses');
+
+  busesListRef.innerHTML = '';
+
+  try{
+    const response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopIdRef}`);
+    if(!response.ok){
+        const error = new Error();
+        error.status = response.status;
+        error.statusText = response.statusText;
+        throw error;
+    }
+    const data = await response.json();
+
+    stopNameRef.textContent = data.name;
+
+    for(const [key, value] of Object.entries(data.buses)){
+        const li = document.createElement('li');
+        li.textContent = `Bus ${key} arrives in ${value} minutes`;
+        busesListRef.appendChild(li);
+    }
+  }
+  catch(error){
+    stopNameRef.textContent = "Error";
+  }
 }
--- JS-Applications/Asynchronous Programming/Exercise/04.Locked-Profile/app.js
+++ JS-Applications/Asynchronous Programming/Exercise/04.Locked-Profile/app.js
@@ -1,49 +1,83 @@
-function lockedProfile() {
+async function lockedProfile() {
+    const main = document.getElementById('main');
+    main.innerHTML = '';
 
-    const mainSection = document.getElementById('main');
     const url = 'http://localhost:3030/jsonstore/advanced/profiles';
 
-    fetch(url)
-        .then(response => {
-            if (response.status !== 200) {
-                throw new Error()
+    const profilesData = await get(url);
+
+    Object.values(profilesData).forEach((profile, index) => {
+        const profileElement = createProfileCard(profile, index + 1);
+        main.appendChild(profileElement);
+    })
+
+
+    function createProfileCard(profile, count) {
+
+        const profileElement = document.createElement('div');
+        profileElement.className = 'profile';
+
+        profileElement.innerHTML = `
+				<img src="./iconProfile2.png" class="userIcon" />
+				<label>Lock</label>
+				<input type="radio" name="user${count}Locked" value="lock" checked>
+				<label>Unlock</label>
+				<input type="radio" name="user${count}Locked" value="unlock"><br>
+				<hr>
+				<label>Username</label>
+				<input type="text" name="user${count}Username" value="${profile.username}" disabled readonly />`;
+
+        const hiddenInfo = document.createElement('div');
+        hiddenInfo.id = `user${count}HiddenFields`;
+
+        hiddenInfo.innerHTML = `
+					<hr>
+					<label>Email:</label>
+					<input type="email" name="user${count}Email" value="${profile.email}" disabled readonly />
+					<label>Age:</label>
+					<input type="email" name="user${count}Age" value="${profile.age}" disabled readonly />`
+
+        hiddenInfo.style.display = 'none';
+        profileElement.appendChild(hiddenInfo);
+
+        const button = document.createElement('button');
+        button.textContent = 'Show more';
+        button.addEventListener('click', (e) => toggleInfo(e, hiddenInfo, count));
+
+        profileElement.appendChild(button);
+
+        return profileElement;
+    }
+
+    function toggleInfo(e, hiddenInfo, count) {
+        const button = e.target;
+        const inputLock = document.querySelector(`[name="user${count}Locked"]`);
+
+        if (inputLock.checked == false) {
+            if (button.textContent == 'Show more') {
+                button.textContent = 'Hide it';
+                hiddenInfo.style.display = 'block';
+            } else if (button.textContent == 'Hide it') {
+                button.textContent = 'Show more';
+                hiddenInfo.style.display = 'none';
             }
-            return response.json();
-        }).then(data => {
-            let values = Object.entries(data);
-            let firstValue = values.shift();
-            document.querySelector('input[name="user1Username"]').value = firstValue[1].username;
-            document.querySelector('input[name="user1Email"]').value = firstValue[1].email;
-            document.querySelector('input[name="user1Age"]').value = firstValue[1].age;
+        }
+    }
 
-            values.forEach(e => {
-                let profile = document.querySelector('.profile');
-                let profileDuplicate = profile.cloneNode(true);
 
-                profileDuplicate.querySelector('input[name="user1Email"]').value = e[1].email;
-                profileDuplicate.querySelector('input[name="user1Age"]').value = e[1].age;
-                profileDuplicate.querySelector('input[name="user1Username"]').value = e[1].username;
+    async function get(url) {
+        try {
+            const response = await fetch(url);
+            if (response.ok == false) {
+                const error = await response.json();
+                throw error;
+            }
 
-                mainSection.appendChild(profileDuplicate);
-            });
+            const data = await response.json();
+            return data;
 
-            let allBtns = document.querySelectorAll('button');
-            allBtns.forEach(ev => {
-                ev.addEventListener('click', (e) => {
-                    let shown = e.target.parentElement.querySelectorAll('input[type="radio"]')[1];
-                    let hidden = e.target.parentElement.getElementsByTagName('div')[0];
-
-                    if (hidden.classList.contains('hiddenInfo')) {
-                        if (e.target.textContent === "Show more" && shown.checked) {
-                            hidden.classList.remove('hiddenInfo');
-                            e.target.textContent = "Hide it"
-                        }
-                    } else {
-                        e.target.textContent = "Show more";
-                        hidden.classList.add('hiddenInfo')
-                    }
-                })
-            })
-        })
-        .catch();
+        } catch (err) {
+            alert(`Error: ${err.message}`);
+        }
+    }
 }
