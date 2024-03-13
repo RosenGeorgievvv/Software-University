c56f59ff5f29b2e579458389f4e833519270f258
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-03-13 22:59:56 +0200

    Update search.js

--- JS-Applications/Routing/Exercise/03.Search-in-List/search.js
+++ JS-Applications/Routing/Exercise/03.Search-in-List/search.js
@@ -1,5 +1,20 @@
 import { html, render } from "https://esm.run/lit-html@1";
+import {towns} from './towns.js';
 
+console.log(towns);
+
+const searchButtonRef = document.querySelector('button');
+
+searchButtonRef.addEventListener('click', search);
+
+const inputField = document.getElementById('searchText');
+const displayInfo = document.getElementById('result');
+
+const template = (towns) => html`
+<ul>${towns.map(t => html`<li>${t}</li>`)}</ul>
+`
+
+render(template(towns), displayInfo);
 function search() {
    // TODO
 }
