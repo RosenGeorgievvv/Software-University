085213621bd00cb2562cf43f88ceeceda8b20434
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-03-13 22:11:44 +0200

    Update app.js

--- JS-Applications/Routing/Exercise/02.HTTP-Status-Cats/app.js
+++ JS-Applications/Routing/Exercise/02.HTTP-Status-Cats/app.js
@@ -2,34 +2,35 @@ import { html, render } from "https://esm.run/lit-html@1";
 import { cats } from "./catSeeder.js";
 console.log(cats);
 
-const allCatsFied = document.getElementById('allCats');
+const allCatsField = document.getElementById('allCats');
 
 const template = (cats) => html`
-  <ul>
+<ul>
     ${cats.map(cat => html`
     <li>
-      <img
-        src="./images/${cat.imageLocation}"
-        width="250"
-        height="250"
-        alt="Card image cap"
-      />
-      <div class="info">
-        <button class="showBtn" @click=${toggleStatus}>Show status code</button>
-        <div class="status" style="display: none" id="100">
-          <h4>Status Code: ${cat.statusCode}</h4>
-          <p>${cat.statusMessage}</p>
+        <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
+        <div class="info">
+            <button class="showBtn" @click=${toggleStatus.bind(null, cat.id)}>Show status code</button>
+            <div class="status" style="display: none" id="${cat.id}">
+                <h4>Status Code: ${cat.statusCode}</h4>
+                <p>${cat.statusMessage}</p>
+            </div>
         </div>
-      </div>
-    </li>
-    `)}
-  </ul>
-`;
+    </li>`)}
+</ul>`;
 
-function toggleStatus(e){
-    e.preventDefault();
+function toggleStatus(id){
+    debugger
+    const changeStyle = document.getElementById(id);
+    const btnRef = document.querySelector('.showBtn');
 
-
+    if(changeStyle.style.display == 'none'){
+        changeStyle.style.display = 'block';
+        btnRef.textContent = 'Hide status code';
+    }else{
+        changeStyle.style.display = 'none';
+        btnRef.textContent = 'Show status code';
+    }
 }
-render(template(cats), allCatsFied)
+render(template(cats), allCatsField)
 
