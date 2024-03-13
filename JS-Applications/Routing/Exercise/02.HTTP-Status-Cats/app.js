2015eb4854bf51ef0d681836f5145098d96dcb67
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-03-13 22:04:29 +0200

    Update

--- JS-Applications/Routing/Exercise/02.HTTP-Status-Cats/app.js
+++ JS-Applications/Routing/Exercise/02.HTTP-Status-Cats/app.js
@@ -1,6 +1,35 @@
-import { html, render } from "../node_modules/lit-html/lit-html.js";
+import { html, render } from "https://esm.run/lit-html@1";
 import { cats } from "./catSeeder.js";
+console.log(cats);
+
+const allCatsFied = document.getElementById('allCats');
+
+const template = (cats) => html`
+  <ul>
+    ${cats.map(cat => html`
+    <li>
+      <img
+        src="./images/${cat.imageLocation}"
+        width="250"
+        height="250"
+        alt="Card image cap"
+      />
+      <div class="info">
+        <button class="showBtn" @click=${toggleStatus}>Show status code</button>
+        <div class="status" style="display: none" id="100">
+          <h4>Status Code: ${cat.statusCode}</h4>
+          <p>${cat.statusMessage}</p>
+        </div>
+      </div>
+    </li>
+    `)}
+  </ul>
+`;
+
+function toggleStatus(e){
+    e.preventDefault();
 
 
- 
- 
+}
+render(template(cats), allCatsFied)
+
