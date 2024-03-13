552f68e3eefb16bcabb15eb83ad9f89f297a357b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-03-13 23:10:31 +0200

    update

--- JS-Applications/Routing/Exercise/03.Search-in-List/search.js
+++ JS-Applications/Routing/Exercise/03.Search-in-List/search.js
@@ -9,12 +9,24 @@ searchButtonRef.addEventListener('click', search);
 
 const inputField = document.getElementById('searchText');
 const displayInfo = document.getElementById('result');
+const townsDivRef = document.getElementById('towns')
 
 const template = (towns) => html`
 <ul>${towns.map(t => html`<li>${t}</li>`)}</ul>
 `
 
-render(template(towns), displayInfo);
+render(template(towns), townsDivRef);
 function search() {
-   // TODO
+   let count = 0;
+   displayInfo.textContent = '';
+
+   document.querySelectorAll('#towns li').forEach(t =>{
+      t.classList.remove('active');
+
+      if(t.textContent.includes(inputField.value)){
+         t.classList.add('active');
+         count++;
+      }
+   })
+displayInfo.textContent = `${count} matches found`
 }
