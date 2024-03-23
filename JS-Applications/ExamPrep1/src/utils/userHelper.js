8c9c429db05021ef10d2127f07725ae3f7941a43
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-03-23 21:32:28 +0200

    Update userHelper.js

--- JS-Applications/ExamPrep1/src/utils/userHelper.js
+++ JS-Applications/ExamPrep1/src/utils/userHelper.js
@@ -3,4 +3,4 @@ const userItem = 'userData';
 export const getUserData = () => JSON.parse(sessionStorage.getItem(userItem));
 export const setUserData = (data) => sessionStorage.setItem(userItem, JSON.stringify(data));
 export const clearUserData = () => sessionStorage.removeItem(userItem);
-          
+           
