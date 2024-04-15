f7f0da8533ed7c30aa9c6c68769e1725ad7a497b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-04-15 14:28:36 +0300

    Update

---
+++ JS-Web/ReactJS/Workshop/client/src/services/userService.js
@@ -0,0 +1,12 @@
+const baseUrl = "http://localhost:3030/jsonstore/users";
+
+export const getAll = async () => {
+  try {
+    const response = await fetch(baseUrl);
+    const result = await response.json();
+    const data = Object.values(result);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
