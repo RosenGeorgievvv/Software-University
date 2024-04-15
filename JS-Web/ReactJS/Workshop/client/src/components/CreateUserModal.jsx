85b0ff516e122c756613d91b602d73868eb7ec43
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-04-15 18:14:52 +0300

    Update CreateUserModal.jsx

--- JS-Web/ReactJS/Workshop/client/src/components/CreateUserModal.jsx
+++ JS-Web/ReactJS/Workshop/client/src/components/CreateUserModal.jsx
@@ -1,5 +1,6 @@
 const CreateUserModal = ({
-    hideModal
+    hideModal,
+    onUserCreate,
 }) =>{
     return(
         <div className="overlay">
@@ -17,7 +18,7 @@ const CreateUserModal = ({
               </svg>
             </button>
           </header>
-          <form>
+          <form onSubmit={onUserCreate}>
             <div className="form-row">
               <div className="form-group">
                 <label htmlFor="firstName">First name</label>
