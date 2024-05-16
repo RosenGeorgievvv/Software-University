56ad26baf75f3c833874dd83903b6b7fae878f09
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-16 15:38:20 +0300

    Update index2.js

--- JS-Web/JS-Back-End/Intro/index2.js
+++ JS-Web/JS-Back-End/Intro/index2.js
@@ -1,12 +1,17 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((request, response) =>{
-    console.log(request.method);
-    console.log(request.url);
-    console.log(request.headers);
+const server = http.createServer((request, response) => {
+  console.log(request.method);
+  console.log(request.url);
+  console.log(request.headers);
 
-    response.write('Hello World!');
+  if (request.url == "/favicon.ico") {
+    response.statusCode = 404;
     response.end();
-})
+  } else {
+    response.write("Hello World!");
+    response.end();
+  }
+});
 
 server.listen(3000);
