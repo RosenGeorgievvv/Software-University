f420b1b95c1471ed149f341fe1b46c341ff95fbd
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-16 15:35:05 +0300

    Update index2.js

--- JS-Web/JS-Back-End/Intro/index2.js
+++ JS-Web/JS-Back-End/Intro/index2.js
@@ -4,6 +4,9 @@ const server = http.createServer((request, response) =>{
     console.log(request.method);
     console.log(request.url);
     console.log(request.headers);
+
+    response.write();
+    response.end();
 })
 
 server.listen(3000);
