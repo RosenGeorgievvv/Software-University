b29a648952cfe9a0c57db8b57c266d0f63ee6782
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-16 16:36:51 +0300

    Update index.js

--- JS-Web/JS-Back-End/Intro/CatShelter/src/index.js
+++ JS-Web/JS-Back-End/Intro/CatShelter/src/index.js
@@ -8,8 +8,9 @@ const indexHtml = `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../content//styles/site.css">
     <link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../content/styles/site.css">
+    <link rel="shortcut icon" type="image/png" href="../../content/images/pawprint.ico" />
     <title>Cat Shelter</title>
 </head>
 
@@ -17,34 +18,80 @@ const indexHtml = `
     <header>
         <nav>
             <ul class="navigation">
-                <li><a href="">Home Page</a></li>
-                <li><a href="">Add Breed</a></li>
-                <li><a href="">Add Cat</a></li>
+                <li><a href="/">Home Page</a></li>
+                <li><a href="/cats/add-breed">Add Breed</a></li>
+                <li><a href="/cats/add-cat">Add Cat</a></li>
             </ul>
         </nav>
         <h1>Cat Shelter</h1>
-    </header>
-    <main>
-        <form action="#" method="" class="cat-form">
-            <h2>Shelter the cat</h2>
-            <img src="https://cdn.pixabay.com/photo/2015/03/27/13/16/cat-694730_1280.jpg" alt="">
-            <label for="name">Name</label>
-            <input type="text" id="name" value="Pretty Kitty" disabled>
-            <label for="description">Description</label>
-            <textarea id="description" disabled>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</textarea>
-            <label for="group">Breed</label>
-            <select id="group" disabled>
-                <option value="Fluffy Cat">Fluffy Cat</option>
-            </select>
-            <button>SHELTER THE CAT</button>
+        <form action="/search">
+            <input type="text">
+            <button type="button">Search</button>
         </form>
+    </header>
+
+    <main>
+        <section class="cats">
+            <ul>
+                <li>
+                    <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg" alt="Black Cat">
+                    <h3></h3>
+                    <p><span>Breed: </span>Bombay Cat</p>
+                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
+                    <ul class="buttons">
+                        <li class="btn edit"><a href="">Change Info</a></li>
+                        <li class="btn delete"><a href="">New Home</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <img src="https://cdn.pixabay.com/photo/2015/06/19/14/20/cat-814952_1280.jpg" alt="">
+                    <h3>Pretty Kitty</h3>
+                    <p><span>Breed: </span>Bombay Cat</p>
+                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
+                    <ul class="buttons">
+                        <li class="btn edit"><a href="">Change Info</a></li>
+                        <li class="btn delete"><a href="">New Home</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <img src="https://cdn.pixabay.com/photo/2018/08/08/05/12/cat-3591348_1280.jpg" alt="Black Cat">
+                    <h3>Pretty Kitty</h3>
+                    <p><span>Breed: </span>Bombay Cat</p>
+                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
+                    <ul class="buttons">
+                        <li class="btn edit"><a href="">Change Info</a></li>
+                        <li class="btn delete"><a href="">New Home</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <img src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alt="Black Cat">
+                    <h3>Pretty Kitty</h3>
+                    <p><span>Price: </span>350$</p>
+                    <p><span>Breed: </span>Bombay Cat</p>
+                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
+                    <ul class="buttons">
+                        <li class="btn edit"><a href="">Change Info</a></li>
+                        <li class="btn delete"><a href="">New Home</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <img src="https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg" alt="Black Cat">
+                    <h3>Pretty Kitty</h3>
+                    <p><span>Breed: </span>Bombay Cat</p>
+                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
+                    <ul class="buttons">
+                        <li class="btn edit"><a href="">Change Info</a></li>
+                        <li class="btn delete"><a href="">New Home</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
     </main>
 </body>
 </html>
 `;
 
-http
-  .createServer((req, res) => {
+http.createServer((req, res) => {
     if (req.url == "/" || req.url == "/index.html") {
       res.writeHead(200, ["Content-Type", "text/html"]);
       res.write(indexHtml);
