2364680b324e0abdea4c3be9377b83a1eb6fb76f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-16 16:28:26 +0300

    Update index.js

--- JS-Web/JS-Back-End/Intro/CatShelter/src/index.js
+++ JS-Web/JS-Back-End/Intro/CatShelter/src/index.js
@@ -1,8 +1,6 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
+http.createServer((req, res) => {
   res.statusCode = 204;
   res.end();
-});
-
-server.listen(3000);
+}).listen(3000);
