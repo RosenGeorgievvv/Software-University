6408ebeae0f3bb31f8632876d0c2c2e5583ef057
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-22 15:52:07 +0300

    Update index.js

--- JS-Web/JS-Back-End/IntroToExpress/src/index.js
+++ JS-Web/JS-Back-End/IntroToExpress/src/index.js
@@ -4,8 +4,7 @@ const app = express();
 
 app.get('/', (request, response) =>{
     response.send("Hello, Express!")
-});
-
+}); 
 app.listen(3000, () =>{
     console.log('Listen on port 3000');
 });
