0dbf51df31a0f887b15931e54b010a10a4054b48
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-05-23 20:42:26 +0300

    Update catalog.js

--- JS-Web/JS-Back-End/IntroToExpress/handlebars/src/controllers/catalog.js
+++ JS-Web/JS-Back-End/IntroToExpress/handlebars/src/controllers/catalog.js
@@ -3,5 +3,10 @@ const data = require('../../data/catalog.json');
 module.exports = {
     catalogController: (req, res) => {
         res.render('catalog', { parts: data});
+    },
+    detailsController: (res, req) => {
+        const { id } = req.params;
+        const partData = data[Number(id)];
+        res.render('details', )
     }
 }
